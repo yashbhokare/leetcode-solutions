4981d23f45ad07a4075e87afcd02d67bb5d6327c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-09-03 17:32:51 -0700

    Time: 19 ms (64.42%), Space: 14 MB (99.85%) - LeetHub

---
+++ merge-intervals/merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    static bool sortcol(const vector<int>& v1,const vector<int>& v2 ) {
+        return v1[0] < v2[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        vector<vector<int>> result;
+        sort(intervals.begin(),intervals.end(),sortcol);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        // cout<<"Start"<<start;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0] <= end){
+                if(intervals[i][1] > end) end = intervals[i][1];
+            }else{
+                result.push_back({start,end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+            
+        }
+        result.push_back({start,end});
+        return result;
+    }
+};
