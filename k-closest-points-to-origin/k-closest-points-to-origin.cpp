05b5e97cf0e3c0508ba63446e419b3712654c889
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-09-28 23:19:45 -0700

    Time: 200 ms (58.67%), Space: 65.3 MB (30.56%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -4,27 +4,15 @@ public:
         return a[0] < b[0];
     }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        unordered_multimap<int,int> pointsMap;
         vector<vector<int>> pointsVector;
         vector<vector<int>> result;
         int calculate = 0;        
         for(int i=0;i<points.size();i++){
             calculate = pow(points[i][0],2) + pow(points[i][1],2);
-            // pointsMap.insert({calculate,i});
             pointsVector.push_back({calculate,points[i][0],points[i][1]});
         }
-        sort(pointsVector.begin(),pointsVector.end(),comp);
-        
-        // for(int i=0;i<pointsVector.size();i++){
-        //     cout<<pointsVector[i][0]<<"  "<<pointsVector[i][1]<<","<<pointsVector[i][2]<<endl;
-        // }
-        // cout<<"Ans Map"<<endl;
-        int i=0;
-        // for(auto it = ordered.begin();i<k; ++it,i++){
-        //     cout<<it->first<<endl;
-        //     result.push_back({points[it->second][0],points[it->second][1]});
-        // }
-        for(i=0;i<k;i++){
+        sort(pointsVector.begin(),pointsVector.end());
+        for(int i=0;i<k;i++){
             result.push_back({pointsVector[i][1],pointsVector[i][2]});
         }
                 
