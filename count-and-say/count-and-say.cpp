77930983ef7e2c9ef60212d6b1886718a60e840e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-10-15 22:42:51 -0700

    Time: 86 ms (8.00%), Space: 121.2 MB (19.04%) - LeetHub

--- count-and-say/count-and-say.cpp
+++ count-and-say/count-and-say.cpp
@@ -3,37 +3,23 @@ public:
     string countAndSay(int n) {
         return countAndSayCal(n);
     }
-    
     string countAndSayCal(int n){
         if(n==1){
             return "1";
-        }else {
-            string num = countAndSayCal(n-1);
-            int count = 1,pos =0;
-            
-            
-            string result = "";
-            for(int i =0;i<num.length()-1;i++){
-                if(num[i]!=num[i+1]){
-                    result = result + to_string(count)+ num[i];
-                    count=0;
-                }
-                count++;
-            }
-            result = result + to_string(count) + num[num.length()-1];
-            return result;
-            // string newNum = "";
-            // for(int i =0;i<num.length();i++){
-            //     if(num[pos]==num[i]){
-            //         count++;
-            //     }else{
-            //         newNum = newNum + to_string(count) +num[pos];
-            //         count = 1;
-            //         pos = i;
-            //     }
-            // }
-            // newNum = newNum + to_string(count) +num[pos];
-            // return newNum;
         }
+        string val = countAndSayCal(n-1);
+        string result = "";
+        int count = 1;
+        int i=0;
+        for(i=0;i<val.length()-1;i++){
+            if(val[i]!=val[i+1]){
+                result = result + to_string(count) + val[i]; 
+                count = 0;
+            }
+            count++;
+        }
+        // cout<<
+        result = result + to_string(count) + val[i]; 
+        return result;
     }
 };
