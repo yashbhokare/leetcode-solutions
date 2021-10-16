5848515b61f52ab6c365e776ad3e72aa886313cc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-10-15 18:14:46 -0700

    Time: 27 ms (34.93%), Space: 14.2 MB (82.62%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -12,7 +12,7 @@ public:
         // cout<<"Start"<<start;
         for(int i=1;i<intervals.size();i++){
             if(intervals[i][0] <= end){
-                if(intervals[i][1] > end) end = intervals[i][1];
+                 end = max(end,intervals[i][1]);
             }else{
                 result.push_back({start,end});
                 start = intervals[i][0];
