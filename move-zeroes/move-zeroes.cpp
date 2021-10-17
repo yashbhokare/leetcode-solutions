9247b281413667126a8ea39a85fb9d76f1cf303c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-10-16 18:28:30 -0700

    Time: 28 ms (19.44%), Space: 19.1 MB (35.22%) - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -1,45 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeroCount = 0;
-        auto it=nums.begin();
-        while(it !=nums.end()){
-            if(*it == 0){
-                nums.erase(it);
-                zeroCount++;
-            }else{
-                ++it;
-            }
-            if(nums.empty()){
-                break;
+        int lastNonZeroIndex = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[lastNonZeroIndex]);
+                lastNonZeroIndex++;
             }
         }
-        nums.insert(nums.end(),zeroCount,0);
     }
 };
-
-// Better Approach
-void moveZeroes1(vector<int>& nums) {
-    int lastNonZeroFoundAt = 0;
-    // If the current element is not 0, then we need to
-    // append it just in front of last non 0 element we found. 
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-            nums[lastNonZeroFoundAt++] = nums[i];
-        }
-    }
- 	// After we have finished processing new elements,
- 	// all the non-zero elements are already at beginning of array.
- 	// We just need to fill remaining array with 0's.
-    for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
-        nums[i] = 0;
-    }
-}
-
-void moveZeroes2(vector<int>& nums) {
-    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
-        if (nums[cur] != 0) {
-            swap(nums[lastNonZeroFoundAt++], nums[cur]);
-        }
-    }
-}
