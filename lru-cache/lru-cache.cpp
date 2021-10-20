33df74c4b76d4b72a6e9abd93268f0696c1e0f57
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-10-20 07:31:25 -0700

    Time: 368 ms (97.81%), Space: 165.2 MB (72.85%) - LeetHub

--- lru-cache/lru-cache.cpp
+++ lru-cache/lru-cache.cpp
@@ -1,35 +1,36 @@
-class LRUCache{
-    int m_capacity;
-    unordered_map<int,  list<pair<int, int>>::iterator> m_map; //m_map_iter->first: key, m_map_iter->second: list iterator;
-    list<pair<int, int>> m_list;                               //m_list_iter->first: key, m_list_iter->second: value;
+class LRUCache {
 public:
+    int maxCapacity;
+    unordered_map<int,list<pair<int,int>>::iterator> mp;
+    list<pair<int,int>> l;
     LRUCache(int capacity) {
-        m_capacity = capacity;
+        maxCapacity = capacity;
     }
+    
     int get(int key) {
-        auto found_iter = m_map.find(key);
-        if (found_iter == m_map.end()) //key doesn't exist
+        if(mp.find(key)== mp.end()){
             return -1;
-        m_list.splice(m_list.begin(), m_list, found_iter->second); //move the node corresponding to key to front
-        return found_iter->second->second;                         //return value of the node
+        }
+        
+        l.splice(l.begin(),l,mp[key]);       
+        return mp[key]->second;
     }
     
     void put(int key, int value) {
-        auto found_iter = m_map.find(key);
-        if (found_iter != m_map.end()) //key exists
-        {
-            m_list.splice(m_list.begin(), m_list, found_iter->second); //move the node corresponding to key to front
-            found_iter->second->second = value;                        //update value of the node
+        if(mp.find(key)!=mp.end()){
+            l.splice(l.begin(),l,mp[key]);
+            mp[key]->second = value;
             return;
         }
-        if (m_map.size() == m_capacity) //reached capacity
-        {
-           int key_to_del = m_list.back().first; 
-           m_list.pop_back();            //remove node in list;
-           m_map.erase(key_to_del);      //remove key in map
+        
+        if(l.size() == maxCapacity){
+            int removeKey= l.back().first;
+            l.pop_back();
+            mp.erase(removeKey);
         }
-        m_list.emplace_front(key, value);  //create new node in list
-        m_map[key] = m_list.begin();       //create correspondence between key and node
+        
+        l.push_front({key,value});
+        mp.insert({key,l.begin()});
     }
 };
 
