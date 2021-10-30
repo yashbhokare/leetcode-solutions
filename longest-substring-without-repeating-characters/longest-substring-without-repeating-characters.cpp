f3950818b75e4382154d73f53fb723dc7ee4b6e6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-10-30 14:42:10 -0700

    Time: 28 ms (40.26%), Space: 11.1 MB (19.69%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int startIndex =0;
-        int maxLength = 0;
-        int startPos;
+        int maxLen = 0;
+        int startPos = 0;
+        int startingIndex = 0;
         unordered_map<char,int> mapper;
-        for(int i=0;i<s.length();i++){
+        for(int i =0;i<s.size();i++){
             auto it = mapper.find(s[i]);
-            // char found
+
             if(it != mapper.end()){
-                startIndex = max(startIndex,it->second + 1);
+                startPos = max(startPos,mapper[s[i]]+1);
                 mapper.erase(s[i]);
             }
-            mapper.insert({s[i],i});
-            if((i-startIndex +1)>maxLength){
-                maxLength = i-startIndex +1;
-                startPos = startIndex;
+            
+            if(i-startPos + 1 > maxLen){
+                maxLen = i - startPos +1;
+                startingIndex = startPos;
             }
+            mapper.insert({s[i],i});
             
         }
-        cout<<startPos;
-        cout<<maxLength;
-        
-        return maxLength;
+        cout<<s.substr(startingIndex,maxLen);
+        return maxLen;
     }
 };
