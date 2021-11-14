6f25e553bdb20446f9d2b04bb26e20807ee3f349
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-11-13 19:50:52 -0700

    Time: 3 ms (80.85%), Space: 12.1 MB (35.94%) - LeetHub

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int index = -1;
-        
-        for(int i=nums.size()-1;i>=1;i--){
+        int swapIndex = -1;
+        int minElement = INT_MAX;
+        int minElementIndex = 0;
+        for(int i=nums.size()-1;i>0;i--){
             if(nums[i] > nums[i-1]){
-                index = i-1;
+                swapIndex = i-1;
                 break;
             }
         }
-        cout<<index<<endl;
-        if(index!=-1){
-            int minValue = INT_MAX;
-            int swapIndex = 0;
-            for(int i=index+1;i<nums.size();i++){
-                if(nums[i]>nums[index] && minValue>=nums[i] ){
-                    minValue = nums[i];
-                    swapIndex = i;
+        cout<<swapIndex;
+        if(swapIndex >=0){
+            int givenValue = nums[swapIndex];
+            for(int i=swapIndex+1;i<nums.size();i++){
+                if(nums[i] > givenValue && nums[i]<=minElement){ // nums[i]<=minElement is important if we are using reverse logic
+                    minElement = nums[i];
+                    minElementIndex = i;
                 }
             }
-            swap(nums[index],nums[swapIndex]);
-        } 
+            swap(nums[swapIndex],nums[minElementIndex]);
+        }
 
-        reverse(nums.begin()+index+1,nums.end());
+
+        reverse(nums.begin()+swapIndex+1,nums.end());
     }
 };
