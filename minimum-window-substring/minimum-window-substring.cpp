b2eee53d3a3a1a686da3734b65c5c29826db6bfb
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-11-13 21:05:37 -0700

    Time: 147 ms (10.62%), Space: 7.8 MB (66.40%) - LeetHub

--- minimum-window-substring/minimum-window-substring.cpp
+++ minimum-window-substring/minimum-window-substring.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
-string minWindow(string s, string t) {
-        vector<int> hist(128, 0);
-        for (char c : t) hist[c]++;
+    string minWindow(string s, string t) {
+        if(t.size() > s.size()) return "";
+        if( t == s) return t;
+        unordered_map<char,int> mapper;
+        for(int i=0;i<t.size();i++){
+            mapper[t[i]]++;
+        }
         
-        int remaining = t.length();
-        int left = 0, right = 0, minStart = 0, minLen = INT_MAX;
-        while (right < s.length()){
-            if (--hist[s[right++]] >= 0) {
-                remaining--;
-            }
-            while (remaining == 0){
-                if (right - left < minLen){
-                    minLen = right - left;
-                    minStart = left;
-                }
-                if (++hist[s[left++]] > 0) remaining++;
-            }
-        }
+        int minSize = INT_MAX;
+        int left =0;
+        int right=0;
+        int startPos = 0;
+        int req = mapper.size();
+        unordered_map<char,int> window;
+        while(right<s.size()){
             
-         return minLen < INT_MAX ? s.substr(minStart, minLen) : "";
+            if(mapper.find(s[right]) != mapper.end()){
+                window[s[right]]++;
+                if(window[s[right]] == mapper[s[right]]){
+                    req--;
+                }
+            }
+            cout<<req<<endl;
+            while(req==0){
+                //Store the result
+                if(right-left+1 < minSize){
+                    minSize = right-left+1;
+                    startPos = left;
+                }
+                
+                if(mapper.find(s[left]) != mapper.end()){
+                    window[s[left]]--;
+                    if(window[s[left]] < mapper[s[left]]){
+                        req++;
+                    }
+                }
+                left++;
+                
+            }
+            
+            right++;
+            
         }
+        cout<<startPos<<endl;
+        cout<<minSize;
+        if(minSize == INT_MAX) return "";
+        return s.substr(startPos,minSize);
+    }
 };
