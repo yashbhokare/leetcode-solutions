2ffcc3fa1edd813831872c45ecf5d7cb48ffc93f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-11-13 18:14:03 -0700

    Time: 0 ms (100.00%), Space: 6.7 MB (16.20%) - LeetHub

---
+++ guess-the-word/guess-the-word.cpp
@@ -0,0 +1,31 @@
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
+class Solution {
+public:
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        for(int i=0;i<10;i++){
+            string guessWord = wordlist[rand()%wordlist.size()];
+            int guessCount = master.guess(guessWord);
+            wordlist = compareTheCount(guessWord,guessCount,wordlist);
+        }
+    }
+    
+    vector<string> compareTheCount(string guessWord,int guessCount,vector<string> wordlist){
+        vector<string> newWordList;
+        for(int i=0;i<wordlist.size();i++){
+            string currentWord = wordlist[i];
+            int counter = 0;
+            for(int j=0;j<currentWord.size();j++){
+                if(guessWord[j] == currentWord[j]) counter++;
+            }
+            if(counter == guessCount) newWordList.push_back(currentWord);
+        }
+        return newWordList;
+    }
+};
