5a1df274f929bb1c9a564e99562a038b1d86b349
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-11-14 13:57:33 -0700

    Time: 156 ms (58.74%), Space: 40.3 MB (9.42%) - LeetHub

---
+++ word-ladder/word-ladder.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,vector<string>> wordSet;
+        unordered_set<string> visited;
+         string newWord = "";
+        for(string word:wordList){
+            for(int i=0;i<word.size();i++){
+                if(word.size() == 1){
+                    newWord = "*";
+                }else {
+                     newWord = word.substr(0,i) + '*' + word.substr(i+1);
+                }
+                wordSet[newWord].push_back(word);
+                cout<<newWord<<" ";
+            }
+        }
+        
+        queue<string> q;
+        cout<<endl<<beginWord;
+        q.push(beginWord);
+        visited.insert(beginWord);
+        int result = 1;
+        while(!q.empty()){
+            int qSize = q.size();
+            cout<<endl;
+            for(int i=0;i<qSize;i++){
+                string word = q.front();
+                for(int i=0;i<word.size();i++){
+                     if(word.size() == 1){
+                        newWord = "*";
+                    }else {
+                        newWord = word.substr(0,i) + '*' + word.substr(i+1);
+                    }
+                    auto it = wordSet.find(newWord);
+                    if(it != wordSet.end()){
+                        vector<string> wordValues = it->second;
+                        for(string wordVal: wordValues){
+                            if(visited.find(wordVal) == visited.end()){
+                                cout<<wordVal<<" ";
+                                if(wordVal==endWord){
+                                    return result+1;
+                                }
+                                q.push(wordVal);
+                                visited.insert(wordVal);
+                            }
+                        }                        
+                    }
+      
+                }
+                q.pop();
+            }
+            result++;
+            cout<<result;
+           
+        }
+        return 0;
+
+    }
+    
+};
