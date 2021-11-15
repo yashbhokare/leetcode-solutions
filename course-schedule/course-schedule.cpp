7b36ba597fbc1181de53a7421f5952b413b5777e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-11-14 19:52:33 -0700

    Time: 28 ms (35.10%), Space: 16.2 MB (10.17%) - LeetHub

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -2,55 +2,47 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,vector<int>> mapper;
-        unordered_set<int> visited;
-        unordered_set<int> check;
         for(int i=0;i<prerequisites.size();i++){
-            auto it = mapper.find(prerequisites[i][0]);
-            // Key found
-            if(it!=mapper.end()){
-                it->second.push_back(prerequisites[i][1]);
-            }else {
-                mapper.insert({prerequisites[i][0],{prerequisites[i][1]}});
-            }
+            mapper[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
         
-        for(int course=0;course<numCourses;course++){
-            if(isCycle(course,mapper,visited,check)){
+        unordered_set<int> visited;
+        unordered_set<int> memo;
+        for(int i=0;i<numCourses;i++){
+            if(isCycle(i,mapper,visited,memo)){
                 return false;
-            }else {
-                check.insert(course);
             }
         }
+
         return true;
+        
     }
     
-    bool isCycle(int& course,unordered_map<int,vector<int>>& mapper,unordered_set<int>& visited,unordered_set<int>& check){
-        
-        if(check.find(course) != check.end() ){
-            return false;
-        }
-        
-        if(visited.find(course) != visited.end()){
+    bool isCycle(int numCourse,unordered_map<int,vector<int>>& mapper,unordered_set<int>& visited,unordered_set<int>& memo){
+        if(visited.find(numCourse)!=visited.end()){
             return true;
         }
-        
-        if(mapper.find(course) == mapper.end()){
+        if(memo.find(numCourse)!=memo.end()){
             return false;
         }
         
-        bool ret =false;
-        visited.insert(course);
-        auto it = mapper.find(course);
-        vector<int> parentNodes = it->second;
-        for(int i=0;i<parentNodes.size();i++){
-            ret = isCycle(parentNodes[i],mapper,visited,check);
-            if(ret){
+        if(mapper.find(numCourse)==mapper.end()){
+            return false;
+        }
+        
+        visited.insert(numCourse);
+        vector<int> childs = mapper[numCourse];
+        bool res= false;
+        for(int child:childs){
+            if(isCycle(child,mapper,visited,memo)){
+                res= true;
                 break;
             }
         }
         
-        visited.erase(course);
-        return ret;
-
+        visited.erase(numCourse);
+        memo.insert(numCourse);
+        return res;
+        
     }
 };
