5b5b9377d492893c7ad42c7b7ae55185ce400ade
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2021-12-05 18:24:50 -0700

    Time: 56 ms (88.35%), Space: 34.5 MB (67.54%) - LeetHub

--- task-scheduler/task-scheduler.cpp
+++ task-scheduler/task-scheduler.cpp
@@ -11,7 +11,15 @@ public:
         int idleTime = (f_max-1)*n;
         for(int i=1;i<26;i++){
             int freq = alpha[i];
-            idleTime = idleTime - min(f_max-1,freq);
+            if(freq == f_max){
+                idleTime = idleTime - (freq - 1);
+// The idea is that if we have two tasks with the same max frequency (for instance A=3 and B=3) 
+// then when we try to place B we do not consume 3 idle slots, but 2 (f_max-1), because B overflows the last location of A:
+// ["A", "B", " ", "A", "B", " ", "A", "B"].
+            }else {
+                idleTime = idleTime - freq;
+            }
+            
         }
         idleTime = max(0,idleTime);
         return tasks.size() + idleTime;
