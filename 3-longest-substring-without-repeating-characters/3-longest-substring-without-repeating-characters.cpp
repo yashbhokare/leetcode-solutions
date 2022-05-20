304e313a818d07ba8301a533600d2173209b3b77
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-05-20 11:00:29 -0700

    Time: 22 ms (57.11%), Space: 8.3 MB (73.44%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLen = 0;
+        int startPos = 0;
+        int startingIndex = 0;
+        unordered_map<char,int> mapper;
+        for(int i =0;i<s.size();i++){
+            auto it = mapper.find(s[i]);
+
+            if(it != mapper.end()){
+                startPos = max(startPos,mapper[s[i]]+1);
+                // mapper.erase(s[i]);
+            }
+            
+            if(i-startPos + 1 > maxLen){
+                maxLen = i - startPos +1;
+                startingIndex = startPos;
+            }
+            mapper[s[i]]= i;
+            
+        }
+        cout<<s.substr(startingIndex,maxLen);
+        return maxLen;
+    }
+};
