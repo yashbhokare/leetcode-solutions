b2823bd5b10d509f30194a8f0478d542c673cdc6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-05-27 22:35:17 -0700

    Time: 11 ms, Memory: 16.3 MB - LeetHub

--- sort-array-by-parity/sort-array-by-parity.cpp
+++ sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
+        int left=0,right=nums.size()-1;
+       while(left<=right){
+            if(nums[left]%2==0){
+                left++;
+            } else{
+               swap(nums[left],nums[right]);
+                right--;
+            };
+        }
+        return nums;
+    }
+    
+    vector<int> sortArrayByParityWithNewArray(vector<int>& nums) {
         vector<int> result(nums.size());
         int left=0,right=nums.size()-1;
         for(auto& num:nums){
