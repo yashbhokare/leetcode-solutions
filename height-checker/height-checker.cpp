1be94ad7c42bdcdc796be7989fe7a58308f69fea
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-05-27 22:44:17 -0700

    Time: 8 ms, Memory: 8.3 MB - LeetHub

---
+++ height-checker/height-checker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sortedHeights = heights;
+        sort(sortedHeights.begin(),sortedHeights.end());
+        int ans=0;
+        for(int i=0;i<heights.size();i++){
+            if(sortedHeights[i]!=heights[i]) ans++;
+        }
+        return ans;
+    }
+};
