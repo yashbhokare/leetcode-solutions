27b2de3ed31fec93884847daaa59eee0822ffdbb
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-05-29 12:16:35 -0700

    Time: 4 ms, Memory: 7.2 MB - LeetHub

--- valid-palindrome/valid-palindrome.cpp
+++ valid-palindrome/valid-palindrome.cpp
@@ -4,12 +4,10 @@ public:
         int left=0,right=s.size()-1;
         while(left<=right){
             if(!isalnum(s[left])) {
-                // cout<<s[left];
                 left++;
                 continue;
             }
             if(!isalnum(s[right])) {
-                // cout<<s[right];
                 right--;
                 continue;
             }
