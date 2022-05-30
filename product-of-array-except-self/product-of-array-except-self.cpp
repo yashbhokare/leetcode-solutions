3104b084c078b6142d5795ae3745c94cd33693e5
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-05-30 15:38:18 -0700

    Time: 39 ms, Memory: 25.1 MB - LeetHub

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> result;
-        int product = 1;
-        bool zero = false;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 0 && !zero){
-                zero = true;
-            } else {
-                // product = product == 0 ? 1 : product;
-                product = product * (nums[i]);
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 0){
-                result.push_back(product);
-            }else if(zero) {
-                result.push_back(0);
-            } 
-            else {
-                result.push_back(product/nums[i]);
-            }
-        }
+        int len=nums.size();
+        vector<int> left(len, 1);
+        vector<int> right(len, 1);
+        vector<int> result(len, 0);
+        for(int i=1; i<len; i++)  left[i]=left[i-1]*nums[i-1];
+        for(int i=len-2; i>=0; i--)  right[i]=right[i+1]*nums[i+1];
+        for(int i=0; i<len; i++) result[i]=left[i]*right[i];
         return result;
+
+        // vector<int> result;
+        // int product = 1;
+        // bool zero = false;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i] == 0 && !zero){
+        //         zero = true;
+        //     } else {
+        //         product = product * (nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i] == 0){
+        //         result.push_back(product);
+        //     }else if(zero) {
+        //         result.push_back(0);
+        //     } 
+        //     else {
+        //         result.push_back(product/nums[i]);
+        //     }
+        // }
+        // return result;
     }
 };
