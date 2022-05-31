ca96ecaa15a34d8356250a932f6d9ca59ed976e4
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-05-30 17:30:40 -0700

    Time: 821 ms (16.34%), Space: 49.7 MB (27.30%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -8,12 +8,9 @@ public:
         
         int longestStreak=0;
         int result=0;
-        // int tempNum=0
          for(auto& num:nums){
              if(numSet.find(num-1)==numSet.end()){
-        
                  longestStreak=1;
-                 // num++;
                  while(numSet.find(num+1)!=numSet.end()){
                      num++;
                      longestStreak++;
