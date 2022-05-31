d0a79f2e39468c8ef2b7c8329da68818499c2b87
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-05-31 06:19:33 -0700

    Time: 30 ms, Memory: 7.7 MB - LeetHub

--- minimum-window-substring/minimum-window-substring.cpp
+++ minimum-window-substring/minimum-window-substring.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         if(t.size() > s.size()) return "";
-        if( t == s) return t;
         unordered_map<char,int> mapper;
         for(int i=0;i<t.size();i++){
             mapper[t[i]]++;
@@ -15,14 +14,12 @@ public:
         int req = mapper.size();
         unordered_map<char,int> window;
         while(right<s.size()){
-            
             if(mapper.find(s[right]) != mapper.end()){
                 window[s[right]]++;
                 if(window[s[right]] == mapper[s[right]]){
                     req--;
                 }
             }
-            cout<<req<<endl;
             while(req==0){
                 //Store the result
                 if(right-left+1 < minSize){
@@ -39,12 +36,9 @@ public:
                 left++;
                 
             }
-            
             right++;
             
         }
-        cout<<startPos<<endl;
-        cout<<minSize;
         if(minSize == INT_MAX) return "";
         return s.substr(startPos,minSize);
     }
