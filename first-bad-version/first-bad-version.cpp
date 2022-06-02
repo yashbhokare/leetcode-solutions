48315f76743e595437ad66ee8770192a7a09414f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-01 19:19:03 -0700

    Time: 0 ms, Memory: 6 MB - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -5,10 +5,10 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int lower = 1, upper = n, mid;
-        while(lower < upper) {
+        while(lower <= upper) {
             mid = lower + (upper - lower) / 2;
             if(!isBadVersion(mid)) lower = mid + 1;   /* Only one call to API */
-            else upper = mid;
+            else upper = mid-1;
         }
         return lower;   /* Because there will alway be a bad version, return lower here */
     }
