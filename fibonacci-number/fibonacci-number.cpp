96bd79eaadb15e6e5a81d64296a2b9b4cc2250b6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-03 09:33:46 -0700

    Time: 6 ms, Memory: 6.7 MB - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
+    unordered_map<int,int> memo;
     int fib(int n) {
-        if(n<2) return n;
-        return fib(n-1)+fib(n-2);
+        if(memo.find(n)!=memo.end()){
+            return memo[n];
+        }
+        if(n<2) {
+            memo[n] = n;
+            return n;
+        };
+        int sum = fib(n-1)+fib(n-2);
+        
+        memo[n]=sum;
+        return sum;
         // return 0;
     }
 };
