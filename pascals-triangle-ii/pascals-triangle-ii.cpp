04da567c6b5b58e4263ec415e1f978d02ec9101b
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-03 09:21:51 -0700

    Time: 0 ms, Memory: 6.3 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.cpp
+++ pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
+    vector<int> getRowBruteForce(int rowIndex) {
         vector<vector<int>> result;
         vector<int> row;
         for(int r=0;r<=rowIndex;r++){
@@ -16,4 +16,13 @@ public:
         }
         return row;
     }
+    
+    vector<int> getRow(int rowIndex){
+        vector<int> ans = vector<int>(rowIndex + 1, 1);
+        for (int i = 1; i < rowIndex; i++)
+          for (int j = i; j > 0; j--)
+            ans[j] = ans[j] + ans[j - 1];  // ans[j] = ans[j-1] + ans[j]
+        return ans;
+      }
+    
 };
