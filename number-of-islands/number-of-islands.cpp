5c9bff20ad3ea5a2c58840592f495cb0d369d01e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-03 19:02:47 -0700

    Time: 54 ms, Memory: 18.2 MB - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,37 +1,58 @@
 class Solution {
 public:
+    vector<vector<int>> dir={
+        {0,1},{1,0},{-1,0},{0,-1}
+    };
     int numIslands(vector<vector<char>>& grid) {
-        int result=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j] == '1'){
-                     result+=markX(grid,i,j);
+        int res=0;
+        for(int r=0;r<grid.size();r++){
+            for(int c=0;c<grid[0].size();c++){
+                if(grid[r][c]=='1'){
+                    bfs(grid,r,c);
+                    res++;
                     // display(grid);
                 }
-               
             }
         }
-        return result;
+        return res;
     }
     
-    int markX(vector<vector<char>>& grid,int posX,int posY){
-        if(grid[posX][posY] == '1') grid[posX][posY] = 'X';
-        else return 0;
+
+    void bfs(vector<vector<char>>& grid,int row,int col){
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        int rMax=grid.size()-1;
+        int cMax=grid[0].size()-1;
+        grid[row][col]='0';
+        while(!q.empty()){
+            int n= q.size();
+            for(int i=0;i<n;i++){
+                
+                int r=q.front().first;
+                int c=q.front().second;
+                
+                q.pop();
+                for(int j=0;j<4;j++){
+                    int newR=r+dir[j][0];
+                    int newC=c+dir[j][1];
+                    if(newR>=0 && newR<=rMax && newC>=0 && newC<=cMax && grid[newR][newC]=='1'){
+                        q.push({newR,newC});
+                        grid[newR][newC]='0';
+                    }
+                }
+            }
+        }
         
-        if(posX+1 <grid.size() &&  grid[posX+1][posY] == '1') markX(grid,posX+1,posY);
-        if(posY+1 <grid[0].size() && grid[posX][posY+1] == '1') markX(grid,posX,posY+1);
-        if(posY-1 >=0 && grid[posX][posY-1] == '1') markX(grid,posX,posY-1);
-        if(posX -1 >=0 && grid[posX-1][posY] == '1') markX(grid,posX-1,posY);
-        return 1;
     }
     
     void display(vector<vector<char>>& grid){
-         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                cout<<grid[i][j]<<" ";
+    cout<<"----------------------------------";
+    for(int r=0;r<grid.size();r++){
+        for(int c=0;c<grid[0].size();c++){
+            cout<<grid[r][c]<<" ";
             }
-             cout<<endl;
-         }
-         cout<<endl;
+        cout<<endl;
+        }
+        
     }
 };
