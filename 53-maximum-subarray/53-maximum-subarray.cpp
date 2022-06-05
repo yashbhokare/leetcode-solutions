d7aea69d98cc519942cb1c5e9c5daaf483a3c146
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-04 23:28:14 -0700

    Time: 240 ms (5.11%), Space: 67.8 MB (11.64%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = INT_MIN;
+        vector<pair<int,int>> resultArray;
+        
+        int current = nums[0];
+        int maxVal = nums[0];
+        int len = 0;
+        int curIndex = 0;
+        int maxIndex = 0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]> nums[i]+current){
+
+                curIndex = i;
+                current = nums[i];
+            } else {
+                current = nums[i]+current;
+            }
+                // current = max(nums[i],nums[i]+current);
+            
+
+            if(current > maxVal){
+                maxVal = current;
+                maxIndex = i;
+            }
+                // maxVal = max(current,maxVal);
+           } 
+        
+         cout<<"Cur Val Index:"<<curIndex<<endl;
+         cout<<"Max Val Index:"<<maxIndex<<endl;
+        
+        // return maxVal;
+        // BRUTE FORCE
+        // int ans = 0;
+        // int money = 7;
+        // for(int i=0;i<nums.size();i++){
+        //     int sum = 0;
+        //     for(int j =i;j<nums.size();j++){
+        //         sum = sum + nums[j];
+        //         if(sum <= money) {
+        //             // cout<<"Sum"<<sum;
+        //             // cout<<"Cur Val Index:"<<i<<endl;
+        //             // cout<<"Max Val Index:"<<j<<endl;
+        //             ans = max(ans,j-i);
+        //         }
+        //         result = max(sum,result);
+        //     }
+        // }
+        // if(ans) cout<<ans+1;
+        // else cout<<0;
+        // cout<<"Answer:"<<ans+1;
+//         for(int i=1;i<=nums.size();i++){
+
+//             for(int j=0;j<=nums.size()-i;j++){
+//                 int sum = 0;
+//                 for(int k=j;k<j+i;k++){
+//                     sum = sum + nums[k];
+//                 }
+//                 result = max(sum,result);
+
+//             }
+//         }
+        return maxVal;
+    }
+};
