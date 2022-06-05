f15044fbe9c84c78ab67ed66d1e98fd29fdf9b36
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-04 19:12:55 -0700

    Time: 14 ms (45.76%), Space: 8 MB (77.80%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/657-robot-return-to-origin.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        vector<vector<int>> dir={
-            {1,0},
-            {0,1},
-            {-1,0},
-            {0,-1}
-        };
-        
 
         int x=0,y=0;
-        int xDir,yDir;
         for(auto& move:moves){
-            if(move=='U'){
-                xDir = dir[0][0];
-                yDir = dir[0][1];
-            }
-            if(move=='R'){
-                xDir = dir[1][0];
-                yDir = dir[1][1];
-            }
-            if(move=='D'){
-                xDir = dir[2][0];
-                yDir = dir[2][1];
-            }
-            if(move=='L'){
-                xDir = dir[3][0];
-                yDir = dir[3][1];
-            }
-            x+=xDir;
-            y+=yDir;
-            
+            if (move == 'U') y--;
+            else if (move == 'D') y++;
+            else if (move == 'L') x--;
+            else if (move == 'R') x++;
+
         }
         return x==0 && y==0;
     }
