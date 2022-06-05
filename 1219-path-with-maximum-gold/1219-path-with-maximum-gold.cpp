9b687e6db66825bdf276e13d4951891d5acd01d9
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-05 07:43:45 -0700

    Time: 473 ms (27.09%), Space: 7.5 MB (77.87%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> dir = {
+        {1,0},{0,1},{-1,0},{0,-1}
+    };
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int result = 0;
+        for(int r=0;r<grid.size();r++){
+            for(int c=0;c<grid[0].size();c++){
+                if(grid[r][c]!=0){
+                    result=max(result,dfs(grid,r,c));
+                }
+            }
+        }
+        return result;
+    }
+    
+    int dfs(vector<vector<int>>& grid,int r,int c){
+        int nr=grid.size();
+        int nc=grid[0].size();
+        // if(r<0 && c<0 && r>=nr && c>=nc){
+        //     return 0;
+        // }
+        int temp=grid[r][c];
+        // Mark as Visited
+        grid[r][c]=0;
+        
+        int maximum=0;
+        for(auto& d:dir){
+            int newR= d[0]+r;
+            int newC = d[1]+c;
+            if(newR>=0 && newC>=0 && newR<nr && newC<nc && grid[newR][newC]!=0){
+                maximum= max(maximum,dfs(grid,newR,newC));
+            }
+        }
+        // Backtrack to mark it not visited
+        grid[r][c]=temp;
+        return maximum+temp;
+    }
+};
