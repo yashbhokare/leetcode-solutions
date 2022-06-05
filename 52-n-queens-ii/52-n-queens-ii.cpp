91fc73679d9d66272135daa00c3f2a3b6e13cbe6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-04 17:37:05 -0700

    Time: 19 ms (25.12%), Space: 7.9 MB (21.92%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int result= 0;
+    int size;
+    int totalNQueens(int n) {
+        size = n;
+        vector<string> matrix;
+        for(int i=0;i<n;i++){
+            string row = "";
+            for(int j=0;j<n;j++){
+                row+=".";
+            }
+            matrix.push_back(row);
+        }
+        unordered_set<int> col;
+        unordered_set<int> diag;
+        unordered_set<int> anti_diag;
+        backtrack(0,col,diag,anti_diag,matrix);
+        return result;
+    }
+    
+    void backtrack(int r,unordered_set<int>& col,unordered_set<int>& diag,unordered_set<int>& anti_diag,vector<string>& matrix){
+        if(r==size){
+            result++;
+            return;
+        }
+        
+        for(int c=0;c<size;c++){
+            if(col.find(c) != col.end() || diag.find(r-c) != diag.end() || anti_diag.find(r+c) != anti_diag.end()){
+                continue;
+            }
+            matrix[r][c] = 'Q';
+            col.insert(c);
+            diag.insert(r-c);
+            anti_diag.insert(r+c);
+            
+            backtrack(r+1,col,diag,anti_diag,matrix);
+            
+            // Backtrack
+            matrix[r][c] = '.';
+            col.erase(c);
+            diag.erase(r-c);
+            anti_diag.erase(r+c);
+            
+        }
+    }
+};
