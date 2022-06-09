83072d6a171fbea8e906d47977861f655181efbf
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-08 18:08:53 -0700

    Time: 615 ms, Memory: 173.1 MB - LeetHub

--- maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
-    int m,n;
-    vector<vector<int>> memo;
-    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-        n = nums.size();
-        m = multipliers.size();
-        memo.resize(m+1,vector<int>(m+1,0));
-        return dp(0,0,nums,multipliers);
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=0;j<m;j++){
-        //             cout<<memo[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-
-        return memo[0][0];
-    }
+//     int memo[1000][1000];
+//     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+//         // memo.resize(multipliers.size(),vector<int>(multipliers.size(),0));
+//         return dp(nums,multipliers,0,0);
+//     }
     
-    int dp(int i, int left,vector<int>& nums, vector<int>& multipliers){
-        if(i>=m){
-            return 0;
+//     int dp(vector<int>& nums,vector<int>& multipliers,int left,int index){
+//         if(index>=multipliers.size()){
+//             return 0;
+//         }
+        
+//         // int right=
+//         int multi = multipliers[index];
+//         int right = (nums.size()-1)-(index-left);
+//         if(memo[left][index]==0){
+//             memo[left][index] = max(nums[left]*multi+dp(nums,multipliers,left+1,index+1),
+//                                     nums[right]*multi+dp(nums,multipliers,left,index+1));
+//         }
+//         return memo[left][index];
+        
+        
+//     }
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        
+        int n = nums.size();
+        int m = multipliers.size();
+        vector<vector<int>> dp(m+1,vector<int>(m+1,0));
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int left = i; left >= 0; left--) {
+                int mult = multipliers[i];
+                int right = n - 1 - (i - left);
+                dp[i][left] = max(mult * nums[left] + dp[i + 1][left + 1], 
+                                       mult * nums[right] + dp[i + 1][left]);
+            }
         }
-         if(memo[i][left]!=0){
-           return memo[i][left];
-        }
-        int multi = multipliers[i];
-        int right = (n-1)-(i-left);
-        if(memo[i][left]==0){
-            memo[i][left] = max(multi*nums[left] + dp(i+1,left+1,nums,multipliers),
-                                multi*nums[right] + dp(i+1,left,nums,multipliers));
-        }
-        return memo[i][left];
+        
+        return dp[0][0];
     }
 };
