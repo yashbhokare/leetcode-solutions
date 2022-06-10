f4e565b2463760a91de7ae324d913d3792c4fbff
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-10 16:33:58 -0700

    Time: 12 ms (63.56%), Space: 10.2 MB (28.60%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int result=INT_MAX;
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size()));
-        for (int i = grid.size() - 1; i >= 0; i--) {
-            for (int j = grid[0].size() - 1; j >= 0; j--) {
-                if(i == grid.size() - 1 && j != grid[0].size() - 1)
-                    dp[i][j] = grid[i][j] +  dp[i][j + 1];
-                else if(j == grid[0].size() - 1 && i != grid.size() - 1)
-                    dp[i][j] = grid[i][j] + dp[i + 1][j];
-                else if(j != grid[0].size() - 1 && i != grid.size() - 1)
-                    dp[i][j] = grid[i][j] + min(dp[i + 1][j], dp[i][j + 1]);
-                else
-                    dp[i][j] = grid[i][j];
+        int nr = grid.size()-1;
+        int nc= grid[0].size()-1;
+        
+        vector<vector<int>> dp(nr+1,vector<int>(nc+1,0));
+        
+        for(int r=nr;r>=0;r--){
+            for(int c=nc;c>=0;c--){
+                // Lowest row in the grid excluding the bottom last value
+                if(r==nr && c!=nc){
+                    dp[r][c] = grid[r][c]+dp[r][c+1];
+                }else if(c==nc && r!=nr){
+                // Right most column in the grid excluding the bottom last value
+                    dp[r][c] = grid[r][c] + dp[r+1][c];
+                }else if(c!=nc && r!=nr){
+                    dp[r][c] = grid[r][c] + min(dp[r+1][c],dp[r][c+1]);
+                }else {
+                    dp[r][c] = grid[r][c];
+                }
             }
         }
         return dp[0][0];
-    }
-    
-    void rec(vector<vector<int>>& grid,int r,int c,int total){
-        int nr=grid.size();
-        int nc=grid[0].size();
-        if(r==nr-1 && c==nc-1){
-            result=min(result,total);
-            return;
-        }
-        int temp=grid[r][c];
-        grid[r][c]=-1;
-        if(r+1<nr && grid[r+1][c]!=-1){
-            rec(grid,r+1,c,total+grid[r+1][c]);
-        }
-        if(c+1<nc && grid[r][c+1]!=-1){
-            rec(grid,r,c+1,total+grid[r][c+1]);
-        }
-        grid[r][c]=temp;
     }
 };
