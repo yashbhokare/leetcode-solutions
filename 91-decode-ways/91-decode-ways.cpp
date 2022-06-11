596a2d90b335bbfb3799201129697a85d08a60e1
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-11 06:18:51 -0700

    Time: 13 ms (6.09%), Space: 10.3 MB (5.38%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int result=0;
+    unordered_set<string> set;
+    unordered_map<string,int> mapper;
+    int numDecodings(string s) {
+        for(int i=1;i<=26;i++){
+            set.insert(to_string(i));
+        }
+        return recursion(0,s);
+        // return result;
+    }
+    
+    int recursion(int index,string s){
+        if(index==s.size()){
+            result++;
+            return 1; 
+        }
+        
+        string newS = s.substr(index);
+        cout<<newS<<endl;
+        if(mapper.find(newS)!=mapper.end()){
+            return mapper[newS];
+        }
+        
+        string s1{s[index]};
+        int val1 = 0;
+        if(set.find(s1)!=set.end()){
+            val1 = recursion(index+1,s);
+        }
+        int val2 =0;
+        if(index+1<s.size()){
+            string s2 {s[index],s[index+1]};
+            // cout<<s2<<" ";
+            if(set.find(s2)!=set.end()){
+                val2 = recursion(index+2,s);
+            }
+            
+        }
+        mapper[newS] = val1+val2;
+        return mapper[newS];
+    }
+};
