568f431b6e3f2e92aa28c61449f488f37ce001d4
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-11 16:51:02 -0700

    Time: 3 ms (89.92%), Space: 9.6 MB (52.52%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -12,13 +12,35 @@ public:
                 moveDown=false;
                 moveRight=true;
                 col++;
-            }
-        //     if(moveRight && col+1<matrix[0].size() && target>=matrix[row][col+1]){
-        //         col++;
-        //     }else {
-        //         return false;
-        //     }                                                          
+            }                                                       
         }
         return false;
     }
 };
+
+// class Solution {
+//   public:
+//   bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//     int m = matrix.size();
+//     if (m == 0)
+//         return false;
+
+//     int n = matrix[0].size();
+//     // binary search
+//     int left = 0, right = m * n - 1;
+//     int pivotIdx, pivotElement;
+//     while (left <= right) {
+//       pivotIdx = (left + right) / 2;
+//       pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+//       if (target == pivotElement)
+//           return true;
+//       else {
+//         if (target < pivotElement)
+//             right = pivotIdx - 1;
+//         else
+//             left = pivotIdx + 1;
+//       }
+//     }
+//     return false;
+//   }
+// };
