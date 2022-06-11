ab20db99e2fbc210c72d30b6fdfd077d5253b394
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-11 16:24:50 -0700

    Time: 559 ms (5.02%), Space: 65.1 MB (5.01%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    unordered_map<int,int> mapperMemo;
+    int result=INT_MAX;
+    unordered_map<int,int> mapper;
     int coinChange(vector<int>& coins, int amount) {
-        return rec(coins,amount);
+        return rec(coins,amount,0,0);
+        // return result==INT_MAX ? -1 : result;
     }
     
-    int rec(vector<int>& coins,int remaining){
-        if(remaining==0){
-            // result++;
+    int rec(vector<int>& coins, int amount,int total,int start){
+        // cout<<amount<<" "<<total<<" "<<coins[start]<<endl;
+        if(amount==0){
+            // result=min(result,total);
+            cout<<total<<endl;
             return 0;
-        }
-        if(remaining<0){
+        }else if(amount<0){
             return -1;
         }
         
-        if(mapperMemo.find(remaining)!=mapperMemo.end()) return mapperMemo[remaining];
-        int min=INT_MAX;
+        if(mapper.find(amount)!=mapper.end()) return mapper[amount];
         
-        for(auto& coin:coins){
-            int res=rec(coins,remaining-coin);
-            if(res>=0 && res<min){
-                min=res+1;
+        int min= INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            int val=rec(coins,amount-coins[i],total+1,i);
+            if(val>=0 && val<min){
+                min=val+1;
             }
         }
-        min = min==INT_MAX ? -1:min;
-        mapperMemo[remaining]=min;
-        return mapperMemo[remaining];
+
+        mapper[amount] = min==INT_MAX ? -1 : min;
+        return mapper[amount];
     }
 };
