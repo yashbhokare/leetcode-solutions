82fe8db9c430c2054f4d293be24172534864476e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-12 22:10:47 -0700

    Time: 302 ms (8.04%), Space: 18.5 MB (43.86%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int index=0;
+        bool changeDir = false;
+        vector<int> res;
+        int iMax = mat.size();
+        int jMax = mat[0].size();
+        int maxVal = iMax+jMax-1;
+        while(index<maxVal){
+            for(int i=0,j=index;i<=index && j>=0;i++,j--){
+                if(changeDir && i<iMax && j<jMax){
+                    res.push_back(mat[i][j]);
+                     // cout<<mat[i][j]<<" ";
+                }else if(!changeDir && j<iMax && i<jMax){
+                    // cout<<mat[j][i]<<" ";
+                    res.push_back(mat[j][i]);
+                }
+            }
+            cout<<index<<endl;
+            changeDir = !changeDir;
+            index++;
+        }
+        return res;
+    }
+};
