19ae43aa8ee4a3e56ee4c2435db02b83ae54a37a
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-13 13:56:20 -0700

    Time: 6 ms (70.29%), Space: 7.4 MB (49.33%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1;
+        while(left<=right){
+            if(!isalnum(s[left])){
+                left++;
+                continue;
+            }
+            if(!isalnum(s[right])){
+                right--;
+                continue;
+            }
+            if(tolower(s[left])!=tolower(s[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
