6dc506de6712cf7174939a324e3e26b33a69a2bc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-13 12:17:31 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (51.09%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -4,18 +4,16 @@ public:
         stack<char> stk;
         for(int i=0;i<s.size();i++){
             char c = s[i];
-            if(c==')' || c==']' || c=='}'){
+            if(c=='(' || c=='[' || c=='{'){
+                stk.push(c);
+            }else {
                 if(stk.empty()) return false;
-                if((c==')' && stk.top()=='(') || (c==']' && stk.top()=='[') || (c=='}' && stk.top()=='{') ){
+                 if((c==')' && stk.top()=='(') || (c==']' && stk.top()=='[') || (c=='}' && stk.top()=='{') ){
                     stk.pop();
-                    continue;
                 }else{
                     return false;
                 }
-            } 
-
-            cout<<c;
-            stk.push(c);
+            }
         }
         
         return stk.empty();
