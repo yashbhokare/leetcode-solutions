de939373f13401241261ebc902eaa0d095fc8466
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-13 22:50:18 -0700

    Time: 29 ms (18.68%), Space: 13.3 MB (53.47%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool isfirstCol = false;
+        
+        for(int r=0;r<matrix.size();r++){
+            
+            if(matrix[r][0]==0) isfirstCol=true;
+            
+            for(int c=1;c<matrix[0].size();c++){
+                   if(matrix[r][c]==0){
+                       matrix[r][0]=0;
+                       matrix[0][c]=0;
+                   }
+            }
+        }
+        
+        for(int r=1;r<matrix.size();r++){
+            for(int c=1;c<matrix[0].size();c++){
+                if(matrix[r][0] == 0 || matrix[0][c]==0) matrix[r][c]=0;
+            }
+        }
+        
+        // Check for first row
+        if(matrix[0][0]==0){
+            for(int c=0;c<matrix[0].size();c++) matrix[0][c]=0;
+        }
+        
+        //Check for first column
+        if(isfirstCol){
+            for(int r=0;r<matrix.size();r++) matrix[r][0]=0;
+        }
+    }
+};
