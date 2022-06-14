a80a4d3cf25bde274bf87b10f637cdfd8f67839a
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-14 16:53:13 -0700

    Time: 24 ms (98.76%), Space: 23.1 MB (92.20%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -16,33 +16,44 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         P=p;
         Q=q;
-        rec(root);
-        return result;
+        return rec(root);
+        // rec(root);
+        // return result;
     }
     
-    bool rec(TreeNode* root){
-        if(root==NULL) return false;
+//     bool rec(TreeNode* root){
+//         if(root==NULL) return false;
         
         
-        bool leftRes = rec(root->left);
-        bool rightRes= rec(root->right);
+//         bool leftRes = rec(root->left);
+//         bool rightRes= rec(root->right);
         
-        if(root==P && (leftRes || rightRes)){
-            result = root;
-            return false;
-        }else if(root==Q && (leftRes || rightRes)){
-            result = root;
-            return false;
-    }
+//         if(root==P && (leftRes || rightRes)){
+//             result = root;
+//             return false;
+//         }else if(root==Q && (leftRes || rightRes)){
+//             result = root;
+//             return false;
+//         }
             
-        if(root==P || root==Q) return true;
+//         if(root==P || root==Q) return true;
         
-        if(leftRes && rightRes) {
-            result = root;
-            cout<<result->val;
-        }else {
-            return leftRes || rightRes;
-        }
-        return false;
-    }
+//         if(leftRes && rightRes) {
+//             result = root;
+//             cout<<result->val;
+//         }else {
+//             return leftRes || rightRes;
+//         }
+//         return false;
+//     }
+    
+     TreeNode* rec(TreeNode* root){
+         if(P->val < root->val && Q->val < root->val){
+             return rec(root->left);
+         }else if(P->val > root->val && Q->val > root->val){
+             return rec(root->right);
+         }else {
+             return root;
+         }
+     }
 };
