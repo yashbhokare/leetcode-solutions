3ea38800ae7627ad8f24ddc30e782028ed5810b5
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-13 19:17:22 -0700

    Time: 30 ms (52.16%), Space: 17.6 MB (8.44%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -5,10 +5,12 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
+        int lcsCount=0;
         memo.resize(n,vector<int>(m,-1));
-        int lcsCount = lcsWithRec(word1,word2,n-1,m-1);
-        
+        // lcsCount = lcsWithRec(word1,word2,n-1,m-1);
+        lcsCount = lcsWithDp(word1,word2,n,m);
         return n+m - 2*lcsCount;
+        // Total size of both strings - 2 X size of common string
     }
     
     int lcsWithRec(string s1,string s2,int i,int j){
@@ -21,5 +23,19 @@ public:
         }
         return memo[i][j];
         
+    }
+    
+    int lcsWithDp(string s1,string s2,int n,int m){
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int r=1;r<=n;r++){
+            for(int c=1;c<=m;c++){
+                if(s1[r-1]==s2[c-1]){
+                    dp[r][c]=1+dp[r-1][c-1];
+                }else {
+                    dp[r][c] = max(dp[r-1][c],dp[r][c-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
