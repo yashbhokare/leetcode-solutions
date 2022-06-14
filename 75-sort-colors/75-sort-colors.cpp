7fde45d48902159dc30372cf584e4881b5dea3f1
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-13 23:39:38 -0700

    Time: 4 ms (42.06%), Space: 8.3 MB (24.17%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -6,13 +6,12 @@ public:
         int mid=0;
         while(mid<=two){
             if(nums[mid]==0){
-                swap(nums[mid],nums[zero++]);
+                swap(nums[mid++],nums[zero++]);
             }else if(nums[mid]==2){
                 swap(nums[mid],nums[two--]);
             }else {
                 mid++;
             }
-            mid = max(mid,zero);
         }
     }
 };
