7fa4277fedcda1e95ab4727f8d8d96797f7f4df9
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-13 23:22:50 -0700

    Time: 10 ms (24.83%), Space: 12.1 MB (37.03%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int swapIndex=-1;
+        for(int i=nums.size()-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                swapIndex= i-1;
+                break;
+            }
+        }
+        
+        cout<<swapIndex;
+        if(swapIndex>=0){
+            int minVal= INT_MAX;
+            int minIndex = swapIndex;
+            for(int i=swapIndex+1;i<nums.size();i++){
+                if(nums[i]>nums[swapIndex] && nums[i]<=minVal){
+                    minVal=nums[i];
+                    minIndex=i;
+                }
+            }
+        
+            swap(nums[swapIndex],nums[minIndex]);
+        }
+
+        
+        reverse(nums.begin()+swapIndex+1,nums.end());
+        
+    }
+};
