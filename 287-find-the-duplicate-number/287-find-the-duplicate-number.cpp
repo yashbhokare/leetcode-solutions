0c0c7dc876cf907e9471cc941ca273b90e3b89a7
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-14 19:25:59 -0700

    Time: 511 ms (5.03%), Space: 61.3 MB (61.10%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -21,24 +21,36 @@
 // Similar to https://leetcode.com/problems/linked-list-cycle-ii/
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+//     int findDuplicate(vector<int>& nums) {
 
-        // Find the intersection point of the two runners.
-        int tortoise = nums[0];
-        int hare = nums[0];
+//         // Find the intersection point of the two runners.
+//         int tortoise = nums[0];
+//         int hare = nums[0];
 
-        do {
-            tortoise = nums[tortoise];
-            hare = nums[nums[hare]];
-        } while (tortoise != hare);
+//         do {
+//             tortoise = nums[tortoise];
+//             hare = nums[nums[hare]];
+//         } while (tortoise != hare);
 
-        // Find the "entrance" to the cycle.
-        tortoise = nums[0];
-        while (tortoise != hare) {
-            tortoise = nums[tortoise];
-            hare = nums[hare];
-        }
+//         // Find the "entrance" to the cycle.
+//         tortoise = nums[0];
+//         while (tortoise != hare) {
+//             tortoise = nums[tortoise];
+//             hare = nums[hare];
+//         }
 
-        return hare;
-    }
+//         return hare;
+//     }
+    
+     int findDuplicate(vector<int>& nums) {
+         
+         for(int i=0;i<nums.size();i++){
+             int val = nums[abs(nums[i])-1];
+             cout<<val<<endl;
+             if(val<0) return abs(nums[i]);
+             nums[abs(nums[i])-1] = -val;
+         }
+         return -1;
+     }
+    
 };
