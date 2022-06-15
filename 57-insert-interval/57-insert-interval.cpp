eb113788159b40ac553fe44613c807bdc2006411
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-14 18:13:45 -0700

    Time: 19 ms (71.86%), Space: 17.1 MB (39.06%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int startNew = newInterval[0];
+        int endNew = newInterval[1];
+        vector<vector<int>> result;
+        bool pushed =false;
+        
+        // int start = intervals[0][0];
+        // int end = intervals[0][1];
+        for(int i=0;i<intervals.size();i++){
+            
+            if((startNew>=intervals[i][0] && startNew<=intervals[i][1]) || (endNew>=intervals[i][0] && endNew<=intervals[i][1]) ||(intervals[i][0]>=startNew && intervals[i][1]<=endNew) ){
+                startNew = min(startNew,intervals[i][0]);
+                endNew = max(endNew,intervals[i][1]);
+                continue;
+            }
+            
+            if(!pushed && endNew<intervals[i][0]){
+                result.push_back({startNew,endNew});
+                pushed= true;
+            }
+            
+             result.push_back(intervals[i]);
+            
+        }
+        if(!pushed) result.push_back({startNew,endNew});
+        return result;
+    }
+};
