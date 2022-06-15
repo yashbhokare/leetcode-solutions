b053f67fbe373f8c1dc13fa30b6efe9643b49de8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-14 19:27:20 -0700

    Time: 139 ms (74.30%), Space: 61.2 MB (61.10%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -46,7 +46,6 @@ public:
          
          for(int i=0;i<nums.size();i++){
              int val = nums[abs(nums[i])-1];
-             cout<<val<<endl;
              if(val<0) return abs(nums[i]);
              nums[abs(nums[i])-1] = -val;
          }
