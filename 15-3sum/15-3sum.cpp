e307a09289444a9d572845f442143710711c5bf6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-16 15:16:25 -0700

    Time: 123 ms (59.72%), Space: 22 MB (36.15%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
+    vector<vector<int>> result;
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
+        if(nums.size() < 2) return {};
         sort(nums.begin(),nums.end());
-        // return twoSum(nums,0,1);
-        for(int i=0;i<nums.size();i++){
-            if(i!=0 && nums[i]==nums[i-1] ){
-                continue;
+        for(int i=0;i<nums.size()-1;i++){
+            if(i!=0 && nums[i]==nums[i-1]){
+               continue;
             }
             int target = -nums[i];
-            vector<vector<int>> res = twoSum(nums,i+1,target);
-            for(int k=0;k<res.size();k++){
-                // Append the target to given result
-                res[k].push_back(nums[i]);
-                // Append the total pair to final ans array
-                ans.push_back(res[k]);
-            }
-        }
-        return ans;
-    }
-    
-    vector<vector<int>> twoSum(vector<int>& nums,int start,int target){
-        vector<vector<int>> result;
-        int lastPos = nums.size()-1;
-        int startPos = start;
-        while(startPos<lastPos){
-            if(startPos!=start && nums[startPos]==nums[startPos-1]){
-                startPos++;
-                continue;
-            }
-            if(lastPos!=nums.size()-1 && nums[lastPos]==nums[lastPos+1]){
-                lastPos--;
-                continue;
-            }
-            int sum = nums[startPos]+nums[lastPos];
-            if(sum==target){
-                result.push_back({nums[startPos],nums[lastPos]});
-                startPos++;
-                lastPos--;
-            }
-            else if(sum<target){
-                startPos++;
-            }else {
-                lastPos--;
-            }
+            twoSumII(nums,i+1,nums.size()-1,target);
         }
         return result;
+    }
+    
+    void twoSumII(vector<int>& nums,int start,int end,int target){
+        int left = start;
+        int right = end;
+        while(left<right){
+            if(left!=start && nums[left]==nums[left-1]) {
+                left++;
+                continue;
+            }
+            
+            if(right!=end && nums[right]==nums[right+1]){
+                right--;
+                continue;
+            }
+            
+            int sum = nums[left]+nums[right];
+            
+            if(sum<target){
+                left++;
+            }else if(sum>target){
+                right--;
+            }else {
+                result.push_back({-target,nums[left],nums[right]});
+                left++;
+                right--;
+            }
+        }
     }
 };
