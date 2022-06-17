3afd93cebf3ede0c921b83e363adf62cbab3bc93
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-16 19:43:05 -0700

    Time: 3 ms (90.52%), Space: 9.5 MB (51.93%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row=0,col=0;
-        bool moveDown=true;
-        bool moveRight=false;
-        while(row<matrix.size() && col<matrix[0].size()){
-            if(target==matrix[row][col]) return true;
-            if(moveDown && row+1<matrix.size() && target>=matrix[row+1][col]){
-                row++;
-            }else {
-                moveDown=false;
-                moveRight=true;
-                col++;
-            }                                                       
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int moveRight = 0;
+        int moveDown = 0;
+        bool goDown=true;
+        while(moveRight<n && moveDown<m){
+            if(matrix[moveDown][moveRight]==target) return true;
+            if(moveDown+1<m && goDown && target>=matrix[moveDown+1][moveRight]){
+                moveDown++;
+            }else{
+                moveRight++;
+                goDown=false;
+            }
         }
         return false;
+        
     }
 };
-
-// class Solution {
-//   public:
-//   bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//     int m = matrix.size();
-//     if (m == 0)
-//         return false;
-
-//     int n = matrix[0].size();
-//     // binary search
-//     int left = 0, right = m * n - 1;
-//     int pivotIdx, pivotElement;
-//     while (left <= right) {
-//       pivotIdx = (left + right) / 2;
-//       pivotElement = matrix[pivotIdx / n][pivotIdx % n];
-//       if (target == pivotElement)
-//           return true;
-//       else {
-//         if (target < pivotElement)
-//             right = pivotIdx - 1;
-//         else
-//             left = pivotIdx + 1;
-//       }
-//     }
-//     return false;
-//   }
-// };
