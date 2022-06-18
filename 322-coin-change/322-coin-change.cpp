ac4b2e7c8378a31cb5189d34eec3a3880300221a
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-17 19:01:05 -0700

    Time: 551 ms (5.00%), Space: 64.8 MB (5.07%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int result=INT_MAX;
+    int ans = 0;
     unordered_map<int,int> mapper;
     int coinChange(vector<int>& coins, int amount) {
-        return rec(coins,amount,0,0);
-        // return result==INT_MAX ? -1 : result;
+        return rec(coins,0,amount);
     }
     
-    int rec(vector<int>& coins, int amount,int total,int start){
-        // cout<<amount<<" "<<total<<" "<<coins[start]<<endl;
+    int rec(vector<int>& coins,int start,int amount){
         if(amount==0){
-            // result=min(result,total);
-            cout<<total<<endl;
             return 0;
-        }else if(amount<0){
+        }
+        if(amount<0){
             return -1;
         }
         
         if(mapper.find(amount)!=mapper.end()) return mapper[amount];
         
-        int min= INT_MAX;
+        int min = INT_MAX;
         for(int i=0;i<coins.size();i++){
-            int val=rec(coins,amount-coins[i],total+1,i);
-            if(val>=0 && val<min){
-                min=val+1;
+            int res= rec(coins,i,amount-coins[i]);
+            if(res>=0 && res<min){
+                min=res+1;
             }
         }
-
-        mapper[amount] = min==INT_MAX ? -1 : min;
+        min = min==INT_MAX ? -1:min;
+        mapper[amount] = min;
         return mapper[amount];
     }
 };
