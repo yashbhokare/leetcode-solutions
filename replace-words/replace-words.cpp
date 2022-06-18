54633fc424fa953657ebecc93fbaec6a52fa603b
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-18 09:42:48 -0700

    Time: 353 ms, Memory: 292.3 MB - LeetHub

---
+++ replace-words/replace-words.cpp
@@ -0,0 +1,49 @@
+struct Trie{
+    bool isWord;
+    unordered_map<char,Trie*> child;
+};
+
+class Solution {
+private:
+    Trie* root = new Trie();
+public:
+    
+    void insertWords(string word){
+        Trie* current = root;
+        for(auto c:word){
+            if(current->child.count(c)<=0) current->child[c]=new Trie();
+            current = current->child[c];
+            // cout<<c;
+        }
+        // cout<<endl;
+        // cout<<word<<endl;
+        current->isWord=true;
+    }
+    
+    string prefix(string word){
+        string result = "";
+        Trie* current = root;
+         for(auto c:word){
+             if(current->child.count(c)<=0) return word;
+             result = result + c;
+             // cout<<result<<endl;
+             if(current->child[c]->isWord) return result;
+             current = current->child[c];
+         }
+        return result;
+    }
+    
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        for(auto word:dictionary) {
+            insertWords(word);
+        }
+        
+        stringstream ss(sentence);
+        string word;
+        string result = "";
+        while(ss>>word){
+            result = result + " "+prefix(word);
+        }
+        return result.substr(1);
+    }
+};
