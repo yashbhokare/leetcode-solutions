d7348de7fbbfb5ec2c9d9221e0ab66addb7d995d
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-19 06:44:15 -0700

    Time: 991 ms (10.65%), Space: 665.9 MB (5.29%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,7 +1,61 @@
+struct TrieNode{
+    bool isWord;
+    vector<TrieNode*> child{vector<TrieNode*>(26, NULL)};
+};
+
+class Trie{
+    public:
+    TrieNode* root;
+    Trie(){
+     root = new TrieNode();
+    }
+    
+    void insert(string word){
+        TrieNode* curr = root;
+        for(auto ch:word){
+            if(curr->child[ch-'a']==NULL) curr->child[ch-'a']= new TrieNode();
+            curr = curr->child[ch-'a'];
+        }
+        curr->isWord=true;
+    }
+    
+    vector<string> findString(string searchWord){
+        vector<string> result;
+         TrieNode* curr = root;
+        for(auto ch:searchWord){
+            if(curr->child[ch-'a']==NULL) return result;
+            curr = curr->child[ch-'a'];
+        }
+        
+        dfs(curr,searchWord,result);
+        return result;
+    }
+    
+    void dfs(TrieNode* curr,string prefix, vector<string>& result){
+        if(result.size()==3) return;
+        // cout<<prefix<<endl;
+        // cout<<curr[]<<endl;
+        if(curr->isWord){
+            result.push_back(prefix);
+        }
+        
+        for(int i=0;i<26;i++){
+            if(curr->child[i]!=0){
+                prefix.push_back(i +'a');
+                // cout<<prefix;
+                dfs(curr->child[i],prefix,result);
+                prefix.pop_back();
+            }
+        }
+        
+    }
+};
+
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        return suggestedProductsBinaryApproach(products,searchWord);
+        // return suggestedProductsBinaryApproach(products,searchWord);
+        return suggestedProductsUsingTrie(products,searchWord);
     }
     
     vector<vector<string>> suggestedProductsBinaryApproach(vector<string>& products, string searchWord){
@@ -39,4 +93,54 @@ public:
         }
         return result;
     }
+    
+    vector<vector<string>> suggestedProductsUsingTrie(vector<string>& products, string searchWord){
+        Trie* trie = new Trie();
+        for(auto word:products){
+            trie->insert(word);
+        }
+        
+        vector<vector<string>> result;
+        string prefix = "";
+        for(auto ch:searchWord){
+            prefix+=ch;
+            result.push_back(trie->findString(prefix));
+        }
+        return result;
+    }
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
