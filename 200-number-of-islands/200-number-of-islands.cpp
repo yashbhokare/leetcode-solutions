5ad8eba2321e197d5910e87fb6d1c55d98c70d1e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-19 10:22:04 -0700

    Time: 48 ms (57.29%), Space: 18.2 MB (17.93%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -12,7 +12,8 @@ public:
         for(int r=0;r<rMax;r++){
             for(int c=0;c<cMax;c++){
                 if(grid[r][c]=='1'){
-                    dfs(grid,r,c);
+                    // dfs(grid,r,c);
+                    bfs(grid,r,c);
                     count++;
                 }
             }
@@ -29,6 +30,26 @@ public:
             int newC = c + dir[i][1];
             if(newR>=0 && newR<rMax && newC>=0 && newC<cMax && grid[newR][newC]=='1'){
                 dfs(grid,newR,newC);
+            }
+        }
+    }
+    
+    void bfs(vector<vector<char>>& grid,int r,int c){
+        queue<pair<int,int>> q;
+        grid[r][c] = '0';
+        q.push({r,c});
+        while(!q.empty()){
+            r = q.front().first;
+            c = q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int newR = r + dir[i][0];
+                int newC = c + dir[i][1];
+                if(newR>=0 && newR<rMax && newC>=0 && newC<cMax && grid[newR][newC]=='1'){
+                    grid[newR][newC] = '0';
+                    q.push({newR,newC});
+
+                }
             }
         }
     }
