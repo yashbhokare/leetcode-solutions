2c47ec910fd40b7b06ce11f2b5ccbeafc230cdfc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-20 11:33:01 -0700

    Time: 4 ms (69.58%), Space: 11 MB (74.88%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums,target);
+    }
+    
+    int binarySearch(vector<int>& nums, int target){
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(target==nums[mid]) return mid;
+            if(nums[low] <= nums[mid]){
+                if( target>=nums[low] && target<=nums[mid]){
+                    high=mid-1;
+                }else {
+                    low=mid+1;
+                }
+            }else {
+                if(target>=nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }else {
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
