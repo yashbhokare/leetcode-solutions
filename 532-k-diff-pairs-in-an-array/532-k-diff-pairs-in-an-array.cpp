57ec63d589754392d7f24f8c880cdbccd9db50be
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-20 06:59:56 -0700

    Time: 25 ms (59.42%), Space: 13.3 MB (53.20%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -10,7 +10,7 @@ public:
         for(auto it=mapper.begin();it!=mapper.end();it++){
             int key=it->first;
             int val=it->second;
-            if(k>0 && mapper.find(key-k)!=mapper.end()){
+            if(k>0 && mapper.find(key+k)!=mapper.end()){
                 res++;
             }else if(k==0 & val>1){
                 res++;
