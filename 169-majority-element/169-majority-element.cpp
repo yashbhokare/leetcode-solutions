1e0ebe7eeba43c8e0f2761f993564bb6644b389a
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-22 06:35:30 -0700

    Time: 39 ms (18.29%), Space: 19.5 MB (92.07%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
-    int majorityElementHashSet(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int,int> mapper;
-        for(auto num:nums){
-            mapper[num]++;
-            if(mapper[num]>n/2) return num;
-        }
-        return 0;
-    }
-
-
-//Boyer-Moore Voting Algorithm
     int majorityElement(vector<int>& nums) {
-        int candidate = -1;
         int count = 0;
+        int majorityElement = nums[0];
         for(auto num:nums){
             if(count==0){
-                candidate = num;
+                majorityElement = num;
             }
-            
-            if(candidate==num){
-                count++;
-            }else {
-                count--;
-            }
+            if(majorityElement==num) count++;
+            else count--;
         }
-        return candidate;
-        
+        return majorityElement;
     }
-    
 };
