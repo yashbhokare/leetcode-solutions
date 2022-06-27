4b4afa5156580a45223a8528dd8bf257a82c1e4f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-27 07:08:22 -0700

    Time: 128 ms (12.32%), Space: 42.5 MB (48.00%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,3 +1,40 @@
+// Dp approach
+// class Solution {
+// public:
+//     int maxScore(vector<int> &cardPoints, int k) {
+//         int startingIndex = 0;
+//         int presentSubarrayScore = 0;
+//         int n = cardPoints.size();
+//         int requiredSubarrayLength = n - k;
+//         int minSubarrayScore;
+//         int totalScore = 0;
+
+//         // Total score obtained on selecting all the cards.
+//         for (int i : cardPoints) {
+//             totalScore += i;
+//         }
+        
+//         minSubarrayScore = totalScore;
+
+//         if (k == n) {
+//             return totalScore;
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             presentSubarrayScore += cardPoints[i];
+//             int presentSubarrayLength = i - startingIndex + 1;
+//             // If a valid subarray (having size cardsPoints.size() - k) is possible.
+//             if (presentSubarrayLength == requiredSubarrayLength) {
+//                 minSubarrayScore = min(minSubarrayScore, presentSubarrayScore);
+//                 presentSubarrayScore -= cardPoints[startingIndex++];
+//             }
+//         }
+//         return totalScore - minSubarrayScore;
+//     }
+// };
+
+
+// Sliding window to find the minimum sum of subarray and remove it from the total sum to get the max sum 
 class Solution {
 public:
     int maxScore(vector<int> &cardPoints, int k) {
