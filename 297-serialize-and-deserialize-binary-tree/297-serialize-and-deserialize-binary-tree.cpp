356294cfe8ae252afcfd3a6e3f2a073199376697
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-06-28 10:56:50 -0700

    Time: 104 ms (28.11%), Space: 40.1 MB (28.92%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -8,41 +8,36 @@
  * };
  */
 class Codec {
-    public:
-string serialize(TreeNode* root) {
-    if(!root) {
-     return "NULL,";
-    }
-    return to_string(root->val)+","+serialize(root->left)+serialize(root->right);
-}
+public:
 
-// Decodes your encoded data to tree.
-TreeNode* deserialize(string data) {
-    queue<string> q;
-    string s;
-    for(int i=0;i<data.size();i++)
-    {
-        if(data[i]==',')
-        {
-            q.push(s);
-            s="";
-            continue;
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(!root){
+            return "NULL,";
         }
-        s+=data[i];
+        
+        return to_string(root->val) + "," + serialize(root->left) + serialize(root->right);
     }
-    if(s.size()!=0)q.push(s);
-    return deserialize_helper(q);
-}
 
-TreeNode* deserialize_helper(queue<string> &q) {
-    string s=q.front();
-    q.pop();
-    if(s=="NULL")return NULL;
-    TreeNode* root=new TreeNode(stoi(s));
-    root->left=deserialize_helper(q);
-    root->right=deserialize_helper(q);
-    return root;
-}
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        return deserialize_helper(ss);
+    }
+    
+    TreeNode* deserialize_helper(stringstream& ss){
+        string curr;
+        getline(ss, curr, ',');
+        
+        if(curr == "NULL"){
+            return nullptr;
+        }
+        
+        TreeNode* root = new TreeNode(stoi(curr));
+        root->left = deserialize_helper(ss);
+        root->right = deserialize_helper(ss);
+        return root;
+    }
 };
 
 // Your Codec object will be instantiated and called as such:
