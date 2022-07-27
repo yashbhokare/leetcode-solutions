1423e130fb5d685f7b891ab5fd3706140f6ffebb
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-07-27 06:17:01 -0700

    Time: 21 ms (13.78%), Space: 13.3 MB (31.08%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
+    vector<vector<int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
     int orangesRotting(vector<vector<int>>& grid) {
         return bfs(grid);
     }
     
-    vector<vector<int>> dir = {
-        {0,1},{0,-1},{1,0},{-1,0}
-    };
-    
     int bfs(vector<vector<int>>& grid){
         queue<pair<int,int>> q;
-        int rMax = grid.size();
-        int cMax = grid[0].size();
-        
         int totalFreshOranges = 0;
-        for(int r=0;r<rMax;r++){
-            for(int c=0;c<cMax;c++){
-                if(grid[r][c]==2){
-                    q.push({r,c});
-                }else if(grid[r][c]==1){
-                    totalFreshOranges++;
-                }
+        for(int r=0;r<grid.size();r++){
+            for(int c=0;c<grid[0].size();c++){
+                if(grid[r][c]==1) totalFreshOranges++;
+                else if(grid[r][c]==2) q.push({r,c});
             }
         }
         
         if(totalFreshOranges==0) return 0;
-        int result=0;
+        int minutes = 0;
         while(!q.empty()){
             int size = q.size();
+            minutes++;
             while(size--){
                 int r = q.front().first;
                 int c = q.front().second;
                 q.pop();
-                // if(totalFreshOranges==0) return result;
                 for(int i=0;i<4;i++){
-                    int newR=r+dir[i][0];
-                    int newC=c+dir[i][1];
-                    if(newR>=0 && newR<rMax && newC>=0 && newC<cMax && grid[newR][newC]==1){
-                        grid[newR][newC] = 2;
+                    int newR = r + dir[i][0];
+                    int newC = c + dir[i][1];
+                    
+                    if(newR>=0 && newC>=0 && newR<grid.size() && newC<grid[0].size() && grid[newR][newC]==1){
                         totalFreshOranges--;
+                        grid[newR][newC] = 2;
                         q.push({newR,newC});
+                        if(totalFreshOranges==0) return minutes;
                     }
                 }
+                // if(totalFreshOranges==0) return minutes;
             }
-            result++;
         }
-        if(totalFreshOranges==0) return result-1;
+        if(totalFreshOranges==0) return minutes;
         return -1;
     }
-    
 };
