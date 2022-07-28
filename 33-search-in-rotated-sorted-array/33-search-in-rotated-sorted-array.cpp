cc9e06705a3c672b9f192a8fb3f4794dd0c6e6c8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-07-28 07:20:08 -0700

    Time: 10 ms (25.28%), Space: 11.1 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        return binarySearch(nums,target);
-    }
-    
-    int binarySearch(vector<int>& nums, int target){
-        int low=0;
-        int high=nums.size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
+        int left = 0;
+        int right = nums.size()-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
             if(target==nums[mid]) return mid;
-            if(nums[low] <= nums[mid]){
-                if( target>=nums[low] && target<nums[mid]){
-                    high=mid-1;
+            
+            // Check which section is in ascending order
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target<nums[mid]){
+                    right=mid-1;
                 }else {
-                    low=mid+1;
+                    left=mid+1;
                 }
             }else {
-                if(target>nums[mid] && target<=nums[high]){
-                    low=mid+1;
+                if(nums[mid]<target && target<=nums[right]){
+                    left=mid+1;
                 }else {
-                    high = mid-1;
+                    right=mid-1;
                 }
             }
         }
