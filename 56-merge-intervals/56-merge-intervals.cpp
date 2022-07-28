c46853c5e1ab75ddf1ce11825f1fec1cf73f2541
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-07-28 07:55:36 -0700

    Time: 91 ms (15.80%), Space: 20.1 MB (15.73%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,22 +1,43 @@
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(),intervals.end());
+//         int start = intervals[0][0];
+//         int end =  intervals[0][1];
+//         vector<vector<int>> result;
+//         for(int i=1;i<intervals.size();i++){
+//             if(end>=intervals[i][0]){
+//                 start = min(start,intervals[i][0]);
+//                 end = max(end,intervals[i][1]);
+//             }else {
+//                 result.push_back({start,end});
+//                 start = intervals[i][0];
+//                 end = intervals[i][1];
+//             }
+//         }
+//          result.push_back({start,end});
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
 
-        sort(intervals.begin(),intervals.end());
-        int start = intervals[0][0];
-        int end =  intervals[0][1];
-        vector<vector<int>> result;
-        for(int i=1;i<intervals.size();i++){
-            if(end>=intervals[i][0]){
-                start = min(start,intervals[i][0]);
-                end = max(end,intervals[i][1]);
-            }else {
-                result.push_back({start,end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+        vector<vector<int>> merged;
+        for (auto interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.empty() || merged.back()[1] < interval[0]) {
+                merged.push_back(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.back()[1] = max(merged.back()[1], interval[1]);
             }
         }
-         result.push_back({start,end});
-        return result;
+        return merged;
     }
 };
