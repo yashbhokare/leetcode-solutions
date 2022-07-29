adfe5cc8f1a0354fee7de9ecbfe9501b291483a9
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-07-29 07:43:07 -0700

    Time: 11 ms (23.78%), Space: 8.4 MB (53.91%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        string newPattern = "";
+        unordered_map<char,int> mapper;
+        int i=0;
+        for(auto ch:pattern){
+            if(mapper.find(ch)!=mapper.end()){
+                newPattern+=to_string(mapper[ch]);
+            }else {
+                mapper[ch] = i;
+                newPattern+=to_string(i);
+                i++;
+            }
+        }
+
+        vector<string> ans;
+        for(auto word:words){
+            if(isPatternEqual(word,newPattern)){
+                ans.push_back(word);
+            }
+        }
+        return ans;
+    }
+    
+    bool isPatternEqual(string input,string result){
+        // cout<<input<<" "<<result<<endl;
+        // if(input.size()!=result.size()) return false;
+        string newPattern = "";
+        int i=0;
+        unordered_map<char,int> mapper;
+        for(int j=0;j<input.size();j++){
+            if(mapper.find(input[j])!=mapper.end()){
+                newPattern+=to_string(mapper[input[j]]);
+            }else {
+                mapper[input[j]] = i;
+                newPattern+=to_string(i);
+                i++;
+            }
+            if(newPattern[j]!=result[j]) return false;
+        }
+        // cout<<newPattern<<" "<<result<<endl;
+        return true;
+    }
+};
