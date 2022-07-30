fccd3f1f3ecb8467b53ebef941ddd393ea2c8d12
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-07-30 12:26:09 -0700

    Time: 1088 ms (10.79%), Space: 158.2 MB (23.15%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> result;
+        unordered_map<char,int> word2mapper;
+        for(auto word:words2){
+            unordered_map<char,int> tempMapper;
+            for(auto ch:word){
+                tempMapper[ch]++;
+                // cout<<ch<<" "<<word2mapper[ch]<<endl;
+            }
+            for(auto it=tempMapper.begin();it!=tempMapper.end();it++){
+                // cout<<it->first<<" "<<word2mapper[it->first]<<endl;
+                if(word2mapper[it->first]<tempMapper[it->first]){
+                    word2mapper[it->first] = tempMapper[it->first];
+                    // cout<<it->first<<" "<<word2mapper[it->first]<<endl;
+                }
+            }
+        }
+        
+        for(auto word:words1){
+        int size = 0;
+        unordered_map<char,int> word1mapper;
+            for(auto ch:word){
+                word1mapper[ch]++;
+                 // cout<<ch<<" "<<word1mapper[ch]<<endl;
+                if(word2mapper.find(ch)!=word2mapper.end() && word1mapper[ch]==word2mapper[ch]){
+                    size++;
+                }
+                if(size==word2mapper.size()){
+                    result.push_back(word);
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
