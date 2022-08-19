c09f892309780440df49d8d0b02ee0a262da9615
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-19 08:52:29 -0700

    Time: 146 ms (61.98%), Space: 58.2 MB (66.47%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -7,17 +7,22 @@ public:
             frequency[num]++;
         }
         for(auto num:nums){
+            //num already part of a valid subsequence.
             if(frequency[num]==0){
                 continue;
             }
-            else if(subsequence[num-1]>0){
+            //If a valid subsequence exists with last element = num - 1.
+            if(subsequence[num-1]>0){
                 subsequence[num-1]--;
                 subsequence[num]++;
             }else if(frequency[num+1]>0 && frequency[num+2]>0){
+                // If we want to start a new subsequence, check if num + 1 and num + 2 exists
+                // Update the list of subsequences with the newly created subsequence
                 frequency[num+1]--;
                 frequency[num+2]--;
                 subsequence[num+2]++;
             }else {
+                // No valid subsequence is possible with num
                 return false;
             }
             frequency[num]--;
