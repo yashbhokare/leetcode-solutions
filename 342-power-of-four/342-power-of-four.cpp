5185f2c5ff940fbfa278e5d0972aa04a602ebec5
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-21 19:35:20 -0700

    Time: 6 ms (19.21%), Space: 6.2 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        auto val = log(n)/log(2);
-        double num = 2;
-        auto d = fmod(val,num);
-        cout<<d;
-        if(d==0){
+
+        if(fmod(log(n)/log(2),2)==0){
             return true;
-        } 
-    return false;
+        }
+        return false;
     }
 };
