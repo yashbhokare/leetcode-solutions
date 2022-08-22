e6a651bd76d31a5e9eddf4c2386dc66f11bffc58
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-22 08:05:59 -0700

    Time: 4 ms (54.86%), Space: 7.6 MB (54.82%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -22,24 +22,20 @@ public:
         while(head!=NULL){
             ListNode* thirdPointer = NULL;
             if(head->next!=NULL && head->next->next!=NULL){
-                 thirdPointer = head->next->next;
-                head->next->next =NULL;
+                thirdPointer = head->next->next;
+                // head->next->next =NULL;
             }
                
             if(head->next!=NULL){
-                // cout<<prev->val<<"->";
                 prev->next = head->next;
-                head->next = NULL;
+                // head->next = NULL;
                 prev=prev->next;
             }
-            // cout<<prev->val<<"->";
             prev->next=head;
             prev=prev->next;
-            // cout<<prev->val<<"->";
             head = thirdPointer;
-            // cout<<head<<"->";
         }
-        
+        prev->next = NULL;
         return newHead->next;
         
     }
