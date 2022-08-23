7da107fe406c003c3e757a5fc5d487fd25707a11
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-23 08:45:52 -0700

    Time: 27 ms (80.16%), Space: 19.9 MB (38.98%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<int>> row(9,vector<int>(9,0));
+        vector<vector<int>> col(9,vector<int>(9,0));
+        vector<vector<int>> box(9,vector<int>(9,0));
+        
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                if(board[r][c]=='.') continue;
+                int val = board[r][c]-'1';
+                int boxCal =((r/3)*3)+c/3;
+                // cout<<val;
+                if(row[r][val]) return false;
+                if(col[c][val]) return false;
+                if(box[boxCal][val]) return false;
+                
+                row[r][val]++;
+                col[c][val]++;
+                box[boxCal][val]++;
+            }
+        }
+        return true;
+    }
+};
