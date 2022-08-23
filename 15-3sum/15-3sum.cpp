0ad73e7a7abd1678e049843c8bf1e839c27d4010
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-23 06:43:17 -0700

    Time: 160 ms (46.60%), Space: 26.2 MB (17.41%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> result;
     vector<vector<int>> threeSum(vector<int>& nums) {
-        if(nums.size() < 2) return {};
+        vector<vector<int>> result;
+        
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(i!=0 && nums[i]==nums[i-1]){
-               continue;
+        
+        for(int i=0;i<nums.size();i++){
+            if(i==0 || nums[i]!=nums[i-1]){
+                for(auto res:twoSumSorted(nums,i+1,nums.size()-1,-nums[i])){
+                   res.push_back(nums[i]);
+                   result.push_back(res);
+               }
             }
-            int target = -nums[i];
-            twoSumII(nums,i+1,nums.size()-1,target);
         }
         return result;
+        
     }
-    
-    void twoSumII(vector<int>& nums,int start,int end,int target){
-        int left = start;
-        int right = end;
-        while(left<right){
-            if(left!=start && nums[left]==nums[left-1]) {
-                left++;
+    vector<vector<int>> twoSumSorted(vector<int>& nums,int low,int high,int target){
+        vector<vector<int>> result;
+        int start = low;
+        int end = high;
+        while(low<high){
+
+            if(low!=start && nums[low]==nums[low-1]) {
+                low++;
                 continue;
             }
             
-            // if(right!=end && nums[right]==nums[right+1]){
-            //     right--;
-            //     continue;
-            // }
             
-            int sum = nums[left]+nums[right];
+            int sum = nums[low] + nums[high];
             
-            if(sum<target){
-                left++;
-            }else if(sum>target){
-                right--;
+            if(sum==target){
+                result.push_back({nums[low],nums[high]});
+                low++;
+                high--;
+            }else if(sum<target){
+                low++;
             }else {
-                result.push_back({-target,nums[left],nums[right]});
-                left++;
-                right--;
+                high--;
             }
         }
+        return result;
     }
 };
