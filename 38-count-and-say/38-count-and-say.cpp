c99f5377bdcd27fa14c12abbea8be39170f7685e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-23 09:03:18 -0700

    Time: 121 ms (11.73%), Space: 121.1 MB (21.67%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -5,8 +5,7 @@ public:
         for(int i=2;i<=n;i++){
             int count = 1;
             string ans ="";
-            int j=1;
-            for(j=1;j<=res.size();j++){
+            for(int j=1;j<=res.size();j++){
                 if(j==res.size() || res[j]!=res[j-1]){
                     ans = ans +  to_string(count)+ res[j-1];
                     count = 1;
