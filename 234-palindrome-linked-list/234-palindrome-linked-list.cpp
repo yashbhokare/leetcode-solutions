afd56e17da3445f180f7307434d7561441d78088
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-22 18:25:14 -0700

    Time: 349 ms (57.66%), Space: 114.2 MB (74.71%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* p1 = head;
+        ListNode* midValue = midPoint(head);
+        ListNode* p2 = reverse(midValue->next);
+        // cout<<p2->val;
+        while(p2!=NULL){
+            // cout<<p2->val<<" ";
+            if(p1->val!=p2->val) return false;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return true;
+    }
+    
+    ListNode* midPoint(ListNode* head){
+        ListNode* slow= head;
+        ListNode* fast= head;
+        while(fast && fast->next && fast->next->next){
+            slow= slow->next;
+            fast= fast->next->next;
+        }
+        return slow;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head;
+        ListNode* start = head;
+        while(curr && curr->next){
+            
+            ListNode* temp = curr->next;
+            
+            //Remove element
+            curr->next = curr->next->next;
+            
+            temp->next = start;
+            start = temp;
+    
+            // curr = curr->next;
+        }
+        return start;
+    }
+};
