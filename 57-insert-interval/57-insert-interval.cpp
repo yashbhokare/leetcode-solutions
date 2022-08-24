5010070c725a6761f19b855e1254803c3e477bdc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-24 10:01:09 -0700

    Time: 82 ms (5.25%), Space: 17.5 MB (15.35%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int startNew = newInterval[0];
-        int endNew = newInterval[1];
-        vector<vector<int>> result;
-        bool pushed =false;
-
-        for(int i=0;i<intervals.size();i++){
-            
-            if(!pushed &&(startNew>=intervals[i][0] && startNew<=intervals[i][1]) || (endNew>=intervals[i][0] && endNew<=intervals[i][1]) ||(intervals[i][0]>=startNew && intervals[i][1]<=endNew) ){
-                startNew = min(startNew,intervals[i][0]);
-                endNew = max(endNew,intervals[i][1]);
-                continue;
+        vector<vector<int>> merged;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        for(auto interval:intervals){
+            if(merged.empty() || merged.back()[1] < interval[0]){
+                merged.push_back(interval);
+            }else {
+                merged.back()[1] = max(merged.back()[1],interval[1]);
             }
-            
-            if(!pushed && endNew<intervals[i][0]){
-                result.push_back({startNew,endNew});
-                pushed= true;
-            }
-            
-             result.push_back(intervals[i]);
-            
         }
-        if(!pushed) result.push_back({startNew,endNew});
-        return result;
+        return merged;
     }
 };
