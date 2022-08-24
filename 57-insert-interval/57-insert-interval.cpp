2e6f37fbd94aa940dc277f7fec7aa70b6fb1561e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-24 10:46:34 -0700

    Time: 31 ms (37.39%), Space: 17.1 MB (37.93%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -3,16 +3,26 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> merged;
         
-        // Only change comapred to merge intervals
-        intervals.push_back(newInterval);
+        int index = 0;
         
-        sort(intervals.begin(),intervals.end());
-        for(auto interval:intervals){
-            if(merged.empty() || merged.back()[1] < interval[0]){
-                merged.push_back(interval);
+        while(index<intervals.size() && intervals[index][0] < newInterval[0]){
+            merged.push_back(intervals[index++]);
+        }
+        
+        if(merged.empty() || newInterval[0] > merged.back()[1]){
+            merged.push_back(newInterval);
+        }else {
+            merged.back()[0] = min(merged.back()[0],newInterval[0]);
+            merged.back()[1] = max(merged.back()[1],newInterval[1]);
+        }
+
+        for(int i=index;i<intervals.size();i++){
+            if(merged.empty() || merged.back()[1] < intervals[i][0]){
+                merged.push_back(intervals[i]);
             }else {
-                merged.back()[1] = max(merged.back()[1],interval[1]);
+                merged.back()[1] = max(merged.back()[1],intervals[i][1]);
             }
+
         }
         return merged;
     }
