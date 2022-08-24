7d90aa288b82c20b1cfbfb16b1c27b2aa0b9771d
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-23 19:20:06 -0700

    Time: 35 ms (94.02%), Space: 18.2 MB (96.26%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string,int> mapper;
+        int index=0;
+        for(int i=0;i<strs.size();i++){
+            string newS = strs[i];
+            // cout<<strSort(newS)<<endl;
+            sort(newS.begin(),newS.end());
+            auto it = mapper.find(newS);
+            if(it == mapper.end()){
+                mapper[newS] = index;
+                res.push_back({strs[i]});
+                index++;
+            }else {
+                res[it->second].push_back(strs[i]);
+            }
+        }
+        return res;
+    }
+};
