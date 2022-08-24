264051f2321b9517daff504073afd78fc077acfc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-24 10:01:48 -0700

    Time: 51 ms (9.75%), Space: 17.7 MB (11.32%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> merged;
+        
+        // Only change comapred to merge intervals
         intervals.push_back(newInterval);
+        
         sort(intervals.begin(),intervals.end());
         for(auto interval:intervals){
             if(merged.empty() || merged.back()[1] < interval[0]){
