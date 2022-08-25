13afb4eb303a8f977cb3a63434be895b269805ed
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-25 07:11:39 -0700

    Time: 4 ms (78.71%), Space: 9.4 MB (95.23%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        return searchMatrixUsingBinarySearch(matrix,target);
+    }
+    
+    bool searchMatrixSimple(vector<vector<int>>& matrix, int target) {
         int row=0,col=0;
         bool moveDown=true;
         bool moveRight=false;
@@ -13,6 +17,23 @@ public:
                 moveRight=true;
                 col++;
             }                                                       
+        }
+        return false;
+    }
+    
+    bool searchMatrixUsingBinarySearch(vector<vector<int>>& matrix, int target){
+        int maxR = matrix.size();
+        int maxC = matrix[0].size();
+        
+        int low= 0;
+        int high= maxR*maxC-1;
+        
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int value = matrix[mid/maxC][mid%maxC];
+            if(target==value) return true;
+            else if(target<value) high=mid-1;
+            else low=mid+1;
         }
         return false;
     }
