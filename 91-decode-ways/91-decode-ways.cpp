c2d4a37047717fb82c8fce6d4978dece00634e87
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-25 09:20:40 -0700

    Time: 8 ms (18.42%), Space: 7.5 MB (17.64%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     vector<int> dp;
-    unordered_map<int, int> memo;
     int numDecodings(string s) {
         dp.resize(s.size()+1,-1);
         return rec(s,0);
     }
     
     int rec(string s,int index){
-//          if (memo.find(index) != memo.end()) {
-//              cout<<index<<endl;
-//             return memo[index];
-//         }
-        
         if(dp[index]!=-1) {
-            cout<<index<<endl;
             return dp[index];
         }
-        
+
         if(index==s.size()) return 1;
         if(s[index]=='0') return 0;
         if(index==s.size()-1) return 1; 
@@ -28,7 +21,6 @@ public:
         }
         
         dp[index]=res;
-        memo[index] = res;
         return res;
     }
 };
