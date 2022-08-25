b91030f5444d87e370cba0d663a0f842eba61692
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-25 09:15:23 -0700

    Time: 1790 ms (5.17%), Space: 357.3 MB (5.37%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -8,15 +8,15 @@ public:
     }
     
     int rec(string s,int index){
-         if (memo.find(index) != memo.end()) {
-             cout<<index<<endl;
-            return memo[index];
-        }
-        
-        // if(dp[index]!=0) {
-        //     cout<<index<<endl;
-        //     return dp[index];
+        //  if (memo.find(index) != memo.end()) {
+        //      cout<<index<<endl;
+        //     return memo[index];
         // }
+        
+        if(dp[index]!=0) {
+            cout<<index<<endl;
+            return dp[index];
+        }
         
         if(index==s.size()) return 1;
         if(s[index]=='0') return 0;
