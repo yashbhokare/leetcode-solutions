ed2424af5a9439b338b23f5e8dcc93047cb6d6bf
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-26 08:27:24 -0700

    Time: 2011 ms (5.08%), Space: 6 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -27,7 +27,7 @@ public:
     
     bool isPowerOfTwo(int n) {
         if (n == 0) return false;
-        while (n % 2 == 0) n /= 2;
-        return n == 1;
-    }
+        long x = n;
+        return (x & (-x)) == x;
+  }
 };
