23145b90f8ba33fc772872765656603ff5954459
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-26 08:22:50 -0700

    Time: 1000 ms (5.08%), Space: 6 MB (68.93%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string val = to_string(n);
+        return permutations(val,0);
+        return true;
+    }
+    
+    bool permutations(string s,int index){
+        if(s[0]=='0') return false;
+
+        if(index==s.size()){
+            // cout<<s<<endl;
+            if(isPowerOfTwo(stoi(s))==1){
+                return true;
+            }
+            return false;
+        }
+        
+        for(int i=index;i<s.size();i++){
+            swap(s[i],s[index]);
+            if(permutations(s,index+1)) return true;
+            swap(s[i],s[index]);
+        }
+        return false;
+    }
+    
+    bool isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        while (n % 2 == 0) n /= 2;
+        return n == 1;
+    }
+};
