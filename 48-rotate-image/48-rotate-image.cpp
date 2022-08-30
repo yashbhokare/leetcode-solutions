03c7f65db7ecf61dace3bcfdf520e02b43510fef
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-30 15:35:01 -0700

    Time: 7 ms (17.41%), Space: 7.1 MB (73.72%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -22,3 +22,62 @@ public:
         }
     }
 };
+
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         for(int i=0;i<(n+1)/2;i++){
+//             for(int j=0;j<n/2;j++){
+//                 int temp = matrix[n-j-1][i];
+//                 matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+//                 matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+//                 matrix[j][n-i-1] = matrix[i][j];
+//                 matrix[i][j] = temp;
+//             }
+//         }
+//     }
+// };
+
+
+    
+//     void rotateCalculations(vector<vector<int>>& matrix){
+//         // displayMatrix(matrix);
+//         int matrixLength = matrix.size();
+//         int count = 0;
+//         int rowIndex = 0, columnIndex =0;
+//         int n = matrix.size() - 1;
+//         while(count< matrix.size()*matrix.size()){
+//             int maxTraversal = matrixLength - 1;
+//             if(maxTraversal == 0){
+//                 break;
+//             }
+//             int index =0;
+//             while(maxTraversal !=0){
+//                 int temp = matrix[n-index-rowIndex][columnIndex];
+//                 matrix[n-index-rowIndex][columnIndex] = matrix[n-rowIndex][n-columnIndex-index];
+//                 matrix[n-rowIndex][n-columnIndex-index] = matrix[rowIndex+index][n-columnIndex];
+//                 matrix[rowIndex+index][n-columnIndex] = matrix[rowIndex][columnIndex+index];
+//                 matrix[rowIndex][columnIndex+index] = temp;
+//                 maxTraversal--;
+//                 index++;
+//                 count = count + 4; //At each iteration 4 positions are getting updated
+//                 // displayMatrix(matrix);
+//                 // cout<<"Count:"<<count<<endl;
+//             }
+//             rowIndex++;
+//             columnIndex++;
+//             matrixLength = matrixLength -2;
+//             // cout<<"MatrixLength:"<<matrixLength<<endl;
+//         }
+//     }
+    
+//     void displayMatrix(vector<vector<int>> matrix){
+//         for(int i =0; i< matrix.size();i++){
+//             for(int j=0;j<matrix.size();j++){
+//                 cout<<matrix[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//         cout<<"------------------"<<endl;
+//     }
