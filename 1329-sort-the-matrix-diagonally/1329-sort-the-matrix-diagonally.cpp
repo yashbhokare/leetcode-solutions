4bf279aa14eb864dd6086f1fe0ade388186709ef
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-08-31 06:55:05 -0700

    Time: 28 ms (14.91%), Space: 9.4 MB (29.61%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        // Store the matrix dimensions
-        size_t m = mat.size();
-        size_t n = mat[0].size();
+        int rowMax = mat.size();
+        int colMax = mat[0].size();
 
-        // create a hasmap of min-heaps to store the diagonals
-        map<int, priority_queue<int, vector<int>, greater<int>>> diagonals;
-
-        // Insert the values into the Hash Map
-        for (size_t row = 0; row < m; row++) {
-            for (size_t col = 0; col < n; col++) {
-                // Observing that on each diagonal, the differences between the row and column
-                // indices of each element is the same.
-                // Hence we can use row - col as the key to collect elements on the same diagonal.
-                diagonals[row - col].push(mat[row][col]);
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>> mapper;
+        // priority_queue<int,vector<int>,greater()> minHeap;
+        
+        for(int r=0;r<rowMax;r++){
+            for(int c=0;c<colMax;c++){
+                mapper[r-c].push(mat[r][c]);
             }
         }
-
-        // Take values back out of the Hash Map
-        for (size_t row = 0; row < m; row++) {
-            for (size_t col = 0; col < n; col++) {
-                mat[row][col] = diagonals[row - col].top();
-                diagonals[row - col].pop();
+        
+        for(int r=0;r<rowMax;r++){
+            for(int c=0;c<colMax;c++){
+                mat[r][c] = mapper[r-c].top();
+                mapper[r-c].pop();
+                // mapper[r-c].push(mat[r][c]);
             }
         }
-
         return mat;
     }
 };
