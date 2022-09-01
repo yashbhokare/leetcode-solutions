7c6cf5fb003af550387ff332a977f92f1c0393d7
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-01 13:18:10 -0700

    Time: 146 ms, Memory: 26 MB - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -3,11 +3,32 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(i==0 || nums[i]!=nums[i-1]){
-                vector<vector<int>> currRes = twoSumSorted(nums,i+1,-nums[i]);
+        return kthSum(nums,3,0,0);
+//         for(int i=0;i<nums.size();i++){
+//             if(i==0 || nums[i]!=nums[i-1]){
+//                 vector<vector<int>> currRes = twoSumSorted(nums,i+1,-nums[i]);
+//                 for(auto val:currRes){
+//                     val.push_back(nums[i]);
+//                     ans.push_back(val);
+//                 }
+//             }
+           
+//         }
+//         return ans;
+    }
+    
+    vector<vector<int>> kthSum(vector<int>& nums, int k, int index, int target){
+        if(index>=nums.size()) return {};
+        
+        if(k==2){
+            return twoSumSorted(nums,index,target);
+        }
+        
+        vector<vector<int>> ans;
+        for(int i=index;i<nums.size();i++){
+            if(i==index || nums[i]!=nums[i-1]){
+                vector<vector<int>> currRes = kthSum(nums,k-1,i+1, target-nums[i]);
                 for(auto val:currRes){
-                    // cout<<val.size();
                     val.push_back(nums[i]);
                     ans.push_back(val);
                 }
@@ -15,6 +36,7 @@ public:
            
         }
         return ans;
+        
     }
     
     vector<vector<int>> twoSumSorted(vector<int>& nums,int start, int target){
