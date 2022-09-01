55fb5d980ab26b9d70124d930a35acdf7242d5b8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-01 13:06:17 -0700

    Time: 261 ms, Memory: 25.9 MB - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -3,46 +3,44 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        // return twoSum(nums,0,1);
         for(int i=0;i<nums.size();i++){
-            if(i!=0 && nums[i]==nums[i-1] ){
-                continue;
+            if(i==0 || nums[i]!=nums[i-1]){
+                vector<vector<int>> currRes = twoSumSorted(nums,i+1,-nums[i]);
+                for(auto val:currRes){
+                    // cout<<val.size();
+                    val.push_back(nums[i]);
+                    ans.push_back(val);
+                }
             }
-            int target = -nums[i];
-            vector<vector<int>> res = twoSum(nums,i+1,target);
-            for(int k=0;k<res.size();k++){
-                // Append the target to given result
-                res[k].push_back(nums[i]);
-                // Append the total pair to final ans array
-                ans.push_back(res[k]);
-            }
+           
         }
         return ans;
     }
     
-    vector<vector<int>> twoSum(vector<int>& nums,int start,int target){
+    vector<vector<int>> twoSumSorted(vector<int>& nums,int start, int target){
         vector<vector<int>> result;
-        int lastPos = nums.size()-1;
-        int startPos = start;
-        while(startPos<lastPos){
-            if(startPos!=start && nums[startPos]==nums[startPos-1]){
-                startPos++;
+        int low = start;
+        int high = nums.size()-1;
+        while(low<high){
+            int total = nums[low]+nums[high];
+            
+            if(low!=start && nums[low]==nums[low-1]){
+                low++;
                 continue;
             }
-            if(lastPos!=nums.size()-1 && nums[lastPos]==nums[lastPos+1]){
-                lastPos--;
+            if(high!=nums.size()-1 && nums[high]==nums[high+1]){
+                high--;
                 continue;
             }
-            int sum = nums[startPos]+nums[lastPos];
-            if(sum==target){
-                result.push_back({nums[startPos],nums[lastPos]});
-                startPos++;
-                lastPos--;
-            }
-            else if(sum<target){
-                startPos++;
+            
+            if(total==target){
+                result.push_back({nums[low],nums[high]});
+                low++;
+                high--;
+            }else if(total>target){
+                high--;
             }else {
-                lastPos--;
+                low++;
             }
         }
         return result;
