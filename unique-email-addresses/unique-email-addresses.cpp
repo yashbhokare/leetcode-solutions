80c6c2d81b7dbf516fe0d2eea9768e605578e051
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-01 06:33:44 -0700

    Time: 35 ms, Memory: 15.9 MB - LeetHub

--- unique-email-addresses/unique-email-addresses.cpp
+++ unique-email-addresses/unique-email-addresses.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
         unordered_set<string> set;
-        for(auto email:emails){
-            int domainIndex = email.find_first_of('@');
-            string domain = email.substr(domainIndex);
-            string localName = email.substr(0,domainIndex);
-            string formattedLocalName = formatLocalName(localName);
-            set.insert(formattedLocalName+domain);
+        for(string email:emails){
+            int indexforDomain = email.find_first_of('@');
+            string domain = email.substr(indexforDomain);
+            string localName = email.substr(0,indexforDomain);
+            string filteredLocalName = "";
+            for(char ch:localName){
+                if(ch=='+') break;
+                if(ch=='.') continue;
+                filteredLocalName+=ch;
+            }
+            string result = filteredLocalName + domain;
+            set.insert(result);
         }
         return set.size();
-    }
-    
-    string formatLocalName(string localName){
-        string newLocalName = "";
-        for(auto c:localName){
-            if(c=='+') return newLocalName;
-            if(c=='.') continue;
-            newLocalName+=c;
-        }
-        return newLocalName;
     }
 };
