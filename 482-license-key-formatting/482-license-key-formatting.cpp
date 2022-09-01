026f35145e4b3f61984e34c5b59a67b41fa38ce3
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-01 09:15:19 -0700

    Time: 18 ms (23.09%), Space: 8.8 MB (19.60%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.cpp
+++ 482-license-key-formatting/482-license-key-formatting.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    // string licenseKeyFormatting(string s, int k) {
-    //     string result = "";
-    //     int index = s.size()-1;
-    //     int count=0;
-    //     reverse(s.begin(),s.end());
-    //     for(int i=0;i<s.size();i++){
-    //         if(s[i]=='-') continue;
-    //         if(count==k){
-    //             result.push_back('-');
-    //             count=0;
-    //         }
-    //        result.push_back(toupper(s[i]));
-    //        count++;
-    //     }
-    //     reverse(result.begin(),result.end());
-    //     return result;
-    // }
     string licenseKeyFormatting(string s, int k) {
-        string ss=""; //string without '-'
-        for(auto c: s){
-            if(c=='-'){}
-            else{
-                if(c>96) c-=32;  //transform to upper (ASCII ->'a'=97,'A'=65)
-              ss+=c;  
-            } 
+        string result = "";
+        string formatS = "";
+        
+        for(auto ch:s){
+            if(ch=='-') continue;
+            formatS+=toupper(ch);
         }
-        s="";
-        reverse(ss.begin(),ss.end());
-        int cnt=0;
-        for(auto c: ss)
-        {
-            if(cnt==k) {s.push_back('-'); cnt=0;}  // add '-'
-            s.push_back(c);
-            cnt++;
+        
+        int count=0;
+        
+        reverse(formatS.begin(),formatS.end());
+        
+        for(int i=0;i<formatS.size();i++){
+            if(count==k){
+                result.push_back('-');
+                count=0;
+            }
+           result.push_back(formatS[i]);
+           count++;
         }
-        reverse(s.begin(),s.end()); //reverse the result.
-        return s;
+        reverse(result.begin(),result.end());
+        return result;
     }
+
 };
