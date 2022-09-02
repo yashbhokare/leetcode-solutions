9beda2dc8e146f7a988f827fc42b39f94847e5ed
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-02 16:56:39 -0700

    Time: 201 ms, Memory: 17.9 MB - LeetHub

--- maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
-        int ans =0;
+        int result = 0;
         int n = seats.size();
-        vector<int> left(n,n-1);
-        vector<int> right(n,n-1);
-        for(int i=0;i<n;i++){
-            if(seats[i]==1) left[i]=0;
-            else if(i>0) left[i] = left[i-1]+1;
-        }
-        for(int i=n-1;i>=0;i--){
-            if(seats[i]==1) right[i]=0;
-            else if(i<n-1) right[i] = right[i+1]+1;
-        }
-        for(int i=0;i<n;i++){
-            if(seats[i]==0){
-                ans= max(ans,min(left[i],right[i]));
+        
+        if(n==0) return result;
+        
+        vector<int> left(n,INT_MAX);
+        vector<int> right(n,INT_MAX);
+        
+        if(seats[0]==1) left[0]=0;
+        for(int i=1;i<n;i++){
+            if(seats[i]==1){
+                left[i]=0;
+            }else if(left[i-1]!=INT_MAX){
+                left[i] = left[i-1]+1;
             }
         }
-        return ans;
+        
+        if(seats[n-1]==1) right[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            if(seats[i]==1){
+                right[i]=0;
+            }else if(right[i+1]!=INT_MAX){
+                right[i] = right[i+1]+1;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            cout<<left[i]<<" "<<right[i]<<endl;
+            int minVal = min(left[i],right[i]);
+            if(minVal==INT_MAX) continue;
+            result = max(minVal,result);
+        }
+        return result;
     }
 };
