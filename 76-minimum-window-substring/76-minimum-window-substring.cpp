9e1a14f5fc337541f33c1a543cbb980bc8932684
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-02 07:05:07 -0700

    Time: 25 ms (66.60%), Space: 7.9 MB (50.93%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,45 +1,55 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        
         unordered_map<char,int> targetMapper;
-        unordered_map<char,int> subStringMapper;
+        unordered_map<char,int> currMapper;
+        int left=0,right=0;
+        int n = s.size();
+        int minLen = INT_MAX;
+        int startIndex = -1;
+        int tMapSize = 0;
         
-        for(auto ch:t) targetMapper[ch]++;
+        if(t.size()==0) return "";
         
-        int targetMapSize = targetMapper.size();
+        for(auto c:t){
+            targetMapper[c]++;
+        }
         
-        int left=0;
-        int right=0;
-        int maxLen = s.size();
-        int subStringMapSize = 0;
-        int resultPos = 0;
-        int resultSize = INT_MAX;
-        while(right<maxLen){
-              
-            if(targetMapper.find(s[right])!=targetMapper.end()){
-                subStringMapper[s[right]]++;
-                if(subStringMapper[s[right]]==targetMapper[s[right]]){
-                    targetMapSize--;
-                }
-            }
+        tMapSize = targetMapper.size();
+        
+        while(right<n){
             
-            while(targetMapSize==0 && left<maxLen){
-                if(right-left+1<resultSize){
-                    resultSize = right-left+1;
-                    resultPos = left;
-                }
+            char curr = s[right];
+            if(targetMapper.find(curr)!=targetMapper.end()){
+                currMapper[curr]++;
+                if(currMapper[curr]==targetMapper[curr]) tMapSize--;
+            }
+            while(tMapSize==0){
                 
-                if(targetMapper.find(s[left])!=targetMapper.end()){
-                    subStringMapper[s[left]]--;
-                    if(subStringMapper[s[left]]<targetMapper[s[left]]){
-                        targetMapSize++;
-                    }
+                if(right-left+1<minLen){
+                    minLen = right-left+1;
+                    startIndex = left;
+                }
+                char leftCurr = s[left];
+                if(currMapper.find(leftCurr)!=currMapper.end()){
+                    currMapper[leftCurr]--;
+                    if(currMapper[leftCurr]<targetMapper[leftCurr]) tMapSize++;
                 }
                 left++;
             }
             right++;
         }
-        if(resultSize==INT_MAX) return "";
-        return s.substr(resultPos,resultSize);
+        if(startIndex==-1) return "";
+        
+        return s.substr(startIndex,minLen);
+        
+        
     }
 };
+
+// For more optimization use a filtered string filtered_S
+
+ // S = "ABCDDDDDDEEAFFBC" T = "ABC"
+ //   filtered_S = [(0, 'A'), (1, 'B'), (2, 'C'), (11, 'A'), (14, 'B'), (15, 'C')]
+ //  Here (0, 'A') means in string S character A is at index 0.
