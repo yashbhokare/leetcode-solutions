071cf6268dbcfd9957083019fd8de7b9892fd068
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-02 13:18:47 -0700

    Time: 7 ms, Memory: 10.7 MB - LeetHub

--- find-and-replace-in-string/find-and-replace-in-string.cpp
+++ find-and-replace-in-string/find-and-replace-in-string.cpp
@@ -1,47 +1,48 @@
-// class Solution {
-// public:
-//     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-//         int index=0;
-//         map<int,pair<int,string>> mapper;
-//         for(int i=0;i<indices.size();i++){
-//             index = indices[i];
-//             if(s.substr(index,sources[i].size())==sources[i]){
-//                 mapper[index] = {sources[i].size(),targets[i]};
-//             }
-//         }
-//         index = 0;
-//         for(auto it=mapper.begin();it!=mapper.end();it++){
-//             int currIndex= it->first + index;
-//             s.replace(currIndex,it->second.first,it->second.second);
-//             index+= it->second.second.size()-it->second.first;
-//         }
-        
-//         return s;
-//     }
-// };
-
 class Solution {
 public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        vector<pair<int, int>> indicesInDescendingOrder;
-        for(int i = 0; i < indices.size(); i++){
-            indicesInDescendingOrder.push_back({indices[i], i});
-        }
-        
-        sort(indicesInDescendingOrder.rbegin(), indicesInDescendingOrder.rend());
-        
-        for(int i = 0; i < indicesInDescendingOrder.size(); i++){
-            int indicesIndex = indicesInDescendingOrder[i].first;
-            int sourcesIndex = indicesInDescendingOrder[i].second;
-            
-            string sourcesStr = sources[sourcesIndex];
-            if(s.substr(indicesIndex, sourcesStr.size()) == sourcesStr){
-                 s.replace(indicesIndex,sourcesStr.size(),targets[sourcesIndex]);
-                
-//                 s = s.substr(0, indicesIndex) + targets[sourcesIndex] + s.substr(indicesIndex + sourcesStr.size());
+        int index=0;
+        map<int,pair<int,string>> mapper;
+        for(int i=0;i<indices.size();i++){
+            index = indices[i];
+            if(s.substr(index,sources[i].size())==sources[i]){
+                mapper[index] = {sources[i].size(),targets[i]};
             }
+        }
+        index = 0;
+        for(auto it=mapper.begin();it!=mapper.end();it++){
+            int currIndex= it->first + index;
+            s.replace(currIndex,it->second.first,it->second.second);
+            index+= it->second.second.size()-it->second.first;
         }
         
         return s;
     }
 };
+
+//https://leetcode.com/problems/find-and-replace-in-string/discuss/2076687/Sort-in-Descending-Order-and-traverse-C%2B%2B
+
+// Good Solution to replace in reverse order
+// class Solution {
+// public:
+//     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
+//         vector<pair<int, int>> indicesInDescendingOrder;
+//         for(int i = 0; i < indices.size(); i++){
+//             indicesInDescendingOrder.push_back({indices[i], i});
+//         }
+        
+//         sort(indicesInDescendingOrder.rbegin(), indicesInDescendingOrder.rend());
+        
+//         for(int i = 0; i < indicesInDescendingOrder.size(); i++){
+//             int indicesIndex = indicesInDescendingOrder[i].first;
+//             int sourcesIndex = indicesInDescendingOrder[i].second;
+            
+//             string sourcesStr = sources[sourcesIndex];
+//             if(s.substr(indicesIndex, sourcesStr.size()) == sourcesStr){
+//                  s.replace(indicesIndex,sourcesStr.size(),targets[sourcesIndex]);
+//             }
+//         }
+        
+//         return s;
+//     }
+// };
