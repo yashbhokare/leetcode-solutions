61585f3c108fe5ae810be64061f05aea3ccb7fd9
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-01 17:48:32 -0700

    Time: 4 ms, Memory: 7 MB - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
+    int nR,nC;
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for(int i=0;i<(n+1)/2;i++){
-            for(int j=0;j<n/2;j++){
-                int temp = matrix[n-j-1][i];
-                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-                matrix[j][n-i-1] = matrix[i][j];
-                matrix[i][j] = temp;
+        nR = matrix.size();
+        nC = matrix[0].size();
+        transpose(matrix);
+        mirror(matrix);
+    }
+    
+    void transpose(vector<vector<int>>& matrix){
+        for(int r=0;r<nR;r++){
+            for(int c=r;c<nC;c++){
+                swap(matrix[r][c],matrix[c][r]);
+            }
+        }
+    }
+    
+    void mirror(vector<vector<int>>& matrix){
+        for(int r=0;r<nR;r++){
+            for(int c=0;c<nC/2;c++){
+                swap(matrix[r][c],matrix[r][nC-1-c]);
             }
         }
     }
