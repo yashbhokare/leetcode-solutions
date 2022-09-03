4d383a085ae4c91e7cceccdb2084d2663652e1cc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-03 10:45:07 -0700

    Time: 529 ms, Memory: 75.8 MB - LeetHub

--- multiply-strings/multiply-strings.cpp
+++ multiply-strings/multiply-strings.cpp
@@ -1,49 +1,51 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        // vector<int> result(num1.size()+num2.size(),0);
-        int n1Max = num1.size()-1;
-        int n2Max = num2.size()-1;
+        reverseTheString(num1);
+        reverseTheString(num2);
+        
         string result = "";
-        for(int i=n1Max,m=0;i>=0;i--,m++){
-            string val = "";
+        string zeros = "";
+        for(int n1=0;n1<num1.size();n1++){
+            if(n1>0) zeros.push_back('0');
             int carry = 0;
-            int k=m;
-            for(int h=0;h<m;h++){
-                val = val + "0";
+            int val1 = num1[n1]-'0';
+            string total = ""+zeros;
+            
+            for(int n2=0;n2<num2.size();n2++){
+                int val2 = num2[n2] - '0';
+                int mul = val1*val2+carry;
+                total = total + to_string(mul%10);
+                carry = mul/10;
             }
-            for(int j=n2Max;j>=0;j--,k++){
-                int cal = (num1[i]-'0')*(num2[j]-'0') + carry;
-                val = val + to_string(cal%10);
-                carry = cal/10;
-            }
-            if(carry>0)
-                val = val + to_string(carry);
-            result =addString(result,val);
+            if(carry) total = total + to_string(carry);
+            result=additionOfStrings(result,total);
         }
-        reverse(result.begin(),result.end());
-        int zeroIndex = result.find_first_not_of('0');
-        if(zeroIndex==-1) return "0";
-        result = result.substr(zeroIndex);
+        reverseTheString(result);
         return result;
     }
     
-    string addString(string s1,string s2){
-        if(s1=="") return s2;
-        int carry = 0;
-        string result = "";
-        for(int i=0;i<s1.size() || i<s2.size();i++){
-            int val1 = i<s1.size() ? s1[i]-'0' : 0;
-            int val2 = i<s2.size() ? s2[i]-'0' : 0;
-            int val =val1+val2+carry;
-            result = result + to_string(val%10);
-            carry = val/10;
-            // cout<<result<<endl;
+    void reverseTheString(string& s){
+         reverse(s.begin(),s.end());
+    }
+    
+    string additionOfStrings(string s1,string s2){
+        string res = "";
+        int maxSize = max(s1.size(),s2.size());
+        int index = 0;
+        int carry=0;
+        bool isZeros = true;
+        while(index!=maxSize){
+            int val1= index < s1.size() ? s1[index]-'0' : 0;
+            int val2= index < s2.size() ? s2[index]-'0' : 0;
+            int add = val1+val2+carry;
+            res = res + to_string(add%10);
+            carry = add/10;
+            if(add%10 || carry) isZeros=false;
+            index++;
         }
-        // cout<<result<<endl;
-        if(carry!=0){
-            result = result + to_string(carry);
-        }
-        return result;
+        if(carry) res = res + to_string(carry);
+        if(isZeros) return "0";
+        return res;
     }
 };
