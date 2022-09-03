e0857d537415c93dce131cfed29e9830e8bae185
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-02 17:01:04 -0700

    Time: 24 ms, Memory: 18.1 MB - LeetHub

--- maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -6,29 +6,23 @@ public:
         
         if(n==0) return result;
         
-        vector<int> left(n,INT_MAX);
-        vector<int> right(n,INT_MAX);
+        vector<int> left(n,n-1);
+        vector<int> right(n,n-1);
         
         if(seats[0]==1) left[0]=0;
         for(int i=1;i<n;i++){
-            if(seats[i]==1){
-                left[i]=0;
-            }else if(left[i-1]!=INT_MAX){
-                left[i] = left[i-1]+1;
-            }
+            if(seats[i]==1)left[i]=0;
+            else left[i] = left[i-1]+1;
         }
         
         if(seats[n-1]==1) right[n-1]=0;
         for(int i=n-2;i>=0;i--){
-            if(seats[i]==1){
-                right[i]=0;
-            }else if(right[i+1]!=INT_MAX){
-                right[i] = right[i+1]+1;
-            }
+            if(seats[i]==1) right[i]=0;
+            else right[i] = right[i+1]+1;
         }
         
         for(int i=0;i<n;i++){
-            cout<<left[i]<<" "<<right[i]<<endl;
+            // cout<<left[i]<<" "<<right[i]<<endl;
             int minVal = min(left[i],right[i]);
             // if(minVal==INT_MAX) continue;
             result = max(minVal,result);
