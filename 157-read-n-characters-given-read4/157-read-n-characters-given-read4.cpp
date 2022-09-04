a71264418c437f63fd9667bbc44db830150d9190
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-03 17:33:52 -0700

    Time: 0 ms (100.00%), Space: 6.4 MB (24.02%) - LeetHub

---
+++ 157-read-n-characters-given-read4/157-read-n-characters-given-read4.cpp
@@ -0,0 +1,33 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char *buf4);
+ */
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+       
+        int index = 0;
+        
+        while(n>=0){
+            int newIndex=0;
+            char buf4[4];
+            int v= read4(buf4);
+            while(n>0 && newIndex<v){
+                buf[index] = buf4[newIndex];
+                index++;
+                newIndex++;
+                n--;
+            }
+            if(v<4){
+                break;
+            }
+        }
+        return index;
+    }
+};
