b3c853f843c830df17f679c86d032c389c37eaab
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-05 08:12:54 -0700

    Time: 205 ms (5.10%), Space: 32.3 MB (98.68%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.cpp
+++ 359-logger-rate-limiter/359-logger-rate-limiter.cpp
@@ -1,28 +1,20 @@
 class Logger {
 public:
     unordered_map<string,int> mapper;
-    int index=0;
     Logger() {
         mapper = {};
     }
     
     bool shouldPrintMessage(int timestamp, string message) {
-        // cout<<
-        index++;
-        
         if(mapper.find(message)!=mapper.end()){
-            // cout<<index<<". "<<message<<" "<<timestamp<<"   "<<mapper[message]<<endl;
             if(timestamp>=mapper[message]){
                 mapper[message]=timestamp+10;
-                // cout<<index<<". "<<message<<"\t"<<timestamp<<"\t"<<mapper[message]<<endl;
                 return true;
             }else {
-                // cout<<index<<". "<<message<<"\t"<<timestamp<<"\t"<<mapper[message]<<endl;
                 return false;
             }
         }
         mapper[message]=timestamp+10;
-        // cout<<index<<". "<<message<<"\t"<<timestamp<<"\t"<<mapper[message]<<endl;
         return true;
     }
 };
