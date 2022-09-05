5f13fc21671283dbf4c992b4ecb8968ccb633c97
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-04 17:09:52 -0700

    Time: 4 ms, Memory: 10.8 MB - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,79 +11,20 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-
-    if(head == NULL) return head;
-    
-    ListNode* temp = new ListNode(0);
-    temp->next = head;
-    head= temp;
-    
-    ListNode* slow = head;
-    ListNode* fast = head;
-    for(int i=0;i<n;i++){
-        fast = fast->next;
-    }
-     while(fast->next != NULL){
-         slow = slow->next;
-         fast=fast->next;
-     } 
-     slow->next = slow->next->next;
-    return head->next;
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* slow = dummyHead;
+        ListNode* fast = dummyHead;
+        
+        for(int i=0;i<n;i++){
+            fast = fast->next;
+        }
+        while(fast->next){
+            slow = slow->next;
+            fast=fast->next;
+        }
+        slow->next= slow->next!=NULL ? slow->next->next : NULL;
+        return dummyHead->next;
+        
     }
 };
-
-//     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-//         int total = 0;
-        
-//         if(head == NULL) return head;    
-//         ListNode* temp = new ListNode(0);
-//         temp->next = head;
-//         head = temp;
-        
-//         temp = head;
-        
-//         while(temp->next!=NULL){
-//             total++;
-//             temp=temp->next;
-//         }
-//         temp = head;
-//         for(int i=1;i<total-n+1;i++){
-//             temp= temp->next;
-//         }
-//         temp->next = temp->next->next;
-        
-//         return head->next;
-//     }
-//     ListNode* removeNthFromEnd(ListNode* head, int n) {
-//         ListNode *start = head;
-//         int totalCount =1;
-//         while(start->next != nullptr){
-//             totalCount++;
-//             start=start->next;
-//         }
-//         if(totalCount==1){
-//             head= nullptr;
-//             return head;
-//         }
-       
-//         int pos = totalCount - n + 1;
-        
-//         // Change Head location
-//         if(pos==1){
-//             head = head->next;
-//             return head;
-//         }
-//         ListNode *temp = head;
-//         while(pos>2){
-//             temp=temp->next;
-//             pos--;
-//         }
-//         cout<<temp->val;
-//         if(totalCount - n + 1 == totalCount){
-//             temp->next = nullptr;
-//             return head;
-//         }
-//         temp->next = temp->next->next;
-//         return head;
-//     }
