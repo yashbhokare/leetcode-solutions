3608283dd25d8d3b8abf8645b0e6ee3c1b7ce0d2
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-07 07:27:08 -0700

    Time: 856 ms (36.62%), Space: 131.7 MB (77.45%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -30,39 +30,51 @@
 
 // };
 
+
 class Solution {
 public:
+    vector<int>par;
+    int n;
     
-    int InsertInterval(map<int, int> &mp, int start, int end) 
-    {
-        auto up_it = mp.upper_bound(end);
-        auto lo_it = std::prev(mp.lower_bound(start));
-        
-        lo_it = lo_it->second < start ? std::next(lo_it) : lo_it;              // First overlapping interval
-
-        int paint = end - start;        
-        for(auto it = lo_it; it != up_it; ++it) {
-            paint -= std::min(end, it->second) - std::max(start, it->first);   // Substract overlapping interval from the paint
-            
-            start = std::min(start, it->first);                                // Update new interval to be stored in the map
-            end   = std::max(end  , it->second);
-        }
-           
-        mp.erase(lo_it, up_it);                                               // Remove overlapping intervals
-        
-        mp[start] = end;                                                      // Insert new interval
-        return paint;
+    void make_set(int v){
+        par[v] = v;
     }
     
+    int find_set(int v){
+        if(par[v]==v)
+            return v;
+        return par[v] = find_set(par[v]);
+    }
+    
+    void union_set(int u, int v){
+        u = find_set(u);
+        v = find_set(v);
+        
+        par[u] =v;
+    }
     
     vector<int> amountPainted(vector<vector<int>>& paint) {
-        map<int, int> mp;
-        mp[INT_MIN] = INT_MIN;           // Insert dummy Interval
-        mp[INT_MAX] = INT_MAX;
+        int maxlen = 0;
+        int minlen = INT_MAX;
+        for(auto& v:paint){
+            minlen = min(minlen, v[0]);
+            maxlen = max(maxlen, v[1]);
+        }
+        par.resize(maxlen+2);
         
-        vector<int> ans;
-        for(auto p : paint) {
-            ans.push_back(InsertInterval(mp, p[0], p[1]));
+        for(int i=minlen; i<=maxlen+1; i++){
+            make_set(i);
+        }
+        n = paint.size();
+        vector<int>ans(n,0);
+        
+        for(int i=0; i< paint.size(); i++){
+            int l = paint[i][0]; int r = paint[i][1];
+            
+            for(l=find_set(l); l<r; l=find_set(l)){
+                ans[i]++;
+                union_set(l, l+1);
+            }
         }
         
         return ans;
