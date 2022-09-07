fbe8858622e6e18a3f4a8f395b470239106dbc86
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-07 07:25:59 -0700

    Time: 1358 ms (8.10%), Space: 180.6 MB (17.80%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -1,66 +1,70 @@
-class Solution {
-public:
-   
-    vector<int> workDone;
-    unordered_map<int,int> mapper;
-    vector<int> amountPainted(vector<vector<int>>& paint) {
-        for(auto p:paint){
-            workDone.push_back(calAmountPainted(p[0],p[1]));
-        }
-        return workDone;
-    }
-    
-    int calAmountPainted(int start, int end){
-        int work=0;
-        while(start<end){
-            // start not found
-            if(mapper.find(start)==mapper.end()){
-                mapper[start]=end;
-                work++;
-                start++;
-            }else {
-                int newStart = mapper[start];
-                mapper[start] = max(mapper[start],end);
-                start = newStart;
-            }
-        }
-        return work;
-    }
-
-
-};
-
 // class Solution {
 // public:
+   
+//     vector<int> workDone;
+//     unordered_map<int,int> mapper;
 //     vector<int> amountPainted(vector<vector<int>>& paint) {
-//         // Lets create a map that stores where we started and where we ended
-//         vector<int> res;
-//         unordered_map<int, int> m;
-        
-//         for(int i = 0; i < paint.size(); ++i)
-//         {
-//             int work = 0;
-//             int start = paint[i][0];
-//             int end = paint[i][1];
-            
-//             while(start < end)
-//             {
-//                 if(m.count(start))
-//                 {
-//                     int prev_end = m[start];
-//                     m[start] = max(m[start], end);
-//                     start = prev_end;
-//                 }
-            
-//                 else
-//                 {
-//                     m[start] = end;
-//                     start += 1;
-//                     work += 1;
-//                 }
-//             }
-//             res.push_back(work);
+//         for(auto p:paint){
+//             workDone.push_back(calAmountPainted(p[0],p[1]));
 //         }
-//         return res;
+//         return workDone;
 //     }
+    
+//     int calAmountPainted(int start, int end){
+//         int work=0;
+//         while(start<end){
+//             // start not found
+//             if(mapper.find(start)==mapper.end()){
+//                 mapper[start]=end;
+//                 work++;
+//                 start++;
+//             }else {
+//                 int newStart = mapper[start];
+//                 mapper[start] = max(mapper[start],end);
+//                 start = newStart;
+//             }
+//         }
+//         return work;
+//     }
+
+
 // };
+
+class Solution {
+public:
+    
+    int InsertInterval(map<int, int> &mp, int start, int end) 
+    {
+        auto up_it = mp.upper_bound(end);
+        auto lo_it = std::prev(mp.lower_bound(start));
+        
+        lo_it = lo_it->second < start ? std::next(lo_it) : lo_it;              // First overlapping interval
+
+        int paint = end - start;        
+        for(auto it = lo_it; it != up_it; ++it) {
+            paint -= std::min(end, it->second) - std::max(start, it->first);   // Substract overlapping interval from the paint
+            
+            start = std::min(start, it->first);                                // Update new interval to be stored in the map
+            end   = std::max(end  , it->second);
+        }
+           
+        mp.erase(lo_it, up_it);                                               // Remove overlapping intervals
+        
+        mp[start] = end;                                                      // Insert new interval
+        return paint;
+    }
+    
+    
+    vector<int> amountPainted(vector<vector<int>>& paint) {
+        map<int, int> mp;
+        mp[INT_MIN] = INT_MIN;           // Insert dummy Interval
+        mp[INT_MAX] = INT_MAX;
+        
+        vector<int> ans;
+        for(auto p : paint) {
+            ans.push_back(InsertInterval(mp, p[0], p[1]));
+        }
+        
+        return ans;
+    }
+};
