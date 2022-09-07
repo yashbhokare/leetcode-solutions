b7aed58092e6ccca0da7174b5e7309857d2dd39c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-07 07:29:10 -0700

    Time: 1384 ms (7.17%), Space: 165.6 MB (26.37%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -1,82 +1,82 @@
-// class Solution {
-// public:
-   
-//     vector<int> workDone;
-//     unordered_map<int,int> mapper;
-//     vector<int> amountPainted(vector<vector<int>>& paint) {
-//         for(auto p:paint){
-//             workDone.push_back(calAmountPainted(p[0],p[1]));
-//         }
-//         return workDone;
-//     }
-    
-//     int calAmountPainted(int start, int end){
-//         int work=0;
-//         while(start<end){
-//             // start not found
-//             if(mapper.find(start)==mapper.end()){
-//                 mapper[start]=end;
-//                 work++;
-//                 start++;
-//             }else {
-//                 int newStart = mapper[start];
-//                 mapper[start] = max(mapper[start],end);
-//                 start = newStart;
-//             }
-//         }
-//         return work;
-//     }
-
-
-// };
-
-
 class Solution {
 public:
-    vector<int>par;
-    int n;
-    
-    void make_set(int v){
-        par[v] = v;
-    }
-    
-    int find_set(int v){
-        if(par[v]==v)
-            return v;
-        return par[v] = find_set(par[v]);
-    }
-    
-    void union_set(int u, int v){
-        u = find_set(u);
-        v = find_set(v);
-        
-        par[u] =v;
-    }
-    
+   
+    vector<int> workDone;
+    unordered_map<int,int> mapper;
     vector<int> amountPainted(vector<vector<int>>& paint) {
-        int maxlen = 0;
-        int minlen = INT_MAX;
-        for(auto& v:paint){
-            minlen = min(minlen, v[0]);
-            maxlen = max(maxlen, v[1]);
+        for(auto p:paint){
+            workDone.push_back(calAmountPainted(p[0],p[1]));
         }
-        par.resize(maxlen+2);
-        
-        for(int i=minlen; i<=maxlen+1; i++){
-            make_set(i);
-        }
-        n = paint.size();
-        vector<int>ans(n,0);
-        
-        for(int i=0; i< paint.size(); i++){
-            int l = paint[i][0]; int r = paint[i][1];
-            
-            for(l=find_set(l); l<r; l=find_set(l)){
-                ans[i]++;
-                union_set(l, l+1);
+        return workDone;
+    }
+    
+    int calAmountPainted(int start, int end){
+        int work=0;
+        while(start<end){
+            // start not found
+            if(mapper.find(start)==mapper.end()){
+                mapper[start]=end;
+                work++;
+                start++;
+            }else {
+                int newStart = mapper[start];
+                mapper[start] = max(mapper[start],end);
+                start = newStart;
             }
         }
-        
-        return ans;
+        return work;
     }
+
+
 };
+
+
+// class Solution {
+// public:
+//     vector<int>par;
+//     int n;
+    
+//     void make_set(int v){
+//         par[v] = v;
+//     }
+    
+//     int find_set(int v){
+//         if(par[v]==v)
+//             return v;
+//         return par[v] = find_set(par[v]);
+//     }
+    
+//     void union_set(int u, int v){
+//         u = find_set(u);
+//         v = find_set(v);
+        
+//         par[u] =v;
+//     }
+    
+//     vector<int> amountPainted(vector<vector<int>>& paint) {
+//         int maxlen = 0;
+//         int minlen = INT_MAX;
+//         for(auto& v:paint){
+//             minlen = min(minlen, v[0]);
+//             maxlen = max(maxlen, v[1]);
+//         }
+//         par.resize(maxlen+2);
+        
+//         for(int i=minlen; i<=maxlen+1; i++){
+//             make_set(i);
+//         }
+//         n = paint.size();
+//         vector<int>ans(n,0);
+        
+//         for(int i=0; i< paint.size(); i++){
+//             int l = paint[i][0]; int r = paint[i][1];
+            
+//             for(l=find_set(l); l<r; l=find_set(l)){
+//                 ans[i]++;
+//                 union_set(l, l+1);
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
