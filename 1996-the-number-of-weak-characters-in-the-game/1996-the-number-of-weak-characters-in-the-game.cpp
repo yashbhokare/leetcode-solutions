3ccb77ea052da334e1a101ee3c47bd9fe71d1551
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-08 18:45:34 -0700

    Time: 912 ms (68.11%), Space: 126.6 MB (35.16%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,20 +1,59 @@
+// class Solution {
+// public:
+//     int numberOfWeakCharacters(vector<vector<int>>& properties) {
+//         // Sort in ascending order of attack, 
+//         // If attack is same sort in descending order of defense
+//         sort(properties.begin(), properties.end(), 
+//              [](const vector<int>& a, vector<int>& b) -> bool { 
+//                  return (a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]);});
+             
+//         int weakCharacters = 0;
+//         int maxDefense = 0;
+//         for (int i = (int)properties.size() - 1; i >= 0; i--) {
+//             // Compare the current defense with the maximum achieved so far
+//             if (properties[i][1] < maxDefense) {
+//                 weakCharacters++;
+//             }
+//             maxDefense = max(maxDefense, properties[i][1]);
+//         }
+        
+//         return weakCharacters;
+//     }
+// };
+
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        // Sort in ascending order of attack, 
-        // If attack is same sort in descending order of defense
-        sort(properties.begin(), properties.end(), 
-             [](const vector<int>& a, vector<int>& b) -> bool { 
-                 return (a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]);});
-             
+        int maxAttack = 0;
+        // Find the maximum atack value
+        for (vector<int>& property : properties) {
+            int attack = property[0];
+            maxAttack = max(maxAttack, attack);
+        }
+        
+        vector<int> maxDefense(maxAttack + 2, 0);
+        // Store the maximum defense for an attack value
+        for (vector<int>& property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            maxDefense[attack] = max(maxDefense[attack], defense);
+        }
+
+        // Store the maximum defense for attack greater than or equal to a value
+        for (int i = maxAttack - 1; i >= 0; i--) {
+            maxDefense[i] = max(maxDefense[i], maxDefense[i + 1]);
+        }
+        
         int weakCharacters = 0;
-        int maxDefense = 0;
-        for (int i = (int)properties.size() - 1; i >= 0; i--) {
-            // Compare the current defense with the maximum achieved so far
-            if (properties[i][1] < maxDefense) {
+        for (vector<int>& property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+            
+            // If their is a greater defense for properties with greater attack
+            if (defense < maxDefense[attack + 1]) {
                 weakCharacters++;
             }
-            maxDefense = max(maxDefense, properties[i][1]);
         }
         
         return weakCharacters;
