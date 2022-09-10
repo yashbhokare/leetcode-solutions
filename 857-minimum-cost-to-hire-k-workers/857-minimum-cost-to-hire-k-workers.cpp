2689dd86d0eb4af57a16140a1ad13144d25c228f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-10 09:37:50 -0700

    Time: 65 ms (53.21%), Space: 23.1 MB (21.89%) - LeetHub

---
+++ 857-minimum-cost-to-hire-k-workers/857-minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        int n= quality.size();
+        vector<pair<double,int>> wagePerQuality;
+        for(int i=0;i<n;i++){
+            double ratio = (double)wage[i]/quality[i];
+            wagePerQuality.push_back({ratio,quality[i]});
+        }
+        sort(wagePerQuality.begin(),wagePerQuality.end());
+        
+       priority_queue<double,vector<double>> maxHeap;
+        double total = 0;
+        for(int workers=0;workers<k;workers++){
+            maxHeap.push(wagePerQuality[workers].second);
+            total+=wagePerQuality[workers].second;
+        }
+        
+        double minResult = total*wagePerQuality[k-1].first;
+        
+        for(int captain=k;captain<n;captain++){
+                int currQuality = wagePerQuality[captain].second;
+                maxHeap.push(currQuality);
+                double maxValue = maxHeap.top();
+                maxHeap.pop();
+                total-=maxValue;
+                total+=currQuality;
+                double currResult = total*wagePerQuality[captain].first;
+                minResult = min(minResult,currResult);
+        }
+                
+        return minResult; 
+    }
+        
+        
+
+        
+};
