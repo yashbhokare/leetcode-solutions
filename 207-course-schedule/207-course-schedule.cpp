6eadf165eceee4a5ad377295a4a729ababcf828e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-10 06:03:15 -0700

    Time: 43 ms (34.72%), Space: 16.9 MB (7.07%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
     unordered_map<int,vector<int>> mapper;
-    unordered_map<int,bool> memo;
-    unordered_set<int> visited;
+    unordered_map<int,int> memo;
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         for(auto pre:prerequisites){
             mapper[pre[0]].push_back(pre[1]);
         }
         
+       
         for(int i=0;i<numCourses;i++){
-            if(!backTrack(i)) return false;
+            unordered_set<int> visited;
+            if(isCycle(i,visited)) return false;
         }
         return true;
     }
     
-    
-    bool backTrack(int course){
-          // If course already in memo return result;
+    bool isCycle(int course,unordered_set<int>& visited){
+        // Check if course exists in mapper
+        if(mapper.find(course)==mapper.end()) return false;
+        
+        // Course already exists so a cycle is detected
+        if(visited.find(course)!=visited.end()) return true;
+        
+        // Check if it exists in memo
         if(memo.find(course)!=memo.end()) return memo[course];
         
-        //Course not found return true
-        if(mapper.find(course)==mapper.end()){
-            return true;
-        }
-        
-        //Course already visited return false as cycle detected
-        if(visited.find(course)!=visited.end()) return false;
-
-      
-        
-
-        cout<<course;
         // Mark course as visited
         visited.insert(course);
-        bool res = true;
         
-        for(auto child:mapper[course]){
-            res = backTrack(child);
-            if(res==false) break;
+        bool result = false;
+        // Check for all it's children
+        for(auto childCourse:mapper[course]){
+            result = isCycle(childCourse,visited);
+            if(result) break;
         }
         
-        cout<<res;
         visited.erase(course);
-        
-        memo[course] = res;
-        
-        return memo[course];
+        memo[course] = result;
+        return result;
     }
 };
