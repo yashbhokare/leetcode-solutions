94451d235245b1e146f7dff669e73ea4ee270f91
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-10 16:52:22 -0700

    Time: 4 ms (35.43%), Space: 6.9 MB (25.46%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> stk;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==']'){
+                string decodedOne = "";
+                // cout<<stk.top();
+                while(stk.top()!="["){
+                    decodedOne= stk.top()+decodedOne;
+                    stk.pop();
+                }
+                stk.pop(); // Remove the [ from stack
+                string sdigit = "";
+                // cout<<decodedOne;
+                while(!stk.empty() && stk.top()>="0" && stk.top()<="9" ){
+                    sdigit = stk.top() + sdigit;
+                    stk.pop();
+                }
+                cout<<sdigit;
+                int digit = stoi(sdigit);
+                while(digit--){
+                    stk.push(decodedOne);
+                }
+            }else {
+                string str ="";
+                str.push_back(s[i]);
+                
+                // cout<<str;
+                stk.push(str);
+            }
+        }
+        string result = "";
+        while(!stk.empty()){
+            result = stk.top()+result;
+            stk.pop();
+        }
+        return result;
+    }
+};
