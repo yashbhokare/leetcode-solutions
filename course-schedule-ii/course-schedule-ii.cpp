6182a41b04e50573476049274a62414db776c553
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-10 06:18:14 -0700

    Time: 39 ms, Memory: 17.5 MB - LeetHub

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -1,81 +1,63 @@
 class Solution {
 public:
     unordered_map<int,vector<int>> mapper;
-    unordered_set<int> memo;
+    unordered_map<int,int> memo;
+    unordered_set<int> visited;
+    unordered_set<int> added;
     vector<int> ans;
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-//         unordered_map<int,vector<int>> courses;
-//         unordered_map<int,int> inDegrees;
-//         for(int i=0;i<numCourses;i++){
-//             inDegrees[i]=0;
-//         }
-//         for(auto preReq:prerequisites){
-//             courses[preReq[1]].push_back(preReq[0]);
-//             inDegrees[preReq[0]]++;
-//         }
-        
-//         queue<int> q;
-//         for(auto it=inDegrees.begin();it!=inDegrees.end();it++){
-//             if(it->second==0){
-//                 q.push(it->first);
-//             }
-//         }
-        
-//         unordered_set<int> visited;
-        
-//         vector<int> result;
-//         if(q.size()==0) return {};
-//         while(!q.empty()){
-//             int val = q.front();
-//             q.pop();
-//             visited.insert(val);
-//             result.push_back(val);
-//             for(auto course:courses[val]){
-//                 if(visited.find(course) == visited.end()){
-//                     inDegrees[course]--;
-//                     if(inDegrees[course]==0){
-//                         q.push(course);
-//                     } 
-//                 }
-//             }
-            
-//         }
-//         if(result.size() == numCourses){
-//             return result;
-//         }
-//         return {};
-        for(auto& preq:prerequisites){
-            mapper[preq[0]].push_back(preq[1]);
+        for(auto pre:prerequisites){
+            mapper[pre[0]].push_back(pre[1]);
         }
-        unordered_set<int> visited;
+        
+       
         for(int i=0;i<numCourses;i++){
-            if(!rec(i,visited)) return {};
+            // if(mapper.find(i)==mapper.end()) continue;
+            if(isCycle(i,visited)) return {};
+            // ans.push_back(i);
         }
         return ans;
     }
-        bool rec(int course,unordered_set<int>& visited){
-        // Already visited so cycle detected
-        if(visited.find(course)!=visited.end()) return false;
+    
+    bool isCycle(int course,unordered_set<int>& visited){
+        // Check if course exists in mapper
+        if(mapper.find(course)==mapper.end()) {
+            if(added.find(course)==added.end()){
+                ans.push_back(course);
+                added.insert(course);
+            }
+            
+            return false;
+        };
+    
         
-        // No cycle as already visited
-        if(memo.find(course)!=memo.end()) return true;
+        // Course already exists so a cycle is detected
+        if(visited.find(course)!=visited.end()) return true;
         
-        // Course doesn't have any prereq
-        // if(mapper.find(course)==mapper.end()) return true;
+          // Check if it exists in memo
+        if(memo.find(course)!=memo.end()) return memo[course];
+
         
+        // Mark course as visited
         visited.insert(course);
-        bool result=true;
-        vector<int> neighbours=mapper[course];
-        for(auto& neighbour:neighbours){
-            result= rec(neighbour,visited);
-            if(!result) break;
+        
+        bool result = false;
+        // Check for all it's children
+        for(auto childCourse:mapper[course]){
+            result = isCycle(childCourse,visited);
+            
+            if(result) break;
         }
         
-        //Backtrack and unvist the node
+        if(!result){
+            if(added.find(course)==added.end()){
+                ans.push_back(course);
+                added.insert(course);
+            }
+        }
+        
         visited.erase(course);
-        // cout<<course<<endl;
-        ans.push_back(course);
-        memo.insert(course);
+        memo[course] = result;
         return result;
     }
 };
