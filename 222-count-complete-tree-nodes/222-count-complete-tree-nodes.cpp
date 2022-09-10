7c79efba3cce481ab61cf93c371b671336943890
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-10 11:29:24 -0700

    Time: 78 ms (7.22%), Space: 31 MB (21.01%) - LeetHub

---
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int depth(TreeNode* root){
+        int depthValue = 0;
+        while(root->left){
+            root=root->left;
+            depthValue++;
+        }
+        return depthValue;
+    }
+    
+     bool ifExists(TreeNode* root,int d,int index){
+        TreeNode* node = root;
+        int left = 0;
+        int right = pow(2,d)-1;
+        
+        while(d--){
+            int mid = (left+right)/2;
+            if(index<=mid){
+                node=node->left;
+                right=mid;
+            }else {
+                node=node->right;
+                left=mid+1;
+            }
+        }
+        return node==NULL? false : true;
+    }
+    
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        
+        int d = depth(root);
+        if(d==0) return 1;
+        
+        int left = 0;
+        int right = pow(2,d)-1;
+        
+        while(left<=right){
+            // cout<<left<<" "<<right<<endl;
+            int mid = (left+right)/2;
+            if(ifExists(root,d,mid)){
+                // cout<<"True";
+                left = mid+1;
+            }else {
+                right=mid-1;
+            }
+        }
+        int total = pow(2,d)-1 + left;
+        return total;
+        
+    }
+    
+   
+};
