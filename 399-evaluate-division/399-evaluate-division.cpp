40f50ab2d448fc9a5f4194ec9f5bf144c14f51fa
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-10 17:24:37 -0700

    Time: 4 ms (53.31%), Space: 8.2 MB (92.69%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,64 +1,64 @@
 class Solution {
 public:
-    unordered_map<string,vector<pair<string,double>>> adjMap;
+    unordered_map<string,vector<pair<string,double>>> mapper;
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        // Creating the adj list for values
-        for(int i=0;i<equations.size();i++){
-            string s1=equations[i][0];
-            string s2=equations[i][1];
-            double val=values[i];
-            
-            adjMap[s1].push_back({s2,val});
-            adjMap[s2].push_back({s1,1/val});
+        int n = equations.size();
+        for(int i=0;i<n;i++){
+            string s1 = equations[i][0];
+            string s2=  equations[i][1];
+            double div = values[i];
+            mapper[s1].push_back({s2,div});
+            mapper[s2].push_back({s1,1/div});
         }
         
         vector<double> result;
-        
         for(int i=0;i<queries.size();i++){
-            
-            auto it1=adjMap.find(queries[i][0]);
-            auto it2= adjMap.find(queries[i][1]);
-            //Case 1 If any digit is not found add -1
-            if(it1 == adjMap.end() || it2 == adjMap.end()){
+            string s1 = queries[i][0];
+            string s2=  queries[i][1];
+            // Case 1 where the string doesn't exist
+            if(mapper.find(s1)==mapper.end() || mapper.find(s2)==mapper.end()){
                 result.push_back(-1);
             }
-            // Case 2 If both are equal and exist
-            else if(queries[i][0]==queries[i][1]){
+            // Case 2 if both string are equal
+            else if(s1==s2){
                 result.push_back(1);
-            }else {
-                unordered_set<string> visited;
-                double val = backtrack(queries[i][0],queries[i][1],visited);
-                result.push_back(val);
             }
+            else {
+                unordered_set<string> visited;
+                double ans = dfs(s1,s2,visited);
+                result.push_back(ans);
+            }
+            // cout<<result[i];
         }
         return result;
     }
     
-    double backtrack(string start,string end,unordered_set<string>& visited){
-        // String not found
-        if(adjMap.find(start)==adjMap.end()){
-           return -1; 
-        }
+    
+    double dfs(string start,string end,unordered_set<string>& visited){
+        if(visited.find(start)!=visited.end()) return -1;
         
-        if(visited.find(start)!=visited.end()){
-            return -1;
-        }
+        if(mapper.find(start)==mapper.end()) return -1;
         
-        vector<pair<string,double>> children = adjMap[start];
-        double res = -1;
         visited.insert(start);
-        
-        for(auto child:children){
-            cout<<child.first<<endl;
-            if(child.first==end){
-                return child.second;
+        double result = -1;
+        for(auto child:mapper[start]){
+            string s2 = child.first;
+            double v2 = child.second;
+            if(s2==end){
+                result = v2;
+                break;
             }else {
-                res = max(res,backtrack(child.first,end,visited)*child.second);
+                double val = dfs(s2,end,visited);
+                if(val!=-1){
+                    result = val*v2;
+                    break;
+                }
             }
         }
         
         visited.erase(start);
-        return res < 0 ? -1 : res;
-        
+        return result;
     }
+    
+    
 };
