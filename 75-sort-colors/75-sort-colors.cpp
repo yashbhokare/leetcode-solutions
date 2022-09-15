b8d4cbaaa5e1ac4d8030367333f28da3d9c27b3f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-15 08:35:58 -0700

    Time: 0 ms (100.00%), Space: 8.2 MB (90.71%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,16 +1,29 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         for(int i=0;i<nums.size();i++){
+//             int minElem = INT_MAX;
+//             int minIndex = i; 
+//             for(int j=i;j<nums.size();j++){
+//                 if(nums[j]<minElem){
+//                     minElem = nums[j];
+//                     minIndex = j;
+//                 }
+//             }
+//             swap(nums[i],nums[minIndex]);
+//         }
+//     }
+// };
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            int minElem = INT_MAX;
-            int minIndex = i; 
-            for(int j=i;j<nums.size();j++){
-                if(nums[j]<minElem){
-                    minElem = nums[j];
-                    minIndex = j;
-                }
-            }
-            swap(nums[i],nums[minIndex]);
+        int startIndex=0;
+        int lastIndex = nums.size()-1;
+        int i=0;
+        while(i<=lastIndex){
+            if(nums[i]==0) swap(nums[i++],nums[startIndex++]);
+            else if(nums[i]==2) swap(nums[i],nums[lastIndex--]);
+            else i++;
         }
     }
 };
