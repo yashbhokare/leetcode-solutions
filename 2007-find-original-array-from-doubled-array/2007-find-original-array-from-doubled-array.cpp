19a7712cf60f0db757f5a7416180e6e68eefd17c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-14 19:25:58 -0700

    Time: 390 ms (94.06%), Space: 124.9 MB (91.75%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,3 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> findOriginalArray(vector<int>& changed) {
+//         if(changed.size()%2!=0) return {};
+        
+//         int reqSize = changed.size()/2;
+        
+//         unordered_map<int,int> mapper;
+//         for(auto num:changed) mapper[num]++;
+        
+//         sort(changed.begin(),changed.end());
+//         vector<int> resultArray;
+//         for(auto num:changed)
+//         {
+//             if(num==0 && mapper[num]<2) continue;
+            
+//             if(mapper[num] && mapper[num*2]){
+//                 mapper[num]--;
+//                 mapper[num*2]--;
+//                 resultArray.push_back(num);
+//             }
+//             if(resultArray.size()>reqSize) return {};
+//         }
+//         if(resultArray.size()==reqSize){
+//             return resultArray;
+//         }else {
+//             return {};
+//         }
+//     }
+// };
+
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
