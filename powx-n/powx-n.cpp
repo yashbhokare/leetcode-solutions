fb3619ee5da8ebf717a651e79ff73dc5cfe4fc00
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-16 08:10:41 -0700

    Time: 3 ms, Memory: 5.9 MB - LeetHub

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N = n;
-        if(n < 0){
-            N = -N;
-            x = 1/x;
+        long N=n;
+        if(n<0){
+            N=-N;
+            x= 1/x;
         }
-        return cal(x,N);
+        return rec(x,N);
     }
     
-    double cal(double x, long long n){
+    double rec(double x, int n){
         if(n==0) return 1;
-        double half = cal(x, n / 2);
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * x;
+        double half = rec(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }else {
+            return half*half*x;
         }
         
     }
