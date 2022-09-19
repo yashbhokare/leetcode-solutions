88eb1295843666bf782d07d4eccf35c73b33bc8a
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 11:42:38 -0700

    Time: 6 ms (50.19%), Space: 7.2 MB (13.47%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -10,10 +10,15 @@ public:
         }
         long result = 0;
         while(s[index]>='0' && s[index]<='9'){
-            result = result*10 + (s[index]-'0');
+            
+            int digit =s[index]-'0';
+            // Check overflow and underflow conditions. 
+            if ((result > INT_MAX / 10) || (result == INT_MAX / 10 && digit > INT_MAX % 10)) { 
+                // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.    
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+            result = result*10 + digit;
             index++;
-            if(result*sign>=INT_MAX) return INT_MAX;
-            if(result*sign<=INT_MIN) return INT_MIN;
         }
         return result*sign;
         
