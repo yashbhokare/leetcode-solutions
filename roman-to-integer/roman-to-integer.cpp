85f9a918d74c0cfc06148bb913c99d4aa6480f99
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 13:05:49 -0700

    Time: 77 ms, Memory: 12.9 MB - LeetHub

--- roman-to-integer/roman-to-integer.cpp
+++ roman-to-integer/roman-to-integer.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
-int romanToInt(string s) 
-{
-    unordered_map<char, int> T = { { 'I' , 1 },
-                                   { 'V' , 5 },
-                                   { 'X' , 10 },
-                                   { 'L' , 50 },
-                                   { 'C' , 100 },
-                                   { 'D' , 500 },
-                                   { 'M' , 1000 } };
-                                   
-   int sum = T[s.back()];
-   for (int i = s.length() - 2; i >= 0; --i) 
-   {
-       if (T[s[i]] < T[s[i + 1]])
-       {
-           sum -= T[s[i]];
-       }
-       else
-       {
-           sum += T[s[i]];
-       }
-   }
-   
-   return sum;
-}
+    int romanToInt(string s) {
+        unordered_map<string,int> mapper = {
+            {"M",1000},
+            {"CM",900},
+            {"D",500},
+            {"CD",400}, 
+            {"C",100},
+            {"XC",90},
+            {"L",50},
+            {"XL",40},
+            {"X",10},
+            {"IX",9},
+            {"V",5}, 
+            {"IV",4},
+            {"I",1}
+        };
+         int result = 0;
+        for(int i=0;i<s.size();i++){
+            string temp = "";
+            if((i+1)<s.size()){
+                temp+=s[i];
+                temp+=s[i+1];
+                // cout<<temp<<endl;
+                if(mapper.find(temp)!=mapper.end()){
+                    result+=mapper[temp];
+                    i++;
+                    continue;
+                }  
+                temp="";
+            }
+            temp+=s[i];
+            // cout<<temp<<endl;
+            result+=mapper[temp];
+            
+        }
+       
+        return result;
+        
+    }
 };
