d24308102897b6f1385c78029368b0beaefbd4f6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-18 18:53:33 -0700

    Time: 216 ms (33.97%), Space: 36.8 MB (75.19%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -3,12 +3,14 @@ public:
     vector<vector<string>> findDuplicate(vector<string>& paths) {
         unordered_map<string,vector<string>> mapper;
         for(auto path:paths){
-            int index = path.find_first_of(" ");
+            // int index = path.find_first_of(" ");
             string root;
-            root=path.substr(0,index);
-            root.push_back('/');
-            path = path.substr(index);
+            // root=path.substr(0,index);
+            // root.push_back('/');
+            // path = path.substr(index);
             stringstream ss(path);
+            ss>>root;
+            root.push_back('/');
             string word;
             while(ss>>word){
                 int firstBracket = word.find_first_of('(');
