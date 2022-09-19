492ad284ecb63e9a9fb228b6a078959b1bd6d9dd
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 12:48:40 -0700

    Time: 13 ms, Memory: 8.8 MB - LeetHub

--- integer-to-roman/integer-to-roman.cpp
+++ integer-to-roman/integer-to-roman.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<int> val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        vector<string> symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<string> symbolMap = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<int> numMap = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         string result = "";
-        int index =0;
-        while(num!=0){
-            if(val[index]>num){
+        int index=0;
+        while(num>0){
+            if(num<numMap[index]){
                 index++;
                 continue;
             }
-            result+= symbols[index];
-            num = num - val[index];
-            
+            result+=symbolMap[index];
+            num = num-numMap[index];
         }
         return result;
     }
