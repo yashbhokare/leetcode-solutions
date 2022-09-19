f196820ef483ddaeec040703734f7bc065c7346c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 13:09:51 -0700

    Time: 25 ms, Memory: 7.8 MB - LeetHub

--- roman-to-integer/roman-to-integer.cpp
+++ roman-to-integer/roman-to-integer.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<string,int> mapper = {
-            {"M",1000},
-            {"CM",900},
-            {"D",500},
-            {"CD",400}, 
-            {"C",100},
-            {"XC",90},
-            {"L",50},
-            {"XL",40},
-            {"X",10},
-            {"IX",9},
-            {"V",5}, 
-            {"IV",4},
-            {"I",1}
+        unordered_map<char,int> mapper = {
+            {'M',1000},
+            {'D',500},
+            {'C',100},
+            {'L',50},
+            {'X',10},
+            {'V',5}, 
+            {'I',1}
         };
          int result = 0;
         for(int i=0;i<s.size();i++){
-            string temp = "";
-            if((i+1)<s.size()){
-                temp+=s[i];
-                temp+=s[i+1];
-                // cout<<temp<<endl;
-                if(mapper.find(temp)!=mapper.end()){
-                    result+=mapper[temp];
-                    i++;
-                    continue;
-                }  
-                temp="";
+            if(i+1<s.size() && mapper[s[i]]<mapper[s[i+1]]){
+                result+=mapper[s[i+1]]-mapper[s[i]];
+                i++;
+            }else {
+                result+=mapper[s[i]];
             }
-            temp+=s[i];
-            // cout<<temp<<endl;
-            result+=mapper[temp];
-            
         }
        
         return result;
