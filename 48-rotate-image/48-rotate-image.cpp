6b08dfd82b12ef1d691c40d3178a485ab95ce6dc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 16:36:32 -0700

    Time: 3 ms (64.52%), Space: 7.2 MB (33.36%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,41 +1,14 @@
-// class Solution {
-// public:
-//     int nR,nC;
-//     void rotate(vector<vector<int>>& matrix) {
-//         nR = matrix.size();
-//         nC = matrix[0].size();
-//         transpose(matrix);
-//         mirror(matrix);
-//     }
-    
-//     void transpose(vector<vector<int>>& matrix){
-//         for(int r=0;r<nR;r++){
-//             for(int c=r;c<nC;c++){
-//                 swap(matrix[r][c],matrix[c][r]);
-//             }
-//         }
-//     }
-    
-//     void mirror(vector<vector<int>>& matrix){
-//         for(int r=0;r<nR;r++){
-//             for(int c=0;c<nC/2;c++){
-//                 swap(matrix[r][c],matrix[r][nC-1-c]);
-//             }
-//         }
-//     }
-// };
-
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         for(int i=0;i<n/2;i++){
-            for(int j=0;j<(n+1)/2;j++){
-                int temp = matrix[n-i-1][j];
-                matrix[n-i-1][j] = matrix[n-j-1][n-i-1];
-                matrix[n-j-1][n-i-1] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = matrix[j][i];
-                matrix[j][i] = temp;
+            for(int j=i;j<n-i-1;j++){
+                int temp = matrix[n-j-1][i];
+                matrix[n-j-1][i] =  matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = matrix[i][j];
+                matrix[i][j]=temp;
             }
         }
     }
