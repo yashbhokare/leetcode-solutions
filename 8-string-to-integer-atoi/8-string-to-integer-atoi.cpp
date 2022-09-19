1e30acb68664275178e34ad5b83d4f2197e27680
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 11:39:28 -0700

    Time: 9 ms (25.42%), Space: 7 MB (52.87%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int index = s.find_first_not_of(' ');
-        if(index==-1) return 0; 
+        
+        int index=s.find_first_not_of(' ');
+        if(index==-1) return 0;
         int sign = 1;
-        long int result = 0;
-        if(s[index]=='+'|| s[index]=='-'){
-            sign = s[index++]=='-' ? -1 : 1;
+        if(s[index]=='+' || s[index]=='-'){
+            sign = s[index]=='-' ? -1 : 1; 
+            index++;
         }
-        while(index<s.size() && s[index]>='0' && s[index]<='9'){
-            result = result*10 + s[index++]-'0';
-            if(sign*result >= INT_MAX) return INT_MAX;
-            if(sign*result <= INT_MIN) return INT_MIN;
+        long result = 0;
+        while(s[index]>='0' && s[index]<='9'){
+            result = result*10 + (s[index]-'0');
+            index++;
+            if(result*sign>=INT_MAX) return INT_MAX;
+            if(result*sign<=INT_MIN) return INT_MIN;
         }
         return result*sign;
+        
     }
 };
