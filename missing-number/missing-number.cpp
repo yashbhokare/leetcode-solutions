5dcc25caeb329cd98b953a0b850c451c6d7d1a16
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 19:09:11 -0700

    Time: 43 ms, Memory: 18.1 MB - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int maxValue = 0;
-        
-        int givenSum = 0;
-        for(int i=0;i<n;i++){
-            givenSum+= nums[i];
-            maxValue = max(maxValue,nums[i]);
-        }
-        int sum = ((n+1)*(n))/2;
-        return  sum - givenSum;
+        int n=nums.size();
+        int currentTotalSum = accumulate(nums.begin(),nums.end(),0);
+        int expectedTotalSum = (n*(n+1))/2;
+        return expectedTotalSum-currentTotalSum;
     }
 };
