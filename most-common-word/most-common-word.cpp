20d7a8925782a00895bf7d4cd1684fa55b941164
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 19:30:34 -0700

    Time: 10 ms, Memory: 8.1 MB - LeetHub

--- most-common-word/most-common-word.cpp
+++ most-common-word/most-common-word.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    static bool isNotAlnum(char c) {
-    return c!=' ' && isalnum(c) == 0;
-}
     string mostCommonWord(string paragraph, vector<string>& banned) {
         unordered_set<string> ban(banned.begin(),banned.end());
-        unordered_map<string,int> val;
-        for(char& c:paragraph){
-           c = isalpha(c) ? tolower(c) : ' ';
-        }
-        stringstream ss(paragraph);
-        string word;
-        int maxCount = INT_MIN;
-        string result = "";
-        while(ss >> word){
-            auto it = val.find(word);
-            auto it2= ban.find(word);
-            if(it2 != ban.end()) continue;
-            if(it != val.end()){
-                it->second = it->second + 1;
-                if(it->second > maxCount ){
-                    maxCount = it->second;
-                    result = word;
+        unordered_map<string,int> mapper;
+        string new_para = "";
+        for(auto ch:paragraph){
+            if((ch>='a' && ch<='z')){
+                new_para.push_back(ch);
+            }else if(ch>='A' && ch<='Z'){
+                ch = ch -'A'+'a';
+                new_para.push_back(ch);
+            }else{
+                new_para.push_back(' ');
             }
-
-            }else {
-                val.insert({word,0});
-                if(0 > maxCount ){
-                    maxCount = 0;
-                    result = word;
+        }
+        
+        stringstream ss(new_para);
+        string word;
+        int maxVal = 0;
+        string result = "";
+        while(ss>>word){
+            if(ban.find(word)==ban.end()){
+                mapper[word]++;
+                if(mapper[word]>maxVal){
+                    maxVal =mapper[word];
+                    result=word;
                 }
             }
-            
-            // cout<<result<<endl;
         }
         return result;
+
+        
     }
 };
