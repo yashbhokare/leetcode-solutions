4d567589d8e943b6854f043df2c563154016bb58
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-19 19:13:22 -0700

    Time: 62 ms (42.54%), Space: 10.6 MB (69.54%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int> charSet(26,-1);
+        vector<int> charArray (26,0);
+        for(auto ch:s) charArray[ch-'a']++;
         for(int i=0;i<s.size();i++){
-            charSet[s[i]-'a']++;
-        }
-        
-        for(int i=0;i<s.size();i++){
-            if(charSet[s[i]-'a']==0) return i;
+            if(charArray[s[i]-'a']==1) return i; 
         }
         return -1;
     }
