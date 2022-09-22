d2402d957db5b4502618c2698794e12b78f8a315
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-21 17:40:53 -0700

    Time: 17 ms (78.09%), Space: 9.5 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     string reverseWords(string s) {
         // string result = "";
-        vector<int> indexes;
-        indexes.push_back(-1);
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' ') indexes.push_back(i);
+        // vector<int> indexes;
+        // indexes.push_back(-1);
+        int start=0;
+        for(int i=0;i<=s.size();i++){
+            if(i==s.size() || s[i]==' '){
+                swapString(s,start,i-1);
+                start=i+1;
+            }
+            
         }
-        indexes.push_back(s.size());
-        for(int i=1;i<indexes.size();i++){
-            int start = indexes[i-1]+1;
-            int end = indexes[i]-1;
-            swapString(s,start,end);
-        }
+        // indexes.push_back(s.size());
+        // for(int i=1;i<indexes.size();i++){
+        //     int start = indexes[i-1]+1;
+        //     int end = indexes[i]-1;
+        //     swapString(s,start,end);
+        // }
         return s;
     }
     
