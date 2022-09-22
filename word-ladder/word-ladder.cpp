b0fb28293d1039fe97857341d35bd0c9b35e331d
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 14:54:16 -0700

    Time: 192 ms, Memory: 32.1 MB - LeetHub

--- word-ladder/word-ladder.cpp
+++ word-ladder/word-ladder.cpp
@@ -1,51 +1,48 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,vector<string>> wordSet;
+        unordered_map<string,vector<string>> wordMap;
         unordered_set<string> visited;
-         string newWord = "";
-        for(string word:wordList){
+        queue<string> q;
+        
+        for(auto word:wordList){
             for(int i=0;i<word.size();i++){
-                newWord = word.substr(0,i) + '*' + word.substr(i+1);
-                wordSet[newWord].push_back(word);
-                cout<<newWord<<" ";
+                string temp = word;
+                int currCh = word[i];
+                word[i] ='*';
+                wordMap[word].push_back(temp);
+                word[i] = currCh;
             }
         }
         
-        queue<string> q;
-        cout<<endl<<beginWord;
+       
         q.push(beginWord);
         visited.insert(beginWord);
+        
         int result = 1;
         while(!q.empty()){
-            int qSize = q.size();
-            cout<<endl;
-            for(int i=0;i<qSize;i++){
-                string word = q.front();
-                for(int i=0;i<word.size();i++){
-                    newWord = word.substr(0,i) + '*' + word.substr(i+1);
-                    auto it = wordSet.find(newWord);
-                    if(it != wordSet.end()){
-                        vector<string> wordValues = it->second;
-                        for(string wordVal: wordValues){
-                            if(visited.find(wordVal) == visited.end()){
-                                cout<<wordVal<<" ";
-                                if(wordVal==endWord){
-                                    return result+1;
-                                }
-                                q.push(wordVal);
-                                visited.insert(wordVal);
-                            }
-                        }                        
-                    }
-      
-                }
+            int size = q.size();
+            while(size--){
+                string curr = q.front();
                 q.pop();
+                if(curr==endWord) return result+1;
+                for(int i=0;i<curr.size();i++){
+                    int currCh = curr[i];
+                    curr[i] ='*';
+                    if(wordMap.find(curr)!=wordMap.end()){
+                        for(auto word:wordMap[curr]){
+                            if(visited.find(word)==visited.end()){
+                                q.push(word);
+                                visited.insert(word);
+                                if(word==endWord) return result+1;
+                            }
+                        }
+                    }
+                    curr[i] = currCh;
+                }
             }
             result++;
         }
         return 0;
-
     }
-    
 };
