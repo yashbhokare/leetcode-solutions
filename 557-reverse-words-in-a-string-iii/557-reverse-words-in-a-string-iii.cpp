f4739f0a820f47267730048f79dbf54dcde78fa8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-21 17:38:57 -0700

    Time: 31 ms (33.20%), Space: 9.9 MB (40.62%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string result="";
-        stringstream ss(s);
-        string word;
-        while(ss>>word){
-            reverse(word.begin(),word.end());
-            result=result+" "+word;
+        // string result = "";
+        vector<int> indexes;
+        indexes.push_back(-1);
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ') indexes.push_back(i);
         }
-        return result.substr(1);
+        indexes.push_back(s.size());
+        for(int i=1;i<indexes.size();i++){
+            int start = indexes[i-1]+1;
+            int end = indexes[i]-1;
+            swapString(s,start,end);
+        }
+        return s;
+    }
+    
+    void swapString(string& s,int start,int end){
+        while(start<=end){
+            swap(s[start++],s[end--]);
+        }
     }
 };
