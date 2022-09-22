211235db4c478e79782653c2af07c90e54a5f747
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 14:54:32 -0700

    Time: 210 ms (64.09%), Space: 32 MB (14.66%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,vector<string>> wordMap;
+        unordered_set<string> visited;
+        queue<string> q;
+        
+        for(auto word:wordList){
+            for(int i=0;i<word.size();i++){
+                string temp = word;
+                int currCh = word[i];
+                word[i] ='*';
+                wordMap[word].push_back(temp);
+                word[i] = currCh;
+            }
+        }
+        
+       
+        q.push(beginWord);
+        visited.insert(beginWord);
+        
+        int result = 1;
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                string curr = q.front();
+                q.pop();
+                if(curr==endWord) return result+1;
+                for(int i=0;i<curr.size();i++){
+                    int currCh = curr[i];
+                    curr[i] ='*';
+                    if(wordMap.find(curr)!=wordMap.end()){
+                        for(auto word:wordMap[curr]){
+                            if(visited.find(word)==visited.end()){
+                                q.push(word);
+                                visited.insert(word);
+                                if(word==endWord) return result+1;
+                            }
+                        }
+                    }
+                    curr[i] = currCh;
+                }
+            }
+            result++;
+        }
+        return 0;
+    }
+};
