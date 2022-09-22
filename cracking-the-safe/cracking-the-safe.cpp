e9068dab6b22204f88e01ae7144d1db044b2bf8d
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 07:29:35 -0700

    Time: 32 ms, Memory: 9.8 MB - LeetHub

--- cracking-the-safe/cracking-the-safe.cpp
+++ cracking-the-safe/cracking-the-safe.cpp
@@ -1,6 +1,46 @@
+
+//De Bruijn sequence
+// Hamilton Graph can also be used
+
 class Solution {
 public:
     string crackSafe(int n, int k) {
+        return dfs_crackSafe(n,k);
+        
+    }
+    // Using DFS
+    //https://leetcode.com/problems/cracking-the-safe/discuss/153039/DFS-with-Explanations
+    string dfs_crackSafe(int n, int k){
+       
+        unordered_set<string> visited;
+        string pwd(n,'0');
+        visited.insert(pwd);
+        int finalSize = (int)pow(k,n);
+        dfs(pwd,visited,n,k,finalSize);
+        return pwd;
+    }
+    
+    bool dfs(string& pwd,unordered_set<string>& visited,int n,int k,int finalSize){
+        if(visited.size()==finalSize){
+            return true;
+        }
+        string lastSubString = pwd.substr(pwd.size()-n+1);
+        for(char ch='0';ch<k+'0';ch++){
+            string newComb =lastSubString+ch;
+            if(visited.find(newComb)==visited.end()){
+                visited.insert(newComb);
+                pwd.push_back(ch);
+                if(dfs(pwd,visited,n,k,finalSize)) return true;
+                visited.erase(newComb);
+                pwd.pop_back();
+            }
+        }
+        return false;
+    }
+    
+    
+    // Greedy Approach
+    string greedy_crackSafe(int n, int k) {
         string ans = string(n, '0');
         unordered_set<string> visited;
         visited.insert(ans);
@@ -9,7 +49,6 @@ public:
             string prev = ans.substr(ans.size()-n+1,n-1);
             for(int j = k-1;j>=0;j--){
                 string now = prev + to_string(j);
-                cout<<now<<endl;
                 if(!visited.count(now)){
                     visited.insert(now);
                     ans += to_string(j);
