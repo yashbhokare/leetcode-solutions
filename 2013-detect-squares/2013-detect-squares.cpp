d1fd53ba576dd8e6bfa7cf2c50b47782425d5999
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-23 08:52:47 -0700

    Time: 302 ms (98.99%), Space: 144 MB (18.51%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.cpp
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -11,16 +11,15 @@ public:
     void add(vector<int> point) {
         int x = point[0];
         int y= point[1];
-
-        if(cntPoints[x][y]==0) points.push_back({x,y});
-                cntPoints[x][y]++;
+        cntPoints[x][y]++;
+        points.push_back({x,y});
     }
     
     int count(vector<int> point) {
         int x1 = point[0], y1 = point[1], ans=0;
         for(auto& [x3,y3]:points){
             if(abs(x1-x3)==0 || abs(x1-x3)!=abs(y1-y3)) continue;
-            ans+=cntPoints[x1][y3]*cntPoints[x3][y1]*cntPoints[x3][y3];
+            ans+=cntPoints[x1][y3]*cntPoints[x3][y1];
         }
         return ans;
     }
