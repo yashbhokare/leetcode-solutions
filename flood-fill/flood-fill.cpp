64e7e193382f45b5637e4b2c4f87f0f1821275b6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 19:04:10 -0700

    Time: 27 ms, Memory: 14.2 MB - LeetHub

--- flood-fill/flood-fill.cpp
+++ flood-fill/flood-fill.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int pixel = image[sr][sc];
-        if(pixel == newColor) return image;
-        // vector<vector<int>> visited(image.size(),vector<int>(image[0].size(),0)); 
-        dfs(image,sr,sc,newColor,pixel);
+    vector<vector<int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+    int maxX,maxY;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc]==color) return image;
+        maxX = image.size();
+        maxY = image[0].size();
+        dfs(sr,sc,image,color,image[sr][sc]);
         return image;
     }
     
-    void dfs(vector<vector<int>>& image,int r, int c, int newColor,int pixel){
-        int nr=image.size(); 
-        int nc = image[0].size();
-        image[r][c] = newColor;
-        if(r - 1 >=0 && image[r-1][c] == pixel) 
-            dfs(image,r-1,c,newColor,pixel);
-        if(r + 1 <nr && image[r+1][c] == pixel ) 
-            dfs(image,r+1,c,newColor,pixel);
-        if(c - 1 >=0 && image[r][c-1] == pixel) 
-            dfs(image,r,c-1,newColor,pixel);
-        if(c + 1 <nc && image[r][c+1] == pixel)
-            dfs(image,r,c+1,newColor,pixel);
+    void dfs(int x,int y,vector<vector<int>>& image,int newColor, int oldColor){
+
+        image[x][y]=newColor;
+        for(int i=0;i<dir.size();i++){
+            int newX = x+ dir[i][0];
+            int newY = y + dir[i][1];
+            if(newX>=0 && newX<maxX && newY>=0 && newY<maxY && image[newX][newY]==oldColor){
+                dfs(newX,newY,image,newColor,oldColor);
+            }
+        }
     }
 };
