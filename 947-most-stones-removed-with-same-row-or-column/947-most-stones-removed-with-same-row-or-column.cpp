c8ac38d0b36d4b5f80c3adfcec688bfc395b2d95
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 20:35:17 -0700

    Time: 341 ms (41.48%), Space: 14.3 MB (81.46%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,47 @@
+class Uf{
+    public:
+    vector<int> root;
+    int count=0;
+    void init(int n){
+        for(int i=0;i<n;i++){
+            root.push_back(i);
+        }
+        count=n;
+    }
+    
+    int find(int x){
+        if(root[x]==x) return x;
+        return root[x]=find(root[x]);
+    }
+    
+    void uni(int x,int y){
+        int rootX=find(x);
+        int rootY=find(y);
+        if(rootX!=rootY){
+            root[rootX] = root[rootY];
+            count--;
+        }
+    }
+    
+    int getCount(){
+        return count;
+    } 
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        Uf unionFind;
+        int n = stones.size();
+        
+        unionFind.init(n);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1] ){
+                    unionFind.uni(i,j);
+                }
+            }
+        }
+        return n - unionFind.getCount();
+    }
+};
