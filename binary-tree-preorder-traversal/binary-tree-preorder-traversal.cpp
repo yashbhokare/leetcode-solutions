b9ec82173c178ff8fafbd040e4dffdacc52a0919
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-23 16:47:34 -0700

    Time: 6 ms, Memory: 8.4 MB - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -12,33 +12,21 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        return preorderTraversalIterative(root);
+    }
+    
+    vector<int> preorderTraversalIterative(TreeNode* root) {
+        if(root==NULL) return {};
         vector<int> result;
-        if(root==NULL) return result;
-        // preorderTraversal(root, result);
-        preorderWithoutRecursion(root,result);
-        return result; 
-    }
-    
-    void preorderTraversal(TreeNode* root,vector<int> &result){
-        if(root==NULL) return;
-        result.push_back(root->val);
-        preorderTraversal(root->left,result);
-        preorderTraversal(root->right,result);
-    }
-    
-    void preorderWithoutRecursion(TreeNode* root,vector<int> &result){
         stack<TreeNode*> stk;
         stk.push(root);
         while(!stk.empty()){
             TreeNode* node = stk.top();
             stk.pop();
             result.push_back(node->val);
-            if(node->right!=NULL){
-                stk.push(node->right);
-            }
-            if(node->left!=NULL){
-                stk.push(node->left);
-            }
+            if(node->right) stk.push(node->right);
+            if(node->left) stk.push(node->left);
         }
+        return result;
     }
 };
