f3519613f7ebd39cc5c62c092c7a1e4c24fc310c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 20:15:10 -0700

    Time: 7 ms, Memory: 12.4 MB - LeetHub

--- flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
+++ flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -12,13 +12,8 @@
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        return recflipEquiv(root1,root2);
-    }
-    
-    bool recflipEquiv(TreeNode* root1, TreeNode* root2){
-        if(root1 ==NULL && root2==NULL) return true;
-        if(root1==NULL || root2==NULL || root1->val != root2->val) return false;
-        return  (recflipEquiv(root1->left,root2->right) && recflipEquiv(root1->right,root2->left))
-            || (recflipEquiv(root1->right,root2->right) && recflipEquiv(root1->left,root2->left));
+        if(root1==NULL && root2==NULL) return true;
+        if(root1==NULL || root2==NULL || root1->val!=root2->val) return false;
+        return (flipEquiv(root1->left,root2->right) && flipEquiv(root1->right,root2->left)) || (flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right));
     }
 };
