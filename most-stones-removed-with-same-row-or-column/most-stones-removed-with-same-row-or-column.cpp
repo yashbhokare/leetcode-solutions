2e309b362e23f64507a3fdd019ea7cfb0920addd
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 20:37:41 -0700

    Time: 278 ms, Memory: 14.4 MB - LeetHub

--- most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,58 +1,47 @@
-// class Solution {
-// public:
-
-//     int removeStones(vector<vector<int>>& stones) {
-//         for (int i = 0; i < stones.size(); ++i){
-//             cout<<stones[i][0]<<~stones[i][1]<<endl;
-//             uni(stones[i][0], ~stones[i][1]);
-//         }
-//         return stones.size() - islands;
-//     }
-
-//     unordered_map<int, int> f;
-//     int islands = 0;
-
-//     int find(int x) {
-//         if (!f.count(x)) f[x] = x, islands++;
-//         if (x != f[x]) f[x] = find(f[x]);
-//         return f[x];
-//     }
-
-//     void uni(int x, int y) {
-//         x = find(x), y = find(y);
-//         if (x != y) f[x] = y, islands--;
-//     }
-// };
+class Uf{
+    public:
+    vector<int> root;
+    int count=0;
+    void init(int n){
+        for(int i=0;i<n;i++){
+            root.push_back(i);
+        }
+        count=n;
+    }
+    
+    int find(int x){
+        if(root[x]==x) return x;
+        return root[x]=find(root[x]);
+    }
+    
+    void uni(int x,int y){
+        int rootX=find(x);
+        int rootY=find(y);
+        if(rootX!=rootY){
+            root[rootX] = root[rootY];
+            count--;
+        }
+    }
+    
+    int getCount(){
+        return count;
+    } 
+};
 
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
-        const int n = stones.size();
-        f = vector<int>(n);
-        count = n;
-        for(int i=0;i<n;i++) f[i] = i;
+        Uf unionFind;
+        int n = stones.size();
+        
+        unionFind.init(n);
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
-                    uni(i,j);
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                    unionFind.uni(i,j);
+                }
             }
         }
-        return n - count;
-    }
-    
-    vector<int> f;
-    int count;
-    
-    int find(int x) {
-        if (x != f[x]) f[x] = find(f[x]);
-        return f[x];
-    }
-
-    void uni(int x, int y) {
-        x = find(x), y = find(y);
-        if (x != y) {
-            f[x] = y;
-            count--;
-        }
+        return n - unionFind.getCount();
     }
 };
