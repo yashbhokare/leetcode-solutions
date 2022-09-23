e96be5aaff7ae12616afb0afe328ac33e1eb07f8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-22 18:29:37 -0700

    Time: 39 ms, Memory: 12.3 MB - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,56 +1,33 @@
 class Solution {
-    vector<vector<int>> dir = {
-        {0,1},{1,0},{-1,0},{0,-1}
-    };
 public:
-    int rMax;
-    int cMax;
+    
+    vector<vector<int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+    int maxX,maxY;
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        rMax = grid.size();
-        cMax = grid[0].size();
-        for(int r=0;r<rMax;r++){
-            for(int c=0;c<cMax;c++){
-                if(grid[r][c]=='1'){
-                    // dfs(grid,r,c);
-                    bfs(grid,r,c);
-                    count++;
+        maxX = grid.size();
+        maxY = grid[0].size();
+        int result=0;
+        for(int i=0;i<maxX;i++){
+            for(int j=0;j<maxY;j++){
+                if(grid[i][j]=='1'){
+                    dfs(i,j,grid);
+                    result++;
                 }
             }
         }
-        return count;
+        return result;
     }
     
     
-    void dfs(vector<vector<char>>& grid,int r,int c){
-        grid[r][c]='0';
+    void dfs(int x,int y,vector<vector<char>>& grid){
+        grid[x][y]='0';
+        for(int i=0;i<dir.size();i++){
+            int newX = x+dir[i][0];
+            int newY = y + dir[i][1];
+            if(newX>=0 && newX<maxX && newY>=0 && newY<maxY && grid[newX][newY]=='1'){
+                dfs(newX,newY,grid);
+            }
+        }
         
-        for(int i=0;i<4;i++){
-            int newR = r + dir[i][0];
-            int newC = c + dir[i][1];
-            if(newR>=0 && newR<rMax && newC>=0 && newC<cMax && grid[newR][newC]=='1'){
-                dfs(grid,newR,newC);
-            }
-        }
-    }
-    
-    void bfs(vector<vector<char>>& grid,int r,int c){
-        queue<pair<int,int>> q;
-        grid[r][c] = '0';
-        q.push({r,c});
-        while(!q.empty()){
-            r = q.front().first;
-            c = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int newR = r + dir[i][0];
-                int newC = c + dir[i][1];
-                if(newR>=0 && newR<rMax && newC>=0 && newC<cMax && grid[newR][newC]=='1'){
-                    grid[newR][newC] = '0';
-                    q.push({newR,newC});
-
-                }
-            }
-        }
     }
 };
