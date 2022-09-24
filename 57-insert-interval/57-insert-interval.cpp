ae5a2da0ef9de89be5b713eece318d0ceb2af47c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-24 10:41:01 -0700

    Time: 32 ms (40.75%), Space: 17.1 MB (74.39%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -5,26 +5,39 @@ public:
     }
     
      vector<vector<int>> insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval){
-         vector<vector<int>> merged;
+        vector<vector<int>> merged;
          
-         // Traverse until the start is greater
-         int index=0;
-         while(index<intervals.size() && intervals[index][1]<newInterval[0]){
-            merged.push_back({intervals[index][0],intervals[index][1]});
+        // Traverse until the new interval is greater than the last end
+      
+        int n = intervals.size();
+        if(n==0) {
+            merged.push_back(newInterval);
+            return merged;
+        };
+        int index=0;
+         // Traverse until the new interval is greater than the last end
+        while(index<n && intervals[index][1]<newInterval[0]){
+            merged.push_back(intervals[index]);
             index++;
-         }
+        }
+        
+        if(index<n && newInterval[0]>=intervals[index][0]){
+            intervals[index][1]=max(intervals[index][1],newInterval[1]);
+            merged.push_back(intervals[index++]);
+            // cout<<index;
+        }else {
+            merged.push_back(newInterval);
+        }
+        // cout<<index;
+        // Merge intervals
+        for(int i=index;i<n;i++){
+                if(merged.size()!=0 && intervals[i][0]<=merged.back()[1]){
+                    merged.back()[1] = max(merged.back()[1],intervals[i][1]);
+                }else {
+                     merged.push_back(intervals[i]);
+                }
+        }
          
-         merged.push_back({newInterval[0],newInterval[1]});
-         
-         // Merge Interval
-         for(int i =index;i<intervals.size();i++){
-             if(intervals[i][0]<=merged.back()[1]){
-                 merged.back()[0] = min(merged.back()[0],intervals[i][0]);
-                 merged.back()[1] = max(merged.back()[1],intervals[i][1]);
-             }else {
-                 merged.push_back(intervals[i]);
-             }
-         }
          return merged;
      }
 };
