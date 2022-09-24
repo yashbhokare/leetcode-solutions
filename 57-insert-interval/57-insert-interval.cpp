d0234726e2d0b1f73713a759caece5dfffbaaf7f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-24 10:44:38 -0700

    Time: 28 ms (52.11%), Space: 17 MB (91.70%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -10,10 +10,7 @@ public:
         // Traverse until the new interval is greater than the last end
       
         int n = intervals.size();
-        // if(n==0) {
-        //     merged.push_back(newInterval);
-        //     return merged;
-        // };
+
         int index=0;
          // Traverse until the new interval is greater than the last end
         while(index<n && intervals[index][1]<newInterval[0]){
@@ -21,16 +18,17 @@ public:
             index++;
         }
         
-        if(index<n && newInterval[0]>=intervals[index][0]){
-            intervals[index][1]=max(intervals[index][1],newInterval[1]);
-            merged.push_back(intervals[index++]);
-        }else {
+        // if(index<n && newInterval[0]>=intervals[index][0]){
+        //     intervals[index][1]=max(intervals[index][1],newInterval[1]);
+        //     merged.push_back(intervals[index++]);
+        // }else {
             merged.push_back(newInterval);
-        }
+        // }
 
         // Merge intervals
         for(int i=index;i<n;i++){
                 if(merged.size()!=0 && intervals[i][0]<=merged.back()[1]){
+                    merged.back()[0] = min(merged.back()[0],intervals[i][0]);
                     merged.back()[1] = max(merged.back()[1],intervals[i][1]);
                 }else {
                      merged.push_back(intervals[i]);
