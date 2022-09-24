3fced20cf47d7b5154a9e3f8c3b151853b18cb7c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-24 08:00:08 -0700

    Time: 23 ms (78.27%), Space: 24.2 MB (60.29%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -13,8 +13,9 @@ class Solution {
 public:
     priority_queue<int> maxHeap;
     int kthSmallest(TreeNode* root, int k) {
-        rec(root,k);
-        return maxHeap.top();
+        return inorderUsingStack(root,k);
+        // rec(root,k);
+        // return maxHeap.top();
     }
     
     void rec(TreeNode* root, int k){
@@ -23,5 +24,20 @@ public:
         if(maxHeap.size()>k)  maxHeap.pop();
         rec(root->left,k);
         rec(root->right,k);
+    }
+    
+    int inorderUsingStack(TreeNode* root, int k){
+        stack<TreeNode*> stk;
+        while(!stk.empty() || root){
+            while(root){
+                stk.push(root);
+                root=root->left;
+            }
+            TreeNode* temp = stk.top();
+            stk.pop();
+            if(--k==0) return temp->val;
+            root=temp->right;
+        }
+        return -1;
     }
 };
