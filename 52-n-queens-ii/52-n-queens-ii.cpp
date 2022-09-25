f26f52a32499cb2022d5574dd2020d3d80fb54c2
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-24 18:12:22 -0700

    Time: 31 ms (20.59%), Space: 8 MB (21.22%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -3,12 +3,12 @@ public:
     unordered_set<int> col_map;
     unordered_set<int> diag;
     unordered_set<int> anti_diag;
-    // vector<vector<int>> matrix;
+    vector<vector<int>> matrix;
     int N;
     int count=0;
     void init(int n){
         N = n;
-        // matrix.resize(n,vector<int>(n,0));
+        matrix.resize(n,vector<int>(n,0));
     }
     
     int totalNQueens(int n) {
@@ -18,15 +18,15 @@ public:
         return count;
     }
     
-    // void display(){
-    //     for(int i=0;i<N;i++){
-    //         for(int j=0;j<N;j++){
-    //             cout<<matrix[i][j]<<" ";
-    //         }
-    //         cout<<endl;
-    //     }
-    //     cout<<"**********************\n";
-    // }
+    void display(){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<"**********************\n";
+    }
     void rec(int row){
         if(row==N){
             count++;
@@ -44,7 +44,7 @@ public:
                 diag.insert(diag_value);
                 anti_diag.insert(anti_diag_value);
                 col_map.insert(col);
-                // matrix[row][col]=1;
+                matrix[row][col]=1;
                 
                 // Recursion call
                 rec(row+1);
@@ -53,7 +53,7 @@ public:
                 diag.erase(diag_value);
                 anti_diag.erase(anti_diag_value);
                 col_map.erase(col);
-                // matrix[row][col]=0;
+                matrix[row][col]=0;
             }
            
         }
