846196a1b6a03940bbc3aeaa556abf5d5ac7e63b
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-25 07:14:06 -0700

    Time: 10 ms, Memory: 7.6 MB - LeetHub

---
+++ convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* prev;
+
+    Node* treeToDoublyList(Node* root) {
+        if(root==NULL) return NULL;
+        prev=new Node(-1);
+        Node* head = prev;
+        rec(root);
+        head= head->right;
+        prev->right=head;
+        head->left=prev;
+
+        return head;
+    }
+    
+    
+    void rec(Node* root){
+        if(root==NULL) return;
+        rec(root->left);
+        prev->right = root;
+        root->left=prev;
+        prev=root;
+        // cout<<root->val<<" ";
+        rec(root->right);
+    }
+};
