b8af48039c507316c5310b1e5815c512c5081ff1
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-27 09:17:40 -0700

    Time: 108 ms (22.01%), Space: 20.4 MB (39.34%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int n=dominoes.size();
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(dominoes[i]=='R'){
+                count=1;
+            }else if(dominoes[i]=='.' && count!=0){
+                count++;
+            }else {
+                count=0;
+            }
+            right[i]=count;
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            if(dominoes[i]=='L'){
+                count=1;
+            }else if(dominoes[i]=='.' && count!=0){
+                count++;
+            }else {
+                count=0;
+            }
+            left[i]=count;
+        }
+        
+        for(int i=0;i<n;i++){
+            char ch='.';
+            if(left[i]==right[i]){
+                ch='.';
+            }else if(left[i]==0 || (right[i]!=0 && right[i]<left[i])){
+                ch='R';
+            }else if(right[i]==0 || left[i]<right[i]){
+                ch='L';
+            }
+            dominoes[i]=ch;
+        }
+        return dominoes;
+    }
+};
