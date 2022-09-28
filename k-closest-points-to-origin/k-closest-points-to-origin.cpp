45b2d2d1d8869386742ce660841642dd3ea1e208
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-28 09:27:05 -0700

    Time: 466 ms, Memory: 64.1 MB - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double,int>> maxHeap;
+        priority_queue<pair<double,int>> max_heap;
         for(int i=0;i<points.size();i++){
-            double numSquare = pow(points[i][0],2) + pow(points[i][1],2);
-            maxHeap.push({numSquare,i});
-            if(maxHeap.size()>k) maxHeap.pop();
+            int x = points[i][0];
+            int y = points[i][1];
+            double val = sqrt(pow(x,2)+pow(y,2));
+            max_heap.push({val,i});
+            if(max_heap.size()>k){
+                max_heap.pop();
+            }
         }
-        
         vector<vector<int>> result;
-        while(!maxHeap.empty()){
-            result.push_back(points[maxHeap.top().second]);
-            maxHeap.pop();
+        while(!max_heap.empty()){
+            result.push_back(points[max_heap.top().second]);
+            max_heap.pop();
         }
         return result;
     }
