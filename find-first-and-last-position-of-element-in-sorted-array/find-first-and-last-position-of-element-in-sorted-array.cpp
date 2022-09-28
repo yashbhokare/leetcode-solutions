737b0adbff30088833d4294ac0fc55f2cb1e7f0c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-27 19:37:37 -0700

    Time: 7 ms, Memory: 13.7 MB - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int f = binarySearch(nums,target,true);
-        if(f==-1) return {-1,-1};
-        int l = binarySearch(nums,target,false);
-        return {f,l};
+        int first=binary_search(nums,target,true);
+        if(first==-1) return {-1,-1};
+        int last = binary_search(nums,target,false);
+        return {first,last};
     }
     
-    int binarySearch(vector<int>& nums,int target,bool firstOccurence){
-        int start = 0;
+    int binary_search(vector<int>& nums,int target,bool is_first_occurance){
+        int start=0;
         int end = nums.size()-1;
-        int result = -1;
+        
         while(start<=end){
             int mid = start+(end-start)/2;
             if(nums[mid]==target){
-                if(firstOccurence){
-                    if(start==mid || nums[mid-1]!=target){
+                if(is_first_occurance){
+                    if((mid>0 && nums[mid-1]!=nums[mid]) || start==mid){
                         return mid;
+                    }else {
+                        end = mid-1;
                     }
-                    end= mid-1;
+                }else {
+                    if((mid<nums.size()-1 && nums[mid+1]!=nums[mid]) || end==mid){
+                        return mid;
+                    }else {
+                        start = mid+1;
+                    }
                 }
-            else {
-                if(end==mid || nums[mid+1]!=target){
-                    return mid;
-                }
-                start=mid+1;
+            }else if(nums[mid]>target){
+                end = mid-1;
+            }else {
+                start = mid+1;
             }
-            }
-        else if(nums[mid]<target){
-            start=mid+1;
-        }else {
-            end = mid-1;
         }
-            
-        }
-        return result;
+        return -1;
     }
 };
