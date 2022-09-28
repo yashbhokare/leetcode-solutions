6054717f88765b7fdd0c8167bec0e964a802e209
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-27 20:24:06 -0700

    Time: 353 ms (6.88%), Space: 59.7 MB (66.04%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -5,7 +5,7 @@ public:
         int right =arr.size()-1;
         while(left<right){
             int mid = left+(right-left)/2;
-            if(arr[mid-1]<arr[mid]){
+            if(arr[mid]>arr[mid-1]){
                 left=mid+1;
             }else {
                 right=mid;
