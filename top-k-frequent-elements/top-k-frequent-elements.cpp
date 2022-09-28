9d80e4758142dddf09b4ddbc01ae6d895de5789c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-28 09:36:20 -0700

    Time: 45 ms, Memory: 13.7 MB - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,36 +1,22 @@
-typedef pair<int, int> pi;
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> map;
-        for(int num : nums){
-            map[num]++;
+        unordered_map<int,int> mapper;
+        for(auto num:nums) mapper[num]++;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> min_heap;
+        
+        for(auto it=mapper.begin();it!=mapper.end();it++){
+            min_heap.push({it->second,it->first});
+            if(min_heap.size()>k) min_heap.pop();
         }
         
-        // vector<int> res;
-        // // pair<first, second>: first is frequency,  second is number
-        // priority_queue<pair<int,int>> pq; 
-        // for(auto it = map.begin(); it != map.end(); it++){
-        //     pq.push(make_pair(it->second, it->first));
-        //     if(pq.size() > (int)map.size() - k){
-        //         res.push_back(pq.top().second);
-        //         pq.pop();
-        //     }
-        // }
-
-        priority_queue<pi,vector<pi>,greater<pi>> pq1; 
-        for(auto it = map.begin(); it != map.end(); it++){
-            pq1.push(make_pair(it->second, it->first));
-            if(pq1.size() > k){
-                pq1.pop();
-            }
-        }
+        vector<int> result;
         
-        vector<int> res2;
-        while(k--){
-            res2.insert(res2.begin(),pq1.top().second);
-            pq1.pop();
+        while(!min_heap.empty()){
+            result.push_back(min_heap.top().second);
+            min_heap.pop();
         }
-        return res2;
+        return result;
     }
 };
