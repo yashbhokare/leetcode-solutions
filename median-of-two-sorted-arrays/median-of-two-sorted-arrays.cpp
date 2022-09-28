8fe1e5daafa32817d1df56990e3bc98c7c326c8e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-28 06:55:43 -0700

    Time: 74 ms, Memory: 89.3 MB - LeetHub

---
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int x = nums1.size();
+        int y = nums2.size();
+        if(x>y){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int low=0;
+        int high = x;
+        while(low<=high){
+            int posX = (low + high)/2;
+            int posY = (x+y+1)/2 - posX;
+            
+            int maxLeftX = posX==0 ? INT_MIN : nums1[posX-1];
+            int minRightX = posX==x ? INT_MAX : nums1[posX];
+            
+            int maxLeftY = posY==0 ? INT_MIN : nums2[posY-1];
+            int minRightY = posY==y ? INT_MAX : nums2[posY];
+            
+            if(maxLeftX<=minRightY && maxLeftY<=minRightX){
+                
+                // cout<<maxLeftX<<minRightY<<endl;
+                // cout<<maxLeftY<<minRightX<<endl;
+                int leftMax = max(maxLeftX,maxLeftY);
+                int rightMax = min(minRightX,minRightY);
+                
+                if((x+y)%2!=0){
+                    return leftMax;
+                }else {
+                    double result= (leftMax+rightMax)/2.0;
+                    return result;
+                }
+            }else if(maxLeftX>minRightY){
+                high = posX-1;
+            }else {
+                low=posX+1;
+            }
+        }
+        return -1;
+    }
+};
