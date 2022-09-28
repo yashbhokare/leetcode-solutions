7df92f548c88a67ff7ed6d13a565a5e6c70badf8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-27 20:01:38 -0700

    Time: 21 ms, Memory: 7.3 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        unordered_multiset<char> stringSet;
-        for(int i =0;i<s.length();i++){
-            stringSet.insert(s[i]);
+        if(s.size()!=t.size()) return false;
+
+        vector<int> chars(26,0);
+        for(int i=0;i<s.size();i++){
+            chars[s[i]-'a']++;
+            chars[t[i]-'a']--;
         }
-        
-        for(int i =0;i<t.length();i++){
-            auto it = stringSet.find(t[i]);
-            if(it == stringSet.end()){
-                return false;
-            } else {
-                stringSet.erase(it);
-            }
+        for(int i=0;i<26;i++){
+            if(chars[i]!=0) return false;
         }
-        if(stringSet.empty()) return true;
-        return false;;
+        return true;
     }
 };
-// Better Solution as compared to run time
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-//         if (s.length() != t.length()) return false;
-//         int n = s.length();
-//         int counts[26] = {0};
-//         for (int i = 0; i < n; i++) { 
-//             counts[s[i] - 'a']++;
-//             counts[t[i] - 'a']--;
-//         }
-//         for (int i = 0; i < 26; i++)
-//             if (counts[i]) return false;
-//         return true;
-//     }
-// };
