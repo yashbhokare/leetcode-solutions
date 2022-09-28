e4b01d3f44ee174baf82c4eab81bb55f945f4e42
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-28 09:55:14 -0700

    Time: 53 ms, Memory: 12.2 MB - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.cpp
+++ meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -4,12 +4,11 @@ public:
         sort(intervals.begin(),intervals.end());
         priority_queue<int,vector<int>,greater<int>> min_heap;
         for(auto interval:intervals){
-            if(min_heap.empty() || min_heap.top()>interval[0]){
-                min_heap.push(interval[1]);
-            }else {
-                min_heap.pop();
-                min_heap.push(interval[1]);
+            if(!min_heap.empty() && min_heap.top()<=interval[0]){
+                 min_heap.pop();
             }
+                min_heap.push(interval[1]);
+    
         }
         return min_heap.size();
     }
