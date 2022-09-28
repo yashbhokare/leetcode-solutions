85b6c15424965ae6d14e8f0df298cc04b3ade042
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-28 09:53:43 -0700

    Time: 39 ms, Memory: 12.3 MB - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.cpp
+++ meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        if(intervals.size()<=1) return 1; 
-        priority_queue<int,vector<int>,greater<int>> minHeap;
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<intervals.size();i++){
-            if(minHeap.size() == 0 || minHeap.top() > intervals[i][0]){
-                minHeap.push(intervals[i][1]);
+        priority_queue<int,vector<int>,greater<int>> min_heap;
+        for(auto interval:intervals){
+            if(min_heap.empty() || min_heap.top()>interval[0]){
+                min_heap.push(interval[1]);
             }else {
-                minHeap.pop();
-                minHeap.push(intervals[i][1]);
+                min_heap.pop();
+                min_heap.push(interval[1]);
             }
         }
-        return minHeap.size();
+        return min_heap.size();
     }
 };
