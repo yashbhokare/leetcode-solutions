d52af0fa79a993a86d7da9efd16dcc005c2c7816
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-28 19:11:45 -0700

    Time: 45 ms (94.41%), Space: 31.8 MB (61.64%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,6 +1,55 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        return findClosestElementsUsingBinarySearch(arr,k,x);
+    }
+    
+    vector<int> findClosestElementsUsingBinarySearch(vector<int>& arr, int k, int x) {
+        
+        int n = arr.size();
+        //Binary Search to find the closest element
+        int left = 0;
+        int right= n-1;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(arr[mid]>=x){
+                right=mid;
+            }else {
+                left=mid+1;
+            }
+        }
+        
+        
+        left = left-1;
+      right = left+1;
+        while((right-left-1)<k){
+            
+            if(left==-1){
+                right++;
+                continue;
+            }
+            
+            if(right==n){
+                left--;
+                continue;
+            }
+            if(abs(x-arr[left])<=abs(x-arr[right])){
+                left--;
+            }else {
+                right++;
+            }
+        }
+        
+        vector<int> result;
+        for(int index=left+1;index<right;index++){
+            result.push_back(arr[index]);
+        }
+        
+        return result;
+
+    }
+    
+    vector<int> findClosestElementsUsingHeap(vector<int>& arr, int k, int x) {
         priority_queue<pair<int,int>> max_heap;
         for(int i=arr.size()-1;i>=0;i--){
             int diff = abs(arr[i]-x);
