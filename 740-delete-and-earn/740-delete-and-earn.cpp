3aad5dab21c7ad19b9dfd3883b46636405d86efe
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-29 07:35:41 -0700

    Time: 62 ms (8.67%), Space: 17.1 MB (11.17%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -14,15 +14,9 @@ public:
     int dp(int n){
         if(cache.find(n)!=cache.end()) return cache[n];
         if(n==0) return 0;
-        if(n==1) {
-            if(num_mapper.find(1)==num_mapper.end()) return 0;
-            else return num_mapper[1];
-        }
-        int gain = 0;
-        if(num_mapper.find(n)!=num_mapper.end()){
-            gain = num_mapper[n];
-        }
-
+        if(n==1) return num_mapper[1];
+        
+        int gain = num_mapper[n];
         cache[n] = max(gain+dp(n-2),dp(n-1));
         return cache[n];
     }
