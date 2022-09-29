46b19540938a02ee76644d7e4a91a1460a0a0f5b
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-29 06:35:14 -0700

    Time: 5 ms, Memory: 6.3 MB - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+    unordered_map<int,int> memo;
     int tribonacci(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 1;
-        vector<int> memo(n+1);
         memo[0] = 0;
         memo[1] = 1;
         memo[2] = 1;
-        for(int i=3;i<=n;i++){
-            memo[i] = memo[i-3]+memo[i-2]+memo[i-1];
-        }
+        return dp(n);
+    }
+    
+    int dp(int n){
+        if(memo.find(n)!=memo.end()) return memo[n];
+        memo[n] =dp(n-3) + dp(n-2) + dp(n-1);
         return memo[n];
     }
 };
