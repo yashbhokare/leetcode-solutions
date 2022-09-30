a9e5f86ce6b8bb43c8bf8372df53401e04d4f8df
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-29 20:28:01 -0700

    Time: 175 ms (44.88%), Space: 48.6 MB (72.92%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        return sortValuesAndCompare(nums);
+       // return hashTableApproach(nums);
+    }
+    
+    bool sortValuesAndCompare(vector<int> nums){
+        // Sorting approach
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size() -1;i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+        bool hashTableApproach(vector<int> nums){
+        // Using Hash Table 
+         unordered_set <int> numSet;
+        for(int i=0;i<nums.size();i++){
+            //  find returns end iterator if key is not found,
+            //  else it returns iterator to that key
+            if(numSet.find(nums[i]) == numSet.end()) {
+                numSet.insert(nums[i]);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
