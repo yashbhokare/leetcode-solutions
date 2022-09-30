ae084f03f923c0108145b9c3ad7bbd18a5abd6a5
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-30 10:48:07 -0700

    Time: 182 ms (30.61%), Space: 13.7 MB (25.39%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,68 +1,59 @@
 class Solution {
-public:
+public:  
+    int n;
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        n = nums.size();
         sort(nums.begin(),nums.end());
-        return kthSum(nums,target,0,nums.size()-1,4);
+        return kth(nums,0,n-1,target,4);
+
     }
+
+  
+
     
-     vector<vector<int>> kthSum(vector<int>& nums,long target,long start,long end,long k){
-       
-         if(start>=end){
-            return {};
-        }
-        // There are k remaining values to add to the sum. The 
-        // average of these values is at least target / k.
-        int average_value = target / k;
+    vector<vector<int>> kth(vector<int>& nums,int l,int r,long target,int k) {
+        if(l>r) return {};
         
-        // We cannot obtain a sum of target if the smallest value
-        // in nums is greater than target / k or if the largest 
-        // value in nums is smaller than target / k.
-        if  (nums[start] > average_value || average_value > nums[end]) {
-            return {};
-        };
-         
         if(k==2){
-            return twoSumSorted(nums,start,end,target);
-        }else {
-            vector<vector<int>> result;
-            for(long i=start;i<=end;i++){
-                if(i!=start && nums[i]==nums[i-1]) continue;
-                long sum = target - nums[i];
-                vector<vector<int>> ans = kthSum(nums,sum,i+1,end,k-1);
-                for(auto arr:ans){
-                    arr.push_back(nums[i]);
-                    result.push_back(arr);
-                }
-            }
-            return result;
+            return twoSum(nums,l,r,target);
         }
-       
-    }
-    
-    
-    vector<vector<int>> twoSumSorted(vector<int>& nums,long start,long end,long target){
+        
+        vector<vector<int>> ans;
+        for(int i=l;i<n;i++){
+            if(i!=l && nums[i]==nums[i-1]) continue;
+            
+            long new_target = target -  nums[i];
+            for(auto num:kth(nums,i+1,n-1,new_target,k-1)){
+                num.push_back(nums[i]);
+                ans.push_back(num);
+            }
+        }
+        return ans;
+    }    
+    vector<vector<int>> twoSum(vector<int>& nums,int l,int r,long target){
+        int left=l;
+        int right =r;
         vector<vector<int>> result;
-        long low=start;
-        long high =end;
-        while(low<high){
-            if(low!=start && nums[low]==nums[low-1]) {
-                low++;
+        while(left<right){
+            if(left!=l && nums[left]==nums[left-1]){
+                left++;
                 continue;
             }
-            if(high!=end && nums[high]==nums[high+1]){
-                high--;
+            
+            if(right!=r && nums[right]==nums[right+1]){
+                right--;
                 continue;
             }
-           
-            long sum = nums[low]+nums[high];
+            
+            long sum = nums[left]+nums[right];
             if(sum==target){
-                result.push_back({nums[low],nums[high]});
-                low++;
-                high--;
+                result.push_back({nums[left],nums[right]});
+                left++;
+                right--;
             }else if(sum>target){
-                high--;
+                right--;
             }else {
-                low++;
+                left++;
             }
         }
         return result;
