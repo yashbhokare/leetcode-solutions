5d93ff30af0a9d6f0ea77dc516e2d8f0aba43c20
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-30 10:45:50 -0700

    Time: 289 ms (24.26%), Space: 26 MB (18.30%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
+    int n;
     vector<vector<int>> threeSum(vector<int>& nums) {
         int target = 0;
-        int n = nums.size();
+        n = nums.size();
         sort(nums.begin(),nums.end());
+        return kth(nums,0,n-1,0,3);
+//         vector<vector<int>> ans;
+//         for(int i=0;i<n;i++){
+//             if(i!=0 && nums[i]==nums[i-1]) continue;
+            
+//             int new_target = target -  nums[i];
+//             for(auto num:twoSum(nums,i+1,n-1,new_target)){
+//                 num.push_back(nums[i]);
+//                 ans.push_back(num);
+//             }
+//         }
+//         return ans;
+    }
+    
+    vector<vector<int>> kth(vector<int>& nums,int l,int r,int target,int k) {
+        if(l>r) return {};
+        
+        if(k==2){
+            return twoSum(nums,l,r,target);
+        }
+        
         vector<vector<int>> ans;
-        for(int i=0;i<n;i++){
+        for(int i=l;i<n;i++){
             if(i!=0 && nums[i]==nums[i-1]) continue;
             
             int new_target = target -  nums[i];
-            for(auto num:twoSum(nums,i+1,n-1,new_target)){
+            for(auto num:kth(nums,i+1,n-1,new_target,k-1)){
                 num.push_back(nums[i]);
                 ans.push_back(num);
             }
         }
         return ans;
-    }
-    
+    }    
     vector<vector<int>> twoSum(vector<int>& nums,int l,int r,int target){
         int left=l;
         int right =r;
