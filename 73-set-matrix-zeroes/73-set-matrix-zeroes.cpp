f9306a94807007cf1deb0c0eb9d2c743ff6b85c6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-30 22:17:35 -0700

    Time: 16 ms (87.38%), Space: 13.3 MB (54.46%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool isfirstCol = false;
-        
+        bool isCol = false;
         for(int r=0;r<matrix.size();r++){
             
-            //Check if first column has 0 or not
-            if(matrix[r][0]==0) isfirstCol=true;
+            if(matrix[r][0]==0) isCol=true;
             
             for(int c=1;c<matrix[0].size();c++){
-                   if(matrix[r][c]==0){
-                       matrix[r][0]=0;
-                       matrix[0][c]=0;
-                   }
+                if(matrix[r][c]==0){
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
+                }
             }
         }
         
         for(int r=1;r<matrix.size();r++){
             for(int c=1;c<matrix[0].size();c++){
-                if(matrix[r][0] == 0 || matrix[0][c]==0) matrix[r][c]=0;
+                if(matrix[r][0]==0 || matrix[0][c]==0){
+                    matrix[r][c] =0;
+                }
             }
         }
         
-        // Check for first row
         if(matrix[0][0]==0){
-            for(int c=0;c<matrix[0].size();c++) matrix[0][c]=0;
+            for(int c=0;c<matrix[0].size();c++){
+                matrix[0][c]=0;
+            }
         }
-        
-        //Check for first column
-        if(isfirstCol){
-            for(int r=0;r<matrix.size();r++) matrix[r][0]=0;
+        if(isCol){
+            for(int r=0;r<matrix.size();r++){
+                matrix[r][0]=0;
+            }
         }
     }
 };
