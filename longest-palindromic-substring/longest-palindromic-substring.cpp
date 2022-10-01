92787e34dd4e97519ce700a6379a94d6f4b666e8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 06:16:47 -0700

    Time: 119 ms, Memory: 159.7 MB - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,110 +1,25 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
-        if(s.length()<=1) return s;
-        // return longestPalindromeUsingDP(s);
-        return longestPalindromeUsingExpandFromMiddle(s);
-        
-// Brute Force       
-//         if(s.length()<=1){
-//             return s;
-//         }
-//         string result;
-//         string finalResult = string(1,s[0]);
-//         for(int i=0;i<s.length();i++){
-//             result = string(1,s[i]);
-//             for(int j=s.length()-1;j>i;j--){
-//                 if(s[i]==s[j]){
-//                     result = s.substr(i,j-i+1);
-//                     if(isPalindrome(result) && result.length() > finalResult.length()){
-//                         finalResult = result;
-//                     }
-//                     if(finalResult.length() == s.length()){
-//                         return finalResult;
-//                     }
-//                 }
-//             }
-//         }
-        
-//         return finalResult;
-    }
-    
- // bool isPalindrome(string s){
- //     //  cout<<"Inside Value:"<<s<<endl;
- //     // for(int i=0,j=s.length()-1;i<s.length()/2;i++,j--){
- //     //    if(s[i]!=s[j]){
- //     //        return false;
- //     //    }     
- //     // }
- //     string reverseString = s;
- //     reverse(reverseString.begin(),reverseString.end());
- //     if(s == reverseString)  return true;
- //     return false;   
- // }
-    
-    string longestPalindromeUsingExpandFromMiddle(string s){
-        int start=0,maxLen=1;
-        for(int i=0;i<s.length()-1;i++){
-            int len1 = expandFromMiddle(s,i,i);
-            int len2 = expandFromMiddle(s,i,i+1);
+        int maxLen = 0;
+        int startIndex = 0;
+        for(int i=0;i<s.size();i++){
+            int len1 = expandFromMiddle(i,i,s);
+            int len2 = expandFromMiddle(i,i+1,s);
             int len = max(len1,len2);
-            if(len > maxLen){
-                start = i - ((len-1)/2);
+            if(len>maxLen){
                 maxLen = len;
+                startIndex = i-((len-1)/2);
             }
         }
-        return s.substr(start,maxLen);
+        return s.substr(startIndex,maxLen);
     }
     
-    int expandFromMiddle(string s,int left,int right){
-        if(s.empty() || left > right) return 0;
-        while(left>=0 && right<=s.length()-1 && s[left]==s[right]){
+    int expandFromMiddle(int left,int right,string s){
+        while(left>=0 && right<s.size() && s[left]==s[right]){
             left--;
             right++;
         }
-        return right - left - 1;
-    }
-    
-    
-    
-    string longestPalindromeUsingDP(string s){
-        int start =0,finalLength = 1;
-        int n = s.length()-1;
-        vector<vector<int>> tableMatrix(s.length(),vector<int>(s.length(),0));
-        
-        for(int i=0;i<n;i++){
-            if(s[i]==s[i+1]){
-                tableMatrix[i][i+1] = 1;
-                finalLength = 2;
-                start =i;
-            }
-            tableMatrix[i][i] = 1;
-        }
-        tableMatrix[n][n] = 1;
-        
-        for(int k=2;k<=n;k++){
-            for(int i=0;i<=n-k;i++){
-                int j = i+k;
-                if(s[i]==s[j] && tableMatrix[i+1][j-1]){
-                    tableMatrix[i][j] = 1;
-                    int tempLen = j-i+1;
-                    if(tempLen > finalLength ){
-                        finalLength = tempLen;
-                        start =i; 
-                    }
-                }
-            }
-        }
-        return s.substr(start,finalLength);
-    }
-    
-    void displayMatrix(vector<vector<int>> tableMatrix , int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cout<<tableMatrix[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+        return right-left-1;
     }
 };
