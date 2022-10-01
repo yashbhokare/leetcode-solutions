3f909fe32684615372f729ba952b16065ef99169
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 06:43:26 -0700

    Time: 19 ms (53.58%), Space: 13.4 MB (36.32%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,55 +1,32 @@
-// class Solution {
-// public:
-//     bool wordBreak(string s, vector<string>& wordDict) {
-//         unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
-//         queue<int> q;
-//         vector<int> visited(s.size());
-        
-//         q.push(0);
-//         while(!q.empty()){
-//             int start = q.front();
-//             q.pop();
-//             if(visited[start]) continue;
-            
-//             for(int end=start+1;end<=s.size();end++){
-//                 string newS = s.substr(start,end-start);
-//                 cout<<newS<<" ";
-//                 if(wordSet.find(newS)!=wordSet.end()){
-//                     q.push(end);
-//                     if(end==s.size()) return true;
-//                 }
-//             }
-//             // cout<<endl;
-//             visited[start] = 1;
-//         }
-        
-//         return false;
-//     }
-// };
-
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        set<string> word_set(wordDict.begin(), wordDict.end());
-        // In the memo table, -1 stands for the state not yet reached,
-        // 0 for false and 1 for true
-        vector<int> memo(s.length(), -1);
-        return wordBreakMemo(s, word_set, 0, memo);
-    }
-
-    bool wordBreakMemo(string& s, set<string>& word_set, int start, vector<int>& memo) {
-        if (start == s.length()) {
-            return true;
-        }
-        if (memo[start] != -1) {
-            return memo[start];
-        }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (word_set.find(s.substr(start, end - start)) != word_set.end() and
-                wordBreakMemo(s, word_set, end, memo)) {
-                return memo[start] = true;
+        unordered_set<string> words(wordDict.begin(),wordDict.end());
+        
+        queue<int> q;
+        unordered_set<int> visited;
+        q.push(0);
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                int start = q.front();
+                q.pop();
+                // if(visited.find(start)!=visited.end()) continue;
+                
+                for(int end =start+1;end<=s.size();end++){
+                    string curr = s.substr(start,end-start);
+                    // cout<<curr<<" ";
+                    if(words.find(curr)!=words.end() && visited.find(end)==visited.end()){
+                        cout<<curr<<" ";
+                        visited.insert(end);
+                        if(end==s.size()) return true;
+                        q.push(end);
+                    }
+                }
             }
+            cout<<endl;
         }
-        return memo[start] = false;
+        return false;
+        
     }
 };
