d800f5430cb4ec853179d6e554865052fc90d157
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-30 19:24:34 -0700

    Time: 16 ms (21.73%), Space: 7.4 MB (55.22%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        int index=0;
+        for(int i=0;i<s.size();i++){
+            if(isalnum(s[i])){
+                if(s[i]>='A' && s[i]<='Z'){
+                    s[i]=s[i]-'A'+'a';
+                }
+                s[index++]=s[i];
+            }
+        }
         int left=0;
-        int right=s.size()-1;
-        while(left<=right){
-            if(!isalnum(s[left])) {
-                left++;
-                continue;
-            }
-            if(!isalnum(s[right])) {
-                right--;
-                continue;
-            }
-            if(tolower(s[left])!=tolower(s[right])) return false;
+        int right=index-1;
+        while(left<right){
+            if(s[left]!=s[right]) return false;
             left++;
             right--;
         }
