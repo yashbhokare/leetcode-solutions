d1f75ddf0267e7e38f63683faa2b23bcffd7da93
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 07:39:39 -0700

    Time: 1066 ms, Memory: 64.8 MB - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    unordered_map<int,int> memo;
+    int ans = 0;
+    unordered_map<int,int> mapper;
     int coinChange(vector<int>& coins, int amount) {
         return rec(coins,amount);
     }
@@ -8,25 +9,22 @@ public:
     int rec(vector<int>& coins,int amount){
         if(amount==0){
             return 0;
-        }else if(amount<0){
+        }
+        if(amount<0){
             return -1;
         }
         
-        if(memo.find(amount)!=memo.end()){
-            return memo[amount];
-        }
-        int ans = INT_MAX;
-        for(auto coin:coins){
-            int val = rec(coins,amount-coin);
-            if(val>-1 && val<ans){
-                ans = val;
+        if(mapper.find(amount)!=mapper.end()) return mapper[amount];
+        
+        int min = INT_MAX;
+        for(int i=0;i<coins.size();i++){
+            int res= rec(coins,amount-coins[i]);
+            if(res>=0 && res<min){
+                min=res+1;
             }
         }
-        if(ans==INT_MAX){
-            ans=-2;
-        }
-        memo[amount] = ans+1;
-        return ans+1;
-
+        min = min==INT_MAX ? -1:min;
+        mapper[amount] = min;
+        return mapper[amount];
     }
 };
