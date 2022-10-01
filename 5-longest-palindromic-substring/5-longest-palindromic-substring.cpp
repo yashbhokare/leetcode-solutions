300930a167ec2d3d5efe4a2dc30a45853bbae8f9
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-09-30 19:35:50 -0700

    Time: 47 ms (64.67%), Space: 159.6 MB (22.84%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int maxLen = 0;
-        int startIndex = 0;
-        string result = "";
+        int max_length = 0;
+        int start_index=0;
         for(int i=0;i<s.size();i++){
-            int len1 = expandMiddle(s,i,i);
-            int len2 = expandMiddle(s,i,i+1);
-            int len = max(len1,len2);
-            if(len>maxLen){
-                maxLen = len;
-                startIndex = i-((len+1)/2)+1;
-                 // startIndex = i -(len-1)/2;
+            int len1 = expandFromMiddle(i,i,s);
+            int len2 = expandFromMiddle(i,i+1,s);
+            int maxLen = max(len1,len2);
+            if(maxLen>max_length){
+                max_length = maxLen;
+                start_index = i-(maxLen-1)/2;
             }
         }
-        return s.substr(startIndex,maxLen);
+        return s.substr(start_index,max_length);
     }
     
-    int expandMiddle(string s,int left,int right){
-        while(left>=0 && right<s.size() && s[left]==s[right]){
+    int expandFromMiddle(int left,int right,string s){
+        int n = s.size();
+        while(left>=0 && right<n && s[left]==s[right]){
             left--;
             right++;
         }
