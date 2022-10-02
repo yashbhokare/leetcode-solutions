cbaac3b456229006497ff9f21be3e70ea24d9d11
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-02 16:49:07 -0700

    Time: 39 ms (62.28%), Space: 6.2 MB (74.37%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
-   int dp[31][1001] = {};
-int numRollsToTarget(int d, int f, int target, int res = 0) {
-  if (d == 0 || target <= 0) return d == target;
-  if (dp[d][target]) return dp[d][target] - 1;
-  for (auto i = 1; i <= f; ++i)
-    res = (res + numRollsToTarget(d - 1, f, target - i)) % 1000000007;
-  dp[d][target] = res + 1;
-  return res;
-}
+    int mod = 1000000007;
+    int memo[31][1001] = {};
+    int dp[32][1002];
+    // unordered_map<int,unordered_map<int,int>> memo;
+    int numRollsToTarget(int n, int k, int target) {
+        memset(dp,-1,sizeof(dp));
+        return rec(n,k,target);
+    }
+    
+    int rec(int n,int k,int current){
+        
+        // if (n == 0 || current <= 0) return n == current;
+        if(n==0 && current==0 ){
+            return 1;
+        }
+        if(n<=0 || current<0){
+            return 0;
+        }
+
+        if(dp[n][current]!=-1) return dp[n][current];
+        
+        if(memo[n][current]==1) return 0;
+
+        int ans = 0;
+       
+        for(int nums=1;nums<=k;nums++){
+            ans=(ans%mod+rec(n-1,k,current-nums)%mod)%mod;
+        }
+        dp[n][current]=ans;
+        return dp[n][current];
+    }
 };
