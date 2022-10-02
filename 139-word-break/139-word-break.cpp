6fc1d3e2d921157ed99696a3e6cb4753755ff802
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 20:50:08 -0700

    Time: 35 ms (31.33%), Space: 13.3 MB (36.15%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -14,14 +14,12 @@ public:
                 for(int end =start+1;end<=s.size();end++){
                     string curr = s.substr(start,end-start);
                     if(words.find(curr)!=words.end() && visited.find(end)==visited.end()){
-                        cout<<curr<<" ";
                         visited.insert(end);
                         if(end==s.size()) return true;
                         q.push(end);
                     }
                 }
             }
-            cout<<endl;
         }
         return false;
         
