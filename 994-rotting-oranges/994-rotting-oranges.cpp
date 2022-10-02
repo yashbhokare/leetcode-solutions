eac5fe3a765aa6e37a1a91705c86161e8e8db8e8
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-02 07:45:30 -0700

    Time: 7 ms (88.42%), Space: 13.4 MB (26.92%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
+    vector<vector<int>> dir = {
+        {0,1},{1,0},{-1,0},{0,-1}  
+    };
     int orangesRotting(vector<vector<int>>& grid) {
-        return bfs(grid);
-    }
-    
-    int bfs(vector<vector<int>>& grid){
+        int total_fresh_oranges = 0;
         queue<pair<int,int>> q;
-        int totalFreshOranges = 0;
-        for(int r=0;r<grid.size();r++){
-            for(int c=0;c<grid[0].size();c++){
-                if(grid[r][c]==1) totalFreshOranges++;
-                else if(grid[r][c]==2) q.push({r,c});
-            }
-        }
-        
-        if(totalFreshOranges==0) return 0;
-        int minutes = 0;
-        while(!q.empty()){
-            int size = q.size();
-            minutes++;
-            while(size--){
-                int r = q.front().first;
-                int c = q.front().second;
-                q.pop();
-                for(int i=0;i<4;i++){
-                    int newR = r + dir[i][0];
-                    int newC = c + dir[i][1];
-                    
-                    if(newR>=0 && newC>=0 && newR<grid.size() && newC<grid[0].size() && grid[newR][newC]==1){
-                        totalFreshOranges--;
-                        grid[newR][newC] = 2;
-                        q.push({newR,newC});
-                        if(totalFreshOranges==0) return minutes;
-                    }
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }else if(grid[i][j]==1){
+                    total_fresh_oranges++;
                 }
-                // if(totalFreshOranges==0) return minutes;
             }
         }
-        if(totalFreshOranges==0) return minutes;
-        return -1;
+    // if(q.size()==0) return -1;
+    
+    if(total_fresh_oranges==0) return 0;
+    int min = 0;
+    while(!q.empty()){
+        int size = q.size();
+        while(size--){
+            int x = q.front().first;
+            int y =  q.front().second;
+            q.pop();
+            for(int d=0;d<4;d++){
+                int newX = x + dir[d][0];
+                int newY = y + dir[d][1];
+                if(newX>=0 && newX<grid.size() && newY>=0 && newY<grid[0].size() && grid[newX][newY]==1){
+                    grid[newX][newY]=2;
+                    q.push({newX,newY});
+                    total_fresh_oranges--;
+                    if(total_fresh_oranges==0) return min+1;
+                }
+            }
+        }
+        min++;
+    }
+    return -1;
     }
 };
