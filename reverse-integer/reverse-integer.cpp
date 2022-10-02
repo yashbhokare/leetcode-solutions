5a0e079c0ddb5574e04542d81730492ae43d895f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 18:33:22 -0700

    Time: 6 ms, Memory: 5.8 MB - LeetHub

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int reverse(int x) {
-
         int result = 0;
         while(x!=0){
-            if((result > INT_MAX/10) || (result == INT_MAX/10 && x%10 > 7 )) return 0;
-            if((result < INT_MIN/10) || (result == INT_MIN/10 && x%10 < -8)) return 0;
-            result = (result*10) + (x%10);
-            x = x/10;
-            // cout<<x<<endl;
+            if((result>INT_MAX/10) || (result==(INT_MAX/10) && x>=INT_MAX%10)) return 0;
+            if((result<INT_MIN/10) || (result==(INT_MIN/10) && x<=INT_MIN%10)) return 0;
+            result = result*10 + x%10;
+            x=x/10;
         }
-
         return result;
     }
 };
