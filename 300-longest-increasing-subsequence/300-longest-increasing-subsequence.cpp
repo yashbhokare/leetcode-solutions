8c28dd7f22b16f4d951eeb8c301399e9f637b7fd
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 20:30:47 -0700

    Time: 13 ms (83.90%), Space: 10.4 MB (67.05%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,18 +1,34 @@
-class Solution {
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         vector<int> dp(nums.size(),1);
+//         int longest = 1;
+//         for(int i=0;i<nums.size();i++){
+//             int num = nums[i];
+//             for(int j=0;j<i;j++){
+//                 int curr_num = nums[j];
+//                 if(curr_num<num){
+//                     dp[i] = max(dp[i],dp[j]+1);
+//                     longest = max(longest,dp[i]);
+//                 }
+//             }
+//         }
+//         return longest;
+//     }
+// };
+
+class Solution { // 8 ms, faster than 91.61%
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(),1);
-        int longest = 1;
-        for(int i=0;i<nums.size();i++){
-            int num = nums[i];
-            for(int j=0;j<i;j++){
-                int curr_num = nums[j];
-                if(curr_num<num){
-                    dp[i] = max(dp[i],dp[j]+1);
-                    longest = max(longest,dp[i]);
-                }
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub.back() < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the smallest number >= x
+                *it = x; // Replace that number with x
             }
         }
-        return longest;
+        return sub.size();
     }
 };
