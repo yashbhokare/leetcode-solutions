f8093ea87aa9b351856bcd49283b7a1842a2b96f
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-02 11:12:04 -0700

    Time: 89 ms, Memory: 12.4 MB - LeetHub

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -1,35 +1,33 @@
-class Solution {
-public:  
-    vector<int> result;
-    vector<vector<int>> edges;
-    vector<int> inDegree;
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        edges.resize(numCourses);
-        inDegree.resize(numCourses,0);
-        for(auto pre:prerequisites){
-            edges[pre[1]].push_back(pre[0]);
-            inDegree[pre[0]]++;
-        }
+// class Solution {
+// public:  
+//     vector<int> result;
+//     vector<vector<int>> edges;
+//     vector<int> inDegree;
+//     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//         edges.resize(numCourses);
+//         inDegree.resize(numCourses,0);
+//         for(auto pre:prerequisites){
+//             edges[pre[1]].push_back(pre[0]);
+//             inDegree[pre[0]]++;
+//         }
         
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0) dfs(i);
-        }
-        if(result.size()==numCourses) return result;
-        return {};
-        
-        
-    }
+//         for(int i=0;i<numCourses;i++){
+//             if(inDegree[i]==0) dfs(i);
+//         }
+//         if(result.size()==numCourses) return result;
+//         return {};
+//     }
     
-    void dfs(int course){
-        result.push_back(course);
-        inDegree[course]=-1;
-        for(auto child:edges[course]){
-            if(--inDegree[child]==0){
-                dfs(child);
-            }
-        }
-    }
-};
+//     void dfs(int course){
+//         result.push_back(course);
+//         inDegree[course]=-1;
+//         for(auto child:edges[course]){
+//             if(--inDegree[child]==0){
+//                 dfs(child);
+//             }
+//         }
+//     }
+// };
 
 // class Solution {
 // public:
@@ -78,3 +76,36 @@ public:
 //         return result;
 //     }
 // };
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> result;
+        vector<int> indegree(numCourses);
+        queue<int> zeroDegree;
+        for (vector<int>& pre : prerequisites) {
+            indegree[pre[0]]++;
+        }
+        for (int i = 0; i < indegree.size(); i++) {
+            if (indegree[i] == 0) {
+                zeroDegree.push(i);
+            }
+        }
+        int index = 0;
+        while (!zeroDegree.empty()) {
+            int course = zeroDegree.front();
+            zeroDegree.pop();
+            result.push_back(course);
+            for (vector<int>& pre : prerequisites) {
+                if (pre[1] == course) {
+                    indegree[pre[0]]--;
+                    if (indegree[pre[0]] == 0) {
+                        zeroDegree.push(pre[0]);
+                    }
+                }
+            }
+        }
+        if(result.size()==numCourses) return result;
+        return {};
+    }
+};
