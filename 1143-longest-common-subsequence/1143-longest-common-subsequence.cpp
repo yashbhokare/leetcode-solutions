248c7bd9ffdea8da8cfb16850cab352a5f1002f6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 20:32:48 -0700

    Time: 162 ms (5.15%), Space: 261.8 MB (12.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,13 +5,19 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         s1=text1;
         s2=text2;
+        // Make the memo big enough to hold the cases where the pointers
+        // go over the edges of the strings.
         memo.resize(1000,vector<int>(1000,-1));
+        // We need to initialise the memo array to -1's so that we know
+        // whether or not a value has been filled in
         return dp(0,0);
     }
     
     int dp(int p1,int p2){
         
         if(p1>=s1.size() || p2>=s2.size()) return 0;
+        
+        // Check whether or not we've already solved this subproblem.
         if(memo[p1][p2]!=-1) return  memo[p1][p2];
         
         int ans=0;
