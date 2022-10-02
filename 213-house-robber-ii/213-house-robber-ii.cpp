91240b25aba0929eb7805f3415966f84ea770149
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 21:50:05 -0700

    Time: 7 ms (15.12%), Space: 8.9 MB (6.86%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int rob(vector<int>& nums) {
+        if(nums.size()==0) return 0; 
+        if(nums.size()==1) return nums[0]; 
+        
+        unordered_map<int,int> memo;
+        int l1 = dp(nums,0,nums.size()-2,memo);
+        unordered_map<int,int> memo2;
+        int l2 = dp(nums,1,nums.size()-1,memo2);
+        return max(l1,l2);
+    }
+    
+    int dp(vector<int>& nums,int start,int i,unordered_map<int,int>& memo){
+
+        if(i==start) return nums[start];
+        if(i==start+1) return max(nums[start],nums[start+1]);
+        if(memo.find(i)!=memo.end()) return memo[i];
+        int amount = nums[i];
+        memo[i] = max(dp(nums,start,i-2,memo)+amount,dp(nums,start,i-1,memo));
+
+        
+        
+        return memo[i];
+    }
+};
