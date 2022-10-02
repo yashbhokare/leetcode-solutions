c4a5f82b008c3dab8071bdddedee5bbe4e80c2f1
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-01 20:48:48 -0700

    Time: 49 ms (36.26%), Space: 13 MB (57.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -41,7 +41,6 @@ public:
         vector<vector<int>> dp(m+1,(vector<int>(n+1,0)));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                // cout<<i<<" "<<j<<endl;
                 if(text1[i]==text2[j]){
                     dp[i+1][j+1] = dp[i][j]+1;
                 }else {
