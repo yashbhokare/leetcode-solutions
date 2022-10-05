8fe24d7eba3a9055142848c155ba7ba55cbd83ba
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-05 09:19:40 -0700

    Time: 234 ms, Memory: 32 MB - LeetHub

--- word-ladder/word-ladder.cpp
+++ word-ladder/word-ladder.cpp
@@ -1,44 +1,48 @@
 class Solution {
 public:
+    unordered_map<string,vector<string>> mapper;
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,vector<string>> wordMap;
-        unordered_set<string> visited;
-        queue<string> q;
-        
         for(auto word:wordList){
+            string curr_word = word;
             for(int i=0;i<word.size();i++){
-                string temp = word;
-                int currCh = word[i];
-                word[i] ='*';
-                wordMap[word].push_back(temp);
-                word[i] = currCh;
+                char temp = word[i];
+                word[i]='*';
+                mapper[word].push_back(curr_word);
+                word[i] = temp;
             }
         }
-        
-       
+        return bfs(beginWord,endWord);
+    }
+    
+    int bfs(string beginWord, string endWord){
+        queue<string> q;
+        unordered_set<string> visited;
         q.push(beginWord);
         visited.insert(beginWord);
-        
+         cout<<beginWord<<endl;
         int result = 1;
         while(!q.empty()){
             int size = q.size();
             while(size--){
-                string curr = q.front();
+                string word = q.front();
+                if(word==endWord){
+                    return result+1;
+                }
                 q.pop();
-                if(curr==endWord) return result+1;
-                for(int i=0;i<curr.size();i++){
-                    int currCh = curr[i];
-                    curr[i] ='*';
-                    if(wordMap.find(curr)!=wordMap.end()){
-                        for(auto word:wordMap[curr]){
-                            if(visited.find(word)==visited.end()){
-                                q.push(word);
-                                visited.insert(word);
-                                if(word==endWord) return result+1;
+                for(int i=0;i<word.size();i++){
+                    char temp = word[i];
+                    word[i]='*';
+                    for(auto list_word:mapper[word]){
+                        if(visited.find(list_word)==visited.end()){
+                            cout<<list_word<<" ";
+                            q.push(list_word);
+                            visited.insert(list_word);
+                            if(list_word==endWord){
+                                return result+1;
                             }
                         }
                     }
-                    curr[i] = currCh;
+                    word[i] = temp;
                 }
             }
             cout<<endl;
