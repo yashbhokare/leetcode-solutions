24b0ec8fac53e867629c5bda1233878c46223c4e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-05 16:41:16 -0700

    Time: 1027 ms (5.01%), Space: 65.2 MB (6.15%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    int ans = 0;
-    unordered_map<int,int> mapper;
+    unordered_map<int,int> memo;
     int coinChange(vector<int>& coins, int amount) {
         return rec(coins,amount);
     }
     
-    int rec(vector<int>& coins,int amount){
-        if(amount==0){
-            return 0;
-        }
-        if(amount<0){
-            return -1;
-        }
+    int rec(vector<int>& coins,int remain){
+        if(remain<0) return -1;
+        if(remain==0) return 0;
+        if(memo.find(remain)!=memo.end()) return memo[remain];
         
-        if(mapper.find(amount)!=mapper.end()) return mapper[amount];
-        
-        int min = INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int res= rec(coins,amount-coins[i]);
-            if(res>=0 && res<min){
-                min=res+1;
+        int res = INT_MAX;
+        for(auto coin:coins){
+            int val = rec(coins,remain-coin);
+            if(val>=0 && val<res){
+                res = val + 1;
             }
         }
-        min = min==INT_MAX ? -1:min;
-        mapper[amount] = min;
-        return mapper[amount];
+        if(res==INT_MAX) res=-1;
+        memo[remain] = res;
+        return memo[remain];
     }
 };
