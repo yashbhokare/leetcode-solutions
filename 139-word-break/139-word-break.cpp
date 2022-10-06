5e58bf68f4bef623ecb0553fb5db84d90d8d7f5b
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-05 17:19:06 -0700

    Time: 32 ms (35.36%), Space: 13.4 MB (35.91%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> words(wordDict.begin(),wordDict.end());
-        
-        queue<int> q;
+        unordered_set<string> word(wordDict.begin(),wordDict.end());
         unordered_set<int> visited;
+        queue<int> q;
         q.push(0);
         while(!q.empty()){
             int size = q.size();
             while(size--){
                 int start = q.front();
                 q.pop();
-                for(int end =start+1;end<=s.size();end++){
-                    string curr = s.substr(start,end-start);
-                    if(words.find(curr)!=words.end() && visited.find(end)==visited.end()){
-                        visited.insert(end);
-                        if(end==s.size()) return true;
+                for(int end=start+1;end<=s.size();end++){
+                    string new_word = s.substr(start,end-start);
+                    if(word.find(new_word)!=word.end() && visited.find(end)==visited.end()){
                         q.push(end);
+                        if(end==s.size()) return true;
+                        visited.insert(end);
                     }
                 }
             }
         }
         return false;
-        
     }
 };
