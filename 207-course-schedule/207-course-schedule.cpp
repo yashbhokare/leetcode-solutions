8bacbb0238f9caf0d85d5c825900e3a14a292faf
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-05 18:37:04 -0700

    Time: 69 ms (8.86%), Space: 16.4 MB (8.38%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,90 +1,41 @@
 class Solution {
 public:
-    unordered_set<int> visited;
     unordered_map<int,vector<int>> mapper;
-    unordered_map<int,bool> cache;
+    unordered_set<int> visited;
+    unordered_map<int,int> cache;
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-       
-        return canFinishUsingTopologicalSort(numCourses,prerequisites);
+        return canFinishUsingRecursion(numCourses,prerequisites);
     }
-    
-    bool canFinishUsingDFS(int numCourses, vector<vector<int>>& prerequisites) {
-        for(auto pre:prerequisites) mapper[pre[0]].push_back(pre[1]);
-        
-        for(int course=0;course<numCourses;course++){
-            if(isCycle(course)) return false;
-        }
-        return true;
-    }
+
+     bool canFinishUsingRecursion(int numCourses, vector<vector<int>>& prerequisites) {
+            for(auto pre:prerequisites){
+                mapper[pre[0]].push_back(pre[1]);
+            }
+         
+            for(int i=0;i<numCourses;i++){
+                if(isCycle(i)) return false;
+            }
+         return true;
+     }
     
     bool isCycle(int course){
-        //If already cached return that
-        if(cache.find(course)!=cache.end()) return cache[course];
-        
-        // Course does not exist on mapper so valid
-        // if(mapper.find(course)==mapper.end()) return false;
-        
-        //If course already visited return cycle found
         if(visited.find(course)!=visited.end()) return true;
         
-        // Mark as visited
-        visited.insert(course);
+        if(mapper.find(course)==mapper.end()) return false;
+        
+        if(cache.find(course)!=cache.end()) return cache[course];
+        
         bool res = false;
+        visited.insert(course);
         for(auto child:mapper[course]){
-            res=isCycle(child);
+            res = isCycle(child);
             if(res) break;
         }
-        
         visited.erase(course);
-        cout<<course<<endl;
+        
+        // cout<<course<<" ";
         cache[course] = res;
-        return res;
-        
-    }
-    
-    bool canFinishUsingTopologicalSort(int numCourses, vector<vector<int>>& prerequisites){
-        unordered_map<int,vector<int>> mapper;
-        vector<int> inDegree(numCourses,0);
-        vector<int> result;
-        queue<int> q;
-          for(auto pre:prerequisites) {
-             mapper[pre[1]].push_back(pre[0]);
-             inDegree[pre[0]]++;
-         }
-        
-        for(int i=0;i<inDegree.size();i++){
-            if(inDegree[i]==0) q.push(i);
-        }
-        
-        while(!q.empty()){
-            int course = q.front();
-            q.pop();
-            result.push_back(course);
-            for(auto child:mapper[course]){
-                inDegree[child]--;
-                if(inDegree[child]==0){
-                    q.push(child);
-                }
-            }
-        }
-        if(result.size()==numCourses) return true;
-        return false;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return cache[course];
         
     }
 };
