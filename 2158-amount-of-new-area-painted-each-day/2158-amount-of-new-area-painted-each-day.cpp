23445e970bdc8ec06765a2615e77fcf06eb8ff24
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-12 07:24:04 -0700

    Time: 908 ms (50.78%), Space: 131.2 MB (85.94%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -1,82 +1,26 @@
 class Solution {
 public:
-   
-    vector<int> workDone;
-    unordered_map<int,int> mapper;
-    vector<int> amountPainted(vector<vector<int>>& paint) {
-        for(auto p:paint){
-            workDone.push_back(calAmountPainted(p[0],p[1]));
+vector<int> amountPainted(vector<vector<int>>& pt) {
+    map<int, int> m;
+    vector<int> res;
+    for (auto &p : pt) {
+        int l = p[0], r = p[1];
+        auto next = m.upper_bound(l), cur = next;
+        if (cur != begin(m) && prev(cur)->second >= l) {
+            cur = prev(cur);
+            l = cur->second;
         }
-        return workDone;
-    }
-    
-    int calAmountPainted(int start, int end){
-        int work=0;
-        while(start<end){
-            // start not found
-            if(mapper.find(start)==mapper.end()){
-                mapper[start]=end;
-                work++;
-                start++;
-            }else {
-                int newStart = mapper[start];
-                mapper[start] = max(mapper[start],end);
-                start = newStart;
-            }
+        else 
+            cur = m.insert({l, r}).first;
+        int paint = r - l;
+        while (next != end(m) && next->first < r) {
+            paint -= min(r, next->second) - next->first;
+            r = max(r, next->second);
+            m.erase(next++);
         }
-        return work;
+        cur->second = max(cur->second, r);
+        res.push_back(max(0, paint));
     }
-
-
+    return res;
+}
 };
-
-
-// class Solution {
-// public:
-//     vector<int>par;
-//     int n;
-    
-//     void make_set(int v){
-//         par[v] = v;
-//     }
-    
-//     int find_set(int v){
-//         if(par[v]==v)
-//             return v;
-//         return par[v] = find_set(par[v]);
-//     }
-    
-//     void union_set(int u, int v){
-//         u = find_set(u);
-//         v = find_set(v);
-        
-//         par[u] =v;
-//     }
-    
-//     vector<int> amountPainted(vector<vector<int>>& paint) {
-//         int maxlen = 0;
-//         int minlen = INT_MAX;
-//         for(auto& v:paint){
-//             minlen = min(minlen, v[0]);
-//             maxlen = max(maxlen, v[1]);
-//         }
-//         par.resize(maxlen+2);
-        
-//         for(int i=minlen; i<=maxlen+1; i++){
-//             make_set(i);
-//         }
-//         n = paint.size();
-//         vector<int>ans(n,0);
-        
-//         for(int i=0; i< paint.size(); i++){
-//             int l = paint[i][0]; int r = paint[i][1];
-            
-//             for(l=find_set(l); l<r; l=find_set(l)){
-//                 ans[i]++;
-//                 union_set(l, l+1);
-//             }
-//         }
-        
-//         return ans;
-//     }
-// };
