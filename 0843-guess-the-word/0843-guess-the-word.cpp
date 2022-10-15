ad88db267a878dff1b5bad2422a0ef19515a0a7c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-14 20:13:07 -0700

    Time: 0 ms (100.00%), Space: 7.1 MB (16.46%) - LeetHub

--- 0843-guess-the-word/0843-guess-the-word.cpp
+++ 0843-guess-the-word/0843-guess-the-word.cpp
@@ -1,42 +1,32 @@
-
-
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
 class Solution {
 public:
-    void findSecretWord(vector<string>& words, Master& master) {
-        auto wordDiff = [](const string_view lhs, const string_view rhs){
-            return transform_reduce(begin(lhs), end(lhs), begin(rhs), 0, std::plus{}, [](auto lch, auto rch){return lch == rch;});
-        };
-        
-        int res;
-        string curr;
-        
-        auto zeroMatchCleanup = [&](){
-            while (res == 0)
-            {
-                words.erase(std::remove_if(words.begin(), words.end(), [&](auto val){return wordDiff(curr, val);}), words.end());
-                curr = words[words.size()/2];
-                res = master.guess(curr);
-            }
-        };
-        
-        curr = words[words.size()/2];
-        
-        res = master.guess(curr);
-        
-        zeroMatchCleanup();
-    
-        while (res < 6)
-        {
-            words.erase(std::remove_if(words.begin(), words.end(), [&](auto val){
-                return wordDiff(curr, val) != res || curr == val;
-            }), words.end());
-            
-            curr = words[words.size()/2];
-            
-            res = master.guess(curr);
-
-            zeroMatchCleanup();
-            
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        for(int i=0;i<30;i++){
+            string guessWord = wordlist[wordlist.size()/2];
+            int guessCount = master.guess(guessWord);
+            if(guessCount==6) break;
+            wordlist = compareTheCount(guessWord,guessCount,wordlist);
         }
+    }
+    
+    vector<string> compareTheCount(string guessWord,int guessCount,vector<string> wordlist){
+        vector<string> newWordList;
+        for(int i=0;i<wordlist.size();i++){
+            string currentWord = wordlist[i];
+            int counter = 0;
+            for(int j=0;j<currentWord.size();j++){
+                if(guessWord[j] == currentWord[j]) counter++;
+            }
+            if(counter == guessCount) newWordList.push_back(currentWord);
+        }
+        return newWordList;
     }
 };
