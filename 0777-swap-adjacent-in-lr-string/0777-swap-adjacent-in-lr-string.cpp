3a7077dc093ed7f9978e9716a16c9c6891bfa696
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-14 19:49:14 -0700

    Time: 7 ms (84.22%), Space: 7.8 MB (17.15%) - LeetHub

--- 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.cpp
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.cpp
@@ -13,7 +13,7 @@ public:
         while(i<m || j<n){
             while(i<m && start[i]=='X') i++;
             while(j<n && end[j]=='X') j++;
-            if(i==m && j==n) return true;
+            // if(i==m && j==n) return true;
             if(start[i]!=end[j]) return false;
             if(start[i]=='L' && j>i) return false;
             if(start[i]=='R' && j<i) return false;
@@ -21,29 +21,5 @@ public:
             j++;
         }
         return true;
-        
-        
-//         for(;i<start.size()  j<end.size();){
-//             if(start[i]=='X'){
-//                 i++;
-//                 continue;
-//             }
-//             if(end[j]=='X'){
-//                 j++;
-//                 continue;
-//             }
-            
-//             if(start[i]=='L' && end[j]=='L'){
-//                 i++;
-//                 j++;
-//             }else if(start[i]=='R' && end[j]=='R'){
-//                 i++;
-//                 j++;
-//             }else {
-//                 return false;
-//             }
-//         }
-//         if(i==start.size() && j==end.size()) return true;
-//         return false;
     }
 };
