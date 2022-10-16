cfa77978224b55f10252d15a5bc750c264fc5637
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-15 17:54:32 -0700

    Time: 0 ms, Memory: 6.3 MB - LeetHub

--- backspace-string-compare/backspace-string-compare.cpp
+++ backspace-string-compare/backspace-string-compare.cpp
@@ -7,17 +7,14 @@ public:
          string str1,str2; 
             
          for(int i = 0; i < S.size(); i++){
-                 
                  if(S[i] == '#' && !s1.empty())
                          s1.pop();
                  
                  else if(S[i] != '#')
                          s1.push(S[i]);
-                 
          }
          
          for(int i = 0; i < T.size(); i++){
-                 
                  if(T[i] == '#' && !s2.empty())
                          s2.pop();
                  
@@ -25,15 +22,13 @@ public:
                          s2.push(T[i]);
                  
          }
-            
          while(!s1.empty() && !s2.empty()){
-                 
                 if(s1.top()!=s2.top()) return false;
                 s1.pop();
                 s2.pop();
                  
          }
-  
+
          return s1.empty() && s2.empty();   
     }
 };
