5014e8e0d5e12a0e2cfe2cb09c8be0b46b006228
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-16 11:45:06 -0700

    Time: 127 ms, Memory: 40.4 MB - LeetHub

--- serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -12,21 +12,18 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(root==NULL){
-            return "NULL";
-        }
-        return to_string(root->val) + " "+serialize(root->left) +" "+ serialize(root->right);
-        
+        if(root==NULL) return "NULL";
+        return to_string(root->val)+" "+serialize(root->left)+" "+serialize(root->right);
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
         stringstream ss(data);
+        string word;
         return deserialize_string(ss);
-        return NULL;
     }
     
-    TreeNode* deserialize_string(stringstream& ss){
+    TreeNode* deserialize_string(stringstream& ss) {
         string word;
         ss>>word;
         if(word=="NULL"){
@@ -42,51 +39,3 @@ public:
 // Your Codec object will be instantiated and called as such:
 // Codec ser, deser;
 // TreeNode* ans = deser.deserialize(ser.serialize(root));
-
-// BFS with Queue
-// class Codec:
-//     '''       O(n) time and O(n) space, BFS traversal
-//     e.g., 1
-//          / \
-//         2   5
-//        / \
-//       3   4  , level order traversal, serialize will be '1,2,5,3,4,None,None,None,None,None,None,'; deserialize 
-//       with queue as well, convert back. Time and Space O(n).
-//     '''
-//     def serialize(self, root):
-//         if not root:
-//             return ''
-//         queue = collections.deque()
-//         queue.append(root)
-//         res = ''
-//         while queue:
-//             node = queue.popleft()
-//             if not node:
-//                 res += 'None,'
-//                 continue
-//             res += str(node.val) + ','
-//             queue.append(node.left)
-//             queue.append(node.right)
-//         return res
-            
-//     def deserialize(self, data):
-//         if not data:
-//             return None
-//         ls = data.split(',')
-//         root = TreeNode(int(ls[0]))
-//         queue = collections.deque()
-//         queue.append(root)
-//         i = 1
-//         while queue and i < len(ls):
-//             node = queue.popleft()
-//             if ls[i] != 'None':
-//                 left = TreeNode(int(ls[i]))
-//                 node.left = left
-//                 queue.append(left)
-//             i += 1
-//             if ls[i] != 'None':
-//                 right = TreeNode(int(ls[i]))
-//                 node.right = right
-//                 queue.append(right)
-//             i += 1
-//         return root
