9e957a2133ef32bd7c8ea40ea95e867cf68b0a7c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-16 13:26:45 -0700

    Time: 384 ms, Memory: 38.7 MB - LeetHub

---
+++ 00my-calendar-ii/00my-calendar-ii.cpp
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/my-calendar-ii/discuss/483337/c%2B%2B-with-map-very-simple
+
+class MyCalendarTwo {
+public:
+
+    map<int,int> mapper;
+    MyCalendarTwo() {
+        
+    }
+    
+    bool canAdd(int n){
+        int count = 0;
+        for(auto entry:mapper){
+            count+=entry.second;
+            if(count>2) return false;
+        }
+        return true;
+    }
+    
+    bool book(int start, int end) {
+        mapper[start]++;
+        mapper[end]--;
+        if(canAdd(2)) return true;
+        else {
+            mapper[start]--;
+            mapper[end]++;
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
