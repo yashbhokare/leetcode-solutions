d0171877d5cc791cf79c3808d4d8b00e2e9162bc
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-15 17:32:48 -0700

    Time: 6 ms, Memory: 8.2 MB - LeetHub

--- expressive-words/expressive-words.cpp
+++ expressive-words/expressive-words.cpp
@@ -1,76 +1,32 @@
-// class Solution {
-// public:
-//     int expressiveWords(string s, vector<string>& words) {
-//         unordered_map<int,char> charMap;
-//         unordered_map<int,int>  countMap;
-//         int curIndex=0;
-//         int res = 0;
-//         for(int i=0;i<s.size();i++){
-//             if(i!=0 && s[i]!=s[i-1]){
-//                 curIndex++;
-//             }
-//             charMap[curIndex] = s[i];
-//             countMap[curIndex]++;
-//         }
-        
-//         for(int i=0;i<=curIndex;i++){
-//             // cout<<i<<" "<<charMap[i]<<" "<<countMap[i]<<endl;
-//         }
-        
-//         // cout<<"******************\n";
-//         for(auto w:words){
-//             int index = 0;
-//             int count = 0;
-//             int i;
-//             // cout<<"\nWord:"<<w<<endl;
-//             for(i=0;i<=w.size();i++){
-//                  // cout<<s[i]<<endl;
-//                 if(i==w.size() || (i!=0 && w[i]!=w[i-1])){
-//                     cout<<index<<"->"<<w[i-1]<<" "<<countMap[index]<<" "<<count<<endl;
-//                     if(charMap[index]==w[i-1] && (countMap[index]==count || (countMap[index]>=3 && count<countMap[index]))){
-
-//                     }else {
-//                         // cout<<"Break"<<endl;
-//                         break;
-//                     }
-//                     index++;
-//                     count = 0;
-//                 }
-//                 count++;
-//             }
-//             if(i>w.size() && curIndex==index-1) res++;
-//         }
-        
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
-    bool isExpressive(string dst,string src) {
-        int i=0,j=0;
-
-        while(i<dst.size() && j<src.size()) {
-           if(dst[i] != src[j]) return false;
-		   int dstCount = 1,srcCount = 1;
-			// dst[++i] means increment the value of i then use it 
-            while(i<dst.size() && dst[i]==dst[++i]) { dstCount++;} 
-            while(j<src.size() && src[j]==src[++j]) { srcCount++;}
-
-            if(dstCount != srcCount && (srcCount > dstCount || dstCount <3))
-                return false;
+    int expressiveWords(string s, vector<string>& words) {
+        int count = 0;
+        for(auto word:words){
+            if(isExpressive(s,word)){
+                count++;
+            }
         }
-        return i==dst.size() && j==src.size();
+        return count;
     }
     
-    int expressiveWords(string S, vector<string>& words) {
-        int res = 0;
-        
-        for(auto w : words) {
-            res += isExpressive(S,w);
+    
+    bool isExpressive(string source,string dstn){
+        int i=0,j=0;
+        int sCount = 0,dCount = 0;
+        int n = source.size(),m=dstn.size();
+        while(i<n && j< m){
+            sCount = 1;
+            dCount = 1;
+            if(source[i]!=dstn[j]) return false;
+            while(i<n && source[i]==source[++i]) sCount++;
+            while(j<m && dstn[j]==dstn[++j]) dCount++;
+            if(sCount==dCount) continue;
+            if(dCount > sCount) return false;
+            if(sCount<3) return false;
         }
-        
-        return res;
-        
+        // cout<<dstn<<" ";
+        // cout<<i<<" "<<j<<endl;
+        return (i==n && j==m);
     }
 };
