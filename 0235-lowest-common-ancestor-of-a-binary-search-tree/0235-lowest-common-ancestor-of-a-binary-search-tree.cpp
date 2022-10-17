e8d4b5082fc7eebdf380489233651b2d391b4df0
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-16 19:41:13 -0700

    Time: 80 ms (5.84%), Space: 23.4 MB (16.78%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* P;
+    TreeNode* Q;
+    TreeNode* result;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        P=p;
+        Q=q;
+        return rec(root);
+        // rec(root);
+        // return result;
+    }
+    
+//     bool rec(TreeNode* root){
+//         if(root==NULL) return false;
+        
+        
+//         bool leftRes = rec(root->left);
+//         bool rightRes= rec(root->right);
+        
+//         if((root==P || root==Q) && (leftRes || rightRes)){
+//             result = root;
+//             return false;
+//         }
+//         else if(root==P || root==Q) return true;
+        
+//         if(leftRes && rightRes) {
+//             result = root;
+//             cout<<result->val;
+//         }else {
+//             return leftRes || rightRes;
+//         }
+//         return false;
+//     }
+    
+     TreeNode* rec(TreeNode* root){
+         if(P->val < root->val && Q->val < root->val){
+             return rec(root->left);
+         }else if(P->val > root->val && Q->val > root->val){
+             return rec(root->right);
+         }else {
+             return root;
+         }
+     }
+};
