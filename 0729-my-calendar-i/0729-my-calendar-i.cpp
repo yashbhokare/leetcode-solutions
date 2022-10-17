d13e690ad457c8e4eb01f5e9454350029e49dda9
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-16 17:45:16 -0700

    Time: 103 ms (92.82%), Space: 38.9 MB (32.99%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,26 +1,51 @@
-class MyCalendar {
-public:
-    map<int,int> mapper;
-    MyCalendar() {
+// class MyCalendar {
+// public:
+//     map<int,int> mapper;
+//     MyCalendar() {
         
-    }
+//     }
     
+//     bool book(int start, int end) {
+//         int count = 0;
+//         mapper[start]++;
+//         mapper[end]--;
+//         for(auto val:mapper){
+//             count+=val.second;
+//             if(count>1){
+//                 mapper[start]--;
+//                 mapper[end]++;
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+class MyCalendar {
+private:
+    set<pair<int,int>> calendar;
+    
+
+public:
     bool book(int start, int end) {
-        int count = 0;
-        mapper[start]++;
-        mapper[end]--;
-        for(auto val:mapper){
-            count+=val.second;
-            if(count>1){
-                mapper[start]--;
-                mapper[end]++;
-                return false;
-            }
+        
+        const pair<int,int> currEvent = {start,end};
+        
+        const auto nextEvent = calendar.lower_bound(currEvent);
+        
+        if(nextEvent!=calendar.end() && end > nextEvent->first){
+            return false;
         }
+        
+        if(nextEvent!=calendar.begin()){
+            auto prevEvent= prev(nextEvent);
+            if(start< prevEvent->second) return false;
+        }
+        
+        calendar.insert(currEvent);
         return true;
     }
 };
-
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar* obj = new MyCalendar();
