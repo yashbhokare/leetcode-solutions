f624bbf1067e527f5bbd0dea075919aeeaf213f2
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-18 16:54:57 -0700

    Time: 54 ms (21.55%), Space: 121.3 MB (15.35%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string res = "1";
+        for(int i=2;i<=n;i++){
+            int count = 1;
+            string ans ="";
+            for(int j=1;j<=res.size();j++){
+                if(j==res.size() || res[j]!=res[j-1]){
+                    ans = ans +  to_string(count)+ res[j-1];
+                    count = 1;
+                }else {
+                   count++;
+                }
+            }
+            res = ans;
+        }
+        return res;
+    }
+};
