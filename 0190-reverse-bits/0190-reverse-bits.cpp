80f00c201ab92ebe6b7525467727d143cdb8d82d
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-19 12:07:24 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (50.82%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        string res = bitset<32>(n).to_string();
+        reverse(res.begin(),res.end());
+        uint32_t r = bitset<32>(res).to_ulong();
+        return r;
+    }
+};
