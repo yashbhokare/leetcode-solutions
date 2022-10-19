20af0b255a6e71fbfb217b3fee428f6d242becda
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-19 12:09:43 -0700

    Time: 0 ms (100.00%), Space: 6 MB (10.14%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
+     uint32_t reverseBits(uint32_t n) {
+         return reverseBitsBitByBit(n);
+     }
+    
+    uint32_t reverseBitsUsingBitSet(uint32_t n) {
         string res = bitset<32>(n).to_string();
         reverse(res.begin(),res.end());
         uint32_t r = bitset<32>(res).to_ulong();
         return r;
+    }
+    
+    uint32_t reverseBitsBitByBit(uint32_t n) {
+        uint32_t ret = 0, power = 31;
+        while (n != 0) {
+          ret += (n & 1) << power;
+          n = n >> 1;
+          power -= 1;
+        }
+        return ret;
     }
 };
