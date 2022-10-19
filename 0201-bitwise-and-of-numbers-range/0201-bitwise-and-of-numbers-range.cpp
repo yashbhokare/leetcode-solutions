92143727de0254c4eed1e4cdda8be413012890ef
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2022-10-19 14:09:13 -0700

    Time: 29 ms (24.74%), Space: 6 MB (5.74%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
-    int rangeBitwiseAnd(int left, int right) {
-        int result = right;
-        for(int i=left;i<right;i++){
-            result = result&i;
-            if(result==0) return result;
+    // int rangeBitwiseAnd(int left, int right) {
+    //     int result = right;
+    //     for(int i=left;i<right;i++){
+    //         result = result&i;
+    //         if(result==0) return result;
+    //     }
+    //     return result;
+    // }
+    int rangeBitwiseAnd(int m, int n) {
+        int shift = 0;
+        // find the common 1-bits
+        while (m < n) {
+          m >>= 1;
+          n >>= 1;
+          ++shift;
         }
-        return result;
-    }
+        return m << shift;
+  }
 };
