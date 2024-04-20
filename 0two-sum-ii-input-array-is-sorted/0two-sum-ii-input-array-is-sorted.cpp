fcfb472322ddc25e33ed44504dcbf59b08f2d452
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-20 19:35:21 -0400

    Time: 3 ms, Memory: 18.1 MB - LeetHub

--- 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.cpp
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        unordered_map<int,int> map;
-        for(int i=0;i<numbers.size();i++){
-            int diff=target-numbers[i];
-            auto it=map.find(diff);
-            if(it!=map.end()) return {it->second+1,i+1};
-            map.insert({numbers[i],i});
+        int low=0;
+        int high=numbers.size()-1;
+        while(low<high){
+            int sum=numbers[low]+numbers[high];
+            if(sum==target) return {low+1,high+1};
+            else if(sum>target) high--;
+            else low++;
         }
         return {};
     }
+    // vector<int> twoSum(vector<int>& numbers, int target) {
+    //     unordered_map<int,int> map;
+    //     for(int i=0;i<numbers.size();i++){
+    //         int diff=target-numbers[i];
+    //         auto it=map.find(diff);
+    //         if(it!=map.end()) return {it->second+1,i+1};
+    //         map.insert({numbers[i],i});
+    //     }
+    //     return {};
+    // }
 };
