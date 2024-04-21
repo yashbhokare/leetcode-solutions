6738996704aafdace9248ed8d50279ef0e0b38fe
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-20 21:40:39 -0400

    Time: 19 ms, Memory: 27.3 MB - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,51 +1,9 @@
 class Solution {
 public:
-    void swap(vector<int>& nums, int posA, int posB){
-        int temp = nums[posA];
-        nums[posA]= nums[posB];
-        nums[posB] = temp;
-    }
-    
     void rotate(vector<int>& nums, int k) {
-        // reverse(nums.begin(),nums.end());
-        // reverse(nums.begin(),nums.begin()+k);
-        // reverse(nums.begin()+k,nums.end());
-        cyclicReplacements(nums,k);
-        // reverseApproach(nums,k);
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
-    
-    void cyclicReplacements(vector<int>& nums, int k){
-        //Using Cyclic Replacements
-            k = k % nums.size();
-            int count = 0;
-            for(int start=0; count <nums.size(); start++){
-                int currrentPosition = start;
-                int prevValue = nums[currrentPosition];
-                do{
-                    int nextPosition = (currrentPosition + k) % nums.size();
-                    int temp = nums[nextPosition];
-                    nums[nextPosition] = prevValue;
-                    prevValue = temp;
-                    currrentPosition = nextPosition;
-                    count++;
-                }while(currrentPosition!=start);
-            }   
-    }
-    
-    void reverseApproach(vector<int>& nums, int k){
-        // Using Reverse
-        k = k % nums.size();
-        reverseNumbers(nums,0,nums.size()-1);
-        reverseNumbers(nums,0,k-1);
-        reverseNumbers(nums,k,nums.size()-1);
-    }
-    
-    void reverseNumbers(vector<int>& nums, int start, int end ){
-        while(start<end){
-            swap(nums,start,end);
-            start++;
-            end--;
-        }
-    }
-    
 };
