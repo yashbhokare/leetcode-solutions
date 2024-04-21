99a48eb5b60e106fb8eb5dc8c954aa41874b20aa
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-20 21:58:11 -0400

    Time: 18 ms, Memory: 27.3 MB - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        cyclicRotation(nums,k);
+    }
+    
+    void reverseApproach(vector<int>& nums, int k){
         k=k%nums.size();
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin()+k);
         reverse(nums.begin()+k,nums.end());
+    }
+    
+    void cyclicRotation(vector<int>& nums, int k){
+        k=k%nums.size();
+        int count=0;
+        int size=nums.size();
+        int start=0;
+        while(count<size){
+            int pos=start;
+            int val=nums[pos];
+            do{
+                int move=(pos+k)%nums.size();
+                int temp=nums[move];
+                nums[move]=val;
+                pos=move;
+                val=temp;
+                count++;
+            }while(pos!=start);
+            start++;
+            // break;
+        }
     }
 };
