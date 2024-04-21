8359e7b3c53e38f06b35724f199090c57eac62f3
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-21 12:14:13 -0400

    Time: 3 ms, Memory: 23.1 MB - LeetHub

--- reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        stack<string> stack;
-        int index=0;
-        string result="";
-        while(index<s.length()){
-            // cout<<index<<" ";
-            if(s[index]==' '){
-                index++;
-                continue;
-            }
-            string word="";
-            while(index<s.length() && s[index]!=' '){
-                // cout<<index<<" ";
-                word+=s[index];
-                index++;
-            }
-            stack.push(word);
+    string reverseWords(string &s){
+        stringstream ss(s);
+        string word, ans;
+        while(ss >> word){
+            ans = word + " " + ans;
         }
-        while(!stack.empty()){
-            // cout<<stack.top()<<endl;
-            result+=stack.top();
-            stack.pop();
-            if(!stack.empty()){
-                result+=' ';
-            }   
-        }
-        return result;
+        return ans.substr(0, ans.size()-1);;     
     }
 };
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stack<string> stack;
+//         int index=0;
+//         string result="";
+//         while(index<s.length()){
+//             if(s[index]==' '){
+//                 index++;
+//                 continue;
+//             }
+//             string word="";
+//             while(index<s.length() && s[index]!=' '){
+//                 word+=s[index];
+//                 index++;
+//             }
+//             stack.push(word);
+//         }
+//         while(!stack.empty()){
+//             result+=stack.top();
+//             stack.pop();
+//             if(!stack.empty()){
+//                 result+=' ';
+//             }   
+//         }
+//         return result;
+//     }
+// };
