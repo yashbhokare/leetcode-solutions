2308e0d094cd9ab083f786e08652d2b6c2d819d6
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-21 20:01:03 -0400

    Time: 11 ms, Memory: 10.6 MB - LeetHub

--- linked-list-cycle/linked-list-cycle.cpp
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -12,10 +12,10 @@ public:
         if(head==NULL) return false;
         ListNode* slow=head;
         ListNode* fast=head->next;
-        while(fast!=NULL){
+        while(fast!=NULL && fast->next!=NULL){
             if(slow==fast) return true;
             slow=slow->next;
-            fast=fast->next!=NULL ? fast->next->next : NULL;
+            fast=fast->next->next;
         }
         return false;
     }
