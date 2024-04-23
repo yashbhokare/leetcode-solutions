c98402b6a342ff3980ad57cd6db391fd5be2c387
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-22 22:20:33 -0400

    Time: 8 ms, Memory: 13.9 MB - LeetHub

--- 0odd-even-linked-list/0odd-even-linked-list.cpp
+++ 0odd-even-linked-list/0odd-even-linked-list.cpp
@@ -12,28 +12,43 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
         if(head==NULL || head->next==NULL || head->next->next==NULL) return head;
-        ListNode* oddHead=head;
-        ListNode* evenHead=new ListNode(0);
-        
-        ListNode* evenVal=evenHead;
-        ListNode* oddVal=oddHead;
-        ListNode* tempEvenVal;
-        
-        while(oddVal->next!=NULL){
-
-            tempEvenVal=oddVal->next;
-            //Remove even number
-            oddVal->next=oddVal->next!=NULL ? oddVal->next->next : NULL;
-            oddVal=oddVal->next!=NULL ? oddVal->next : oddVal;
-
-            //Add the even number
-            evenVal->next=tempEvenVal;
-            evenVal=evenVal->next;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenHead=head->next;
+        while(even!=NULL && even->next!=NULL){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
         }
-        // Add Null to last pointer
-        evenVal->next=NULL;
-        oddVal->next=evenHead->next;
+        odd->next=evenHead;
         return head;
-        
+    
     }
+//     ListNode* oddEvenList(ListNode* head) {
+//         if(head==NULL || head->next==NULL || head->next->next==NULL) return head;
+//         ListNode* oddHead=head;
+//         ListNode* evenHead=new ListNode(0);
+        
+//         ListNode* evenVal=evenHead;
+//         ListNode* oddVal=oddHead;
+//         ListNode* tempEvenVal;
+        
+//         while(oddVal->next!=NULL){
+
+//             tempEvenVal=oddVal->next;
+//             //Remove even number
+//             oddVal->next=oddVal->next!=NULL ? oddVal->next->next : NULL;
+//             oddVal=oddVal->next!=NULL ? oddVal->next : oddVal;
+
+//             //Add the even number
+//             evenVal->next=tempEvenVal;
+//             evenVal=evenVal->next;
+//         }
+//         // Add Null to last pointer
+//         evenVal->next=NULL;
+//         oddVal->next=evenHead->next;
+//         return head;
+        
+//     }
 };
