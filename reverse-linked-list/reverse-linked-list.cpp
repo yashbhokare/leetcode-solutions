79f1f88fc4402173f33ff845d42864a2e38f1eb0
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-22 21:19:00 -0400

    Time: 3 ms, Memory: 11.6 MB - LeetHub

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -11,18 +11,31 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-        ListNode* newHead=new ListNode(0);
-        newHead->next=head;
-        ListNode* p=head;
-        while(p->next!=NULL){
-            ListNode* removedNode=p->next;
-            p->next=p->next!=NULL ? p->next->next : NULL;
+        return reverse(head);
+//         if(head==NULL || head->next==NULL) return head;
+//         ListNode* newHead=new ListNode(0);
+//         newHead->next=head;
+//         ListNode* p=head;
+//         while(p->next!=NULL){
+//             ListNode* removedNode=p->next;
+//             p->next=p->next!=NULL ? p->next->next : NULL;
             
-            // Adding at the head
-            removedNode->next=newHead->next;
-            newHead->next=removedNode;
+//             // Adding at the head
+//             removedNode->next=newHead->next;
+//             newHead->next=removedNode;
+//         }
+//         return newHead->next;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        ListNode* prev= NULL;
+        ListNode* curr= head;
+        while(curr){
+            ListNode* next_node = curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next_node;
         }
-        return newHead->next;
+        return prev;
     }
 };
