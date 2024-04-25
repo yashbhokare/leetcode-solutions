0aea4040903dc4bfffe1b8755516d1075808e36c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-25 08:04:36 -0400

    Time: 14 ms, Memory: 19.2 MB - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         unordered_set<int> hashSet;
+        int result=0;
         for(int num:nums){
-            if(hashSet.count(num)>0){
-                hashSet.erase(num);
-            }else {
-                hashSet.insert(num);
-            }
+            result = result ^ num;
+            // if(hashSet.count(num)>0){
+            //     hashSet.erase(num);
+            // }else {
+            //     hashSet.insert(num);
+            // }
         }
-        return *hashSet.begin();
-        
+        return result;
+        // return *hashSet.begin();
     }
 };
