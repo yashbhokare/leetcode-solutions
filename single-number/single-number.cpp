09d87685a41d4159739db4312181bd79e92664f2
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-25 08:01:57 -0400

    Time: 22 ms, Memory: 23.6 MB - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // return hashTable(nums);
-        return bitManipulation(nums);
-    }
-    
-    int hashTable(vector<int> nums){
-        unordered_set <int> numSet;
-        for(int i=0; i< nums.size(); i++){
-            if(numSet.find(nums[i]) == numSet.end()){
-                numSet.insert(nums[i]);
-            } else {
-                numSet.erase(nums[i]);
+        unordered_set<int> hashSet;
+        for(int num:nums){
+            if(hashSet.count(num)>0){
+                hashSet.erase(num);
+            }else {
+                hashSet.insert(num);
             }
         }
-        return *numSet.begin();
-    }
-    
-    
-// If we take XOR of zero and some bit, it will return that bit
-//a ⊕ 0 =a
-// If we take XOR of two same bits, it will return 0
-// a⊕a = 0
-// a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
-    int bitManipulation(vector<int> nums){
-        int num = 0;
-        for(int i=0; i< nums.size(); i++){
-            num = num ^ nums[i];
-        }
-        return num;
+        return *hashSet.begin();
+        
     }
 };
