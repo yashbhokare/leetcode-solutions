5a66bd09da014938e00f16245f758ce39b0ea042
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-04-25 22:22:04 -0400

    Time: 6 ms, Memory: 13.4 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() >= nums2.size()){
-            return calculateIntersect(nums1,nums2);
-        } else {
-            return calculateIntersect(nums2,nums1);
-        }
-    }
-    
-    vector<int> calculateIntersect(vector<int>& nums1, vector<int>& nums2){
-        // unordered_mulitset is used which can store duplicate elements
-        unordered_multiset <int> numSet;
-        vector<int> result; // Final result
-        for(int i=0; i<nums2.size(); i++){
-            numSet.insert(nums2[i]);
-        }
-        
-        for(int i=0; i<nums1.size(); i++){
-            // find function returns iterator to first position of found value we can pass this iterator to erase instead of actual value to delete only one copy
-            const auto it = numSet.find(nums1[i]);
-            if(it != numSet.end()){
-                result.push_back(nums1[i]);
-                numSet.erase(it);
+        vector<int> result;
+        unordered_multiset<int> set;
+        for(auto num:nums1) set.insert(num);
+        for(auto num:nums2){
+            auto it=set.find(num);
+            if(it!=set.end()){
+                result.push_back(num);
+                set.erase(it);
             }
         }
         return result;
-        
     }
 };
