e2014c0026920cd011cacd911cb141ce0667ae5c
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-05-03 19:26:06 -0400

    Time: 19 ms, Memory: 15.8 MB - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -12,8 +12,8 @@ public:
         for(int r=0;r<rMax;r++){
             for(int c=0;c<cMax;c++){
                 if(grid[r][c]=='1'){
-                    // dfs(grid,r,c);
-                    bfs(grid,r,c);
+                    dfs(grid,r,c);
+                    // bfs(grid,r,c);
                     count++;
                 }
             }
@@ -21,15 +21,13 @@ public:
         return count;
     }
     
-    
     void dfs(vector<vector<char>>& grid,int r,int c){
         grid[r][c]='0';
-        
         for(int i=0;i<4;i++){
-            int newR = r + dir[i][0];
-            int newC = c + dir[i][1];
-            if(newR>=0 && newR<rMax && newC>=0 && newC<cMax && grid[newR][newC]=='1'){
-                dfs(grid,newR,newC);
+            int newRow=r+dir[i][0];
+            int newCol=c+dir[i][1];
+            if(newRow>=0 && newRow<rMax && newCol>=0 && newCol<cMax && grid[newRow][newCol]=='1'){
+                dfs(grid,newRow,newCol);
             }
         }
     }
