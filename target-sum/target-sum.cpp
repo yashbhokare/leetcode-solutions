03b57b04c5d9ed19037e008892d806f36addf958
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-05-16 18:48:57 -0400

    Time: 745 ms, Memory: 10.6 MB - LeetHub

--- target-sum/target-sum.cpp
+++ target-sum/target-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int result=0;
-        recTargetSum(nums,target,0,result);
-        return result;
-    }
     
-    void recTargetSum(vector<int>& nums, int target,int index,int& result){
+    int result=0;
+    void recursion(vector<int>& nums,int index,int total,int target){
         if(index==nums.size()){
-            if(target==0){
+            if(target==total){
                 result++;
             }
             return;
         }
-        int newSum=target-nums[index];
-        int newSum2=target+nums[index];
-        recTargetSum(nums,newSum,index+1,result);
-        recTargetSum(nums,newSum2,index+1,result);
+        recursion(nums,index+1,total+nums[index],target);
+        recursion(nums,index+1,total-nums[index],target);
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        recursion(nums,0,0,target);
+        return result;
     }
 };
