155735cc7a467f72ade3f758da4e76a8004fe912
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-06-25 21:03:37 -0400

    Time: 5 ms, Memory: 10.8 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
- 
-        int lastIndex = nums1.size()-1;
-        int num1Point = m -1;
-        int num2Point = n-1;
-        while(num1Point >= 0 && num2Point>=0){
-            if(nums1[num1Point] >  nums2[num2Point]){
-                nums1[lastIndex] = nums1[num1Point];
-                num1Point--;
+        int index1=m-1;
+        int index2=n-1;
+        for(int i=nums1.size()-1;i>=0;i--){
+            int val1 = index1>=0 ? nums1[index1] : INT_MIN;
+            int val2 = index2>=0 ? nums2[index2] : INT_MIN;
+            if(val1>val2){
+                nums1[i]=val1;
+                index1--;
             }else {
-                nums1[lastIndex] =  nums2[num2Point];
-                num2Point--;
+                nums1[i]=val2;
+                index2--;
             }
-            lastIndex--;
-        }
-        while(num2Point>=0){
-            nums1[num2Point] = nums2[num2Point];
-            num2Point--;
         }
     }
-    
-        //    int i=m-1,j=n-1,k=m+n-1;
-        // while(i>=0&&j>=0)
-        // {
-        //     if(nums1[i]>nums2[j])
-        //     {
-        //         nums1[k]=nums1[i];
-        //         i--;
-        //         k--;
-        //     }
-        //     else
-        //     {
-        //         nums1[k]=nums2[j];
-        //         j--;
-        //         k--;
-        //     }
-        // }
-        // while(i>=0)
-        //     nums1[k--]=nums1[i--];
-        // while(j>=0)
-        //     nums1[k--]=nums2[j--];
 };
