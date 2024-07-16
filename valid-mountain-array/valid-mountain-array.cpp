768d13d7606c2b445155494e6b7c6ad7c95b494e
Yash Bhokare <41475312+yashbhokare@users.noreply.github.com>
2024-07-15 21:11:38 -0400

    Time: 16 ms, Memory: 25.2 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.cpp
+++ valid-mountain-array/valid-mountain-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        int n = arr.size();
-        if(n<3) return false;
-        bool isIncreasing=false;
+        // Check for length
+        if(arr.size()<3) return false;
+        bool isIncreasing = false;
         bool isDecreasing = false;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<arr.size();i++){
             if(arr[i]>arr[i-1] && !isDecreasing){
                 isIncreasing=true;
-            }else if(arr[i]<arr[i-1]){
-                isDecreasing=true;
+            }else if(arr[i]<arr[i-1] && isIncreasing){
+                isDecreasing= true;
             }else {
                 return false;
             }
         }
-        return isIncreasing && isDecreasing;
+        return isDecreasing;
     }
 };
